a581da04f1369da4763d241283667ea628ab98b9
Justin Wozniak <wozniak@mcs.anl.gov>
2011-05-11 21:15:59 +0000

    Correct batch program outline
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@377 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- src/progs/batch.c
+++ src/progs/batch.c
@@ -1,6 +1,4 @@
 
-#include "src/command.h"
-
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,33 +6,14 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
-const char* PROMPT = "turbine> ";
+#include "src/turbine/command.h"
 
 int
 main(int argc, char* argv[])
 {
-  char* state;
-  turbine_code code;
-  code = turbine_init();
+  // Use reader to read input file
 
-  while (true)
-  {
-    char* cmd = readline(PROMPT);
-    if (!cmd)
-      continue;
-    char* copy = strdup(cmd);
-    char* token = strtok_r(copy, " ", &state);
-    if (strcmp(token, "quit") == 0)
-      break;
-    free(copy);
+  // Insert commands into turbine
 
-    turbine_code code = turbine_command(cmd);
-    if (code != TURBINE_SUCCESS)
-    {
-      char msg[64];
-      turbine_code_tostring(code, msg);
-      printf("turbine: command failed: %s\n", msg);
-      exit(1);
-    }
-  }
+  // Run?
 }
