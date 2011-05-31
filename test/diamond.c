3aa886f0fe91f6a972a3815bda9c7d9a69750e5b
Justin Wozniak <wozniak@mcs.anl.gov>
2011-05-31 21:31:46 +0000

    Copy data test to diamond
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@464 dc4e9af1-7f46-4ead-bba6-71afc04862de

---
+++ test/diamond.c
@@ -0,0 +1,20 @@
+/*
+ * data.c
+ *
+ *  Created on: May 4, 2011
+ *      Author: wozniak
+ */
+
+#include <stdio.h>
+
+#include "src/turbine/turbine.h"
+
+int
+main()
+{
+  turbine_datum_id d1;
+  turbine_code code;
+  code = turbine_init();
+  code = turbine_datum_file_create(&d1, "file.txt");
+  turbine_finalize();
+}
