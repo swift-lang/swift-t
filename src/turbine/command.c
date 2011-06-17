57899b1053bf3fadc4e2697f4d3b99c1ed8eb6b7
Justin Wozniak <wozniak@mcs.anl.gov>
2011-06-17 17:54:02 +0000

    Checkin to clean working copy - obsolete code
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@535 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- src/turbine/command.c
+++ src/turbine/command.c
@@ -1,8 +1,19 @@
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 
 #include "src/turbine/command.h"
+
+turbine_code declare(char** state);
+turbine_code rule(char** state);
+
+#define check_token(token) \
+  { if (strlen(token) == 0) return TURBINE_ERROR_COMMAND; \
+    if (!isalpha(token[0])) return TURBINE_ERROR_COMMAND; }
+
+turbine_code
+turbine_command(char* cmd)
 
 #define check(condition) \
   if (!(condition)) return TURBINE_ERROR_COMMAND;
@@ -19,7 +30,35 @@ turbine_code turbine_command(char* cmd)
 
   printf("op: %s\n", op);
   if (strcmp(op, "data") == 0)
+    result = declare(&state);
+  else if (strcmp(op, "rule") == 0)
+    result = rule(&state);
+  else
+    return TURBINE_ERROR_COMMAND;
+
+  return result;
+}
+
+/**
+   declare file <id> <name>
+ */
+turbine_code
+declare(char** state)
+{
+  turbine_code result;
+  char* type = strtok_r(NULL, " ", state);
+  if (!type)
+    return TURBINE_ERROR_COMMAND;
+  if (strcmp(type, "file") == 0)
   {
+    /// WHICH?
+    puts("file");
+    turbine_datum_id id;
+    char* sid = strtok_r(NULL, " ", &state);
+    int count = sscanf(sid, "%li", &id);
+    if (count != 1)
+      return TURBINE_ERROR_COMMAND;
+    /// =======
     char* sid = strtok_r(NULL, " ", &state);
     check(sid);
     turbine_datum_id id;
@@ -28,7 +67,55 @@ turbine_code turbine_command(char* cmd)
 
     char* type = strtok_r(NULL, " ", &state);
     check(type);
-    if (strcmp(type, "file") == 0)
+    /// WHICH? >>>>>>> .r534
+    printf("id: %li\n", id);
+    char* name = strtok_r(NULL, " ", &state);
+    puts("name");
+    result = turbine_datum_file_create(id, name);
+  }
+  else
+    return TURBINE_ERROR_COMMAND;
+  return result;
+}
+
+/**
+   rule ( <output ids> ) <name> ( <input ids> ) executor
+ */
+turbine_code
+rule(char** state)
+{
+  turbine_code result;
+  char executor[TURBINE_COMMAND_MAX_EXECUTOR];
+  turbine_datum_id output_ids[TURBINE_COMMAND_MAX_TOKENS];
+  turbine_datum_id input_ids[TURBINE_COMMAND_MAX_TOKENS];
+
+  result = scan_ids(state, output_ids);
+  turbine_check(result);
+
+  char* name = strtok_r(NULL, state);
+  check_token(name);
+
+  result = scan_ids(state, output_ids);
+  turbine_check(result);
+
+  result = rest_of_tokens(state, executor);
+  turbine_check(result);
+
+  return result;
+}
+
+turbine_code
+scan_ids(char** state, turbine_datum_id* output)
+{
+  char* paren1 = strtok_r(NULL, " ", state);
+  if (strcmp(paren1, "(") != 0)
+    return TURBINE_ERROR_COMMAND;
+
+  int p = 0;
+  while (true)
+  {
+    char* t = strtok_r(NULL, " ", state);
+    if (strcmp(t, ")") == 0)
     {
       puts("file");
       char* sid = strtok_r(NULL, " ", &state);
@@ -41,15 +128,24 @@ turbine_code turbine_command(char* cmd)
       turbine_datum_file_create(id, name);
       printf("id: %li\n", id);
     }
-    else
+    turbine_datum_id id;
+    int count = sscanf(t, "%li", &id);
+    if (count != 1)
+      return TURBINE_ERROR_COMMAND;
+    output[p++] = id;
+    if (p > TURBINE_COMMAND_MAX_TOKENS)
       return TURBINE_ERROR_COMMAND;
   }
-  if (strcmp(op, "rule") == 0)
+  return TURBINE_SUCCESS;
+}
+
+turbine_code
+rest_of_tokens(char** state, char* output)
+{
+  while (true)
   {
+    char* t = strtok_r(NULL, state);
 
   }
-  else
-    return TURBINE_ERROR_COMMAND;
-
-  return result;
+}
 }
--- src/turbine/command.h
+++ src/turbine/command.h
@@ -1,4 +1,14 @@
 
 #include "src/turbine/turbine.h"
 
+/**
+   Maximum number of tokens in a turbine command line
+ */
+#define TURBINE_COMMAND_MAX_TOKENS
+
+/**
+   Maximum number of characters in a turbine command token
+ */
+#define TURBINE_COMMAND_MAX_TOKEN
+
 turbine_code turbine_command(char* cmd);
