c60f692696519b9333585ca91c489e99521f4069
Justin Wozniak <wozniak@mcs.anl.gov>
2011-06-22 20:03:18 +0000

    New use case with function call, stack frame, local variable
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@581 dc4e9af1-7f46-4ead-bba6-71afc04862de

---
+++ test/func1.tcl
@@ -0,0 +1,34 @@
+
+package require turbine 0.1
+
+proc f { inputs outputs } {
+    set tmp [ turbine_new ]
+    set tmpfile "tmp-$tmp.txt"
+    turbine_file $tmp $tmpfile
+    set data1 [ lindex $inputs 0 ]
+    set filename1 [ turbine_filename $data1 ]
+    set filename2 [ turbine_filename [ lindex $outputs 0 ] ]
+    set rule1 [ turbine_new ]
+    turbine_rule $rule1 "f" $inputs $tmp  "cp $filename1 $tmpfile"
+    set rule2 [ turbine_new ]
+    turbine_rule $rule2 "f" $tmp $outputs "cp $tmpfile $filename2"
+}
+
+proc rules { } {
+    turbine_file 0 /dev/null
+    turbine_file 1 A.txt
+    turbine_file 2 B.txt
+
+    turbine_rule 1 A  {   } { 1 } { touch A.txt }
+    turbine_rule 2 F1 { 1 } { 2 } { tp: f { 1 } { 2 } }
+}
+
+turbine_init
+
+rules
+
+turbine_engine
+
+turbine_finalize
+
+puts OK
