080aa5407837742a4d8ccfea4f261d8b3b4a679d
Justin Wozniak <wozniak@mcs.anl.gov>
2011-06-22 19:57:57 +0000

    Enable calls to turbine functions
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@579 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- lib/turbine.tcl
+++ lib/turbine.tcl
@@ -3,6 +3,20 @@
 
 package provide turbine 0.1
 
+proc turbine_eval { command } {
+
+    set command [ string trim $command ]
+    set prefix "[ string range $command 0 2 ]"
+    if { [ string equal $prefix "tp:" ] } {
+        set proccall [ lrange $command 1 end ]
+        puts $proccall
+        eval $proccall
+    } else {
+        puts "exec: $command"
+        eval exec $command
+    }
+}
+
 proc turbine_engine { } {
 
     turbine_push
@@ -15,7 +29,7 @@ proc turbine_engine { } {
         foreach {transform} $ready {
             set command [ turbine_executor $transform ]
             puts "executing: $command"
-            if { [ catch { eval exec $command } ] } {
+            if { [ catch { turbine_eval $command } ] } {
                 error "rule: $transform failed in command: $command"
             }
             turbine_complete $transform
