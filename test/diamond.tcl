bf7bf7e9fb951c7b2748341fd8a4e641671f7292
Justin Wozniak <wozniak@mcs.anl.gov>
2011-06-22 14:30:57 +0000

    Add proc rules{}
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@566 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- test/diamond.tcl
+++ test/diamond.tcl
@@ -1,18 +1,22 @@
 
 package require turbine 0.1
 
+proc rules { } {
+    turbine_file 0 /dev/null
+    turbine_file 1 A.txt
+    turbine_file 2 B.txt
+    turbine_file 3 C.txt
+    turbine_file 4 D.txt
+
+    turbine_rule 1 A {     } { 1 } { touch A.txt }
+    turbine_rule 2 B { 1   } { 2 } { touch B.txt }
+    turbine_rule 3 C { 1   } { 3 } { touch C.txt }
+    turbine_rule 4 D { 2 3 } { 4 } { touch D.txt }
+}
+
 turbine_init
 
-turbine_file 0 /dev/null
-turbine_file 1 A.txt
-turbine_file 2 B.txt
-turbine_file 3 C.txt
-turbine_file 4 D.txt
-
-turbine_rule 1 A {     } { 1 } { touch A.txt }
-turbine_rule 2 B { 1   } { 2 } { touch B.txt }
-turbine_rule 3 C { 1   } { 3 } { touch C.txt }
-turbine_rule 4 D { 2 3 } { 4 } { touch D.txt }
+rules
 
 turbine_engine
 
