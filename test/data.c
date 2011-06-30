4212418a437e72b7e2d640d5338d1178636864b4
Justin Wozniak <wozniak@mcs.anl.gov>
2011-06-30 00:26:59 +0000

    Test framework works
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@624 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- test/data.c
+++ test/data.c
@@ -1,10 +1,13 @@
 /*
  * data.c
  *
+ * Test elemental data operations
+ *
  *  Created on: May 4, 2011
  *      Author: wozniak
  */
 
+#include <assert.h>
 #include <stdio.h>
 
 #include "src/turbine/turbine.h"
@@ -15,7 +18,9 @@ main()
   turbine_datum_id d1 = 1;
   turbine_code code;
   code = turbine_init();
+  assert(code == TURBINE_SUCCESS);
   code = turbine_datum_file_create(d1, "file.txt");
+  assert(code == TURBINE_SUCCESS);
   turbine_finalize();
-  puts("OK");
+  puts("DONE");
 }
---
+++ test/data.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+TESTS=$( dirname $0 )
+
+BIN=$1
+OUTPUT=${BIN%.x}.out
+
+[[ ${TURBINE_DEBUG_TEST} != "" ]] && set -x
+
+${TESTS}/runbin.zsh ${BIN} >& ${OUTPUT}
+
+grep DONE ${OUTPUT} || exit 1
+
+exit 0
