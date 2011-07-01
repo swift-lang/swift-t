1c5fc9580fd8d769a264f4046f5ff45c945b178f
Justin Wozniak <wozniak@mcs.anl.gov>
2011-07-01 18:05:34 +0000

    Clean up
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@645 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- test/diamond2.sh
+++ test/diamond2.sh
@@ -11,4 +11,9 @@ bin/turbine ${SCRIPT} >& ${OUTPUT}
 LINES=$( grep -c "exec.* touch ..txt" ${OUTPUT} )
 (( ${LINES} >= 4 )) || exit 1
 
+LINES=$( ls [ABCD].txt | wc -l )
+(( ${LINES} == 4 )) || exit 1
+
+rm [ABCD].txt || exit 1
+
 exit 0
