37f959b6b6c12bf8bb4f342023d8f53569881a43
Justin Wozniak <wozniak@mcs.anl.gov>
2011-07-05 17:01:36 +0000

    New proc enum{}
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@669 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- lib/helpers.tcl
+++ lib/helpers.tcl
@@ -1,7 +1,20 @@
 
+# Shutdown TCL if condition does not hold
 proc assert { condition msg } {
     if [ expr ! $condition ] {
         puts msg
         exit 1
+    }
+}
+
+# Given: enum E { T1 T2 }
+# Defines global array E with members E(T1)=0, E(T2)=1
+proc enum { name members } {
+    uplevel #0 "global $name"
+    global $name
+    set n 0
+    foreach m $members {
+        set ${name}($m) $n
+        incr n
     }
 }
