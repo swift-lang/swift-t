2d6e2008c120d3528ab20cf6c484974efd80d6ce
Justin Wozniak <wozniak@mcs.anl.gov>
2011-07-07 18:29:19 +0000

    Reorganize Turbine TCL libs
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@699 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- lib/make-package.tcl
+++ lib/make-package.tcl
@@ -1,4 +1,4 @@
 
 # Generate the turbine TCL package
 
-puts [ ::pkg::create -name turbine -version 0.1 -load libtclturbine.so -load libtcladlb.so -source turbine.tcl -source turbine-adlb.tcl -source helpers.tcl ]
+puts [ ::pkg::create -name turbine -version 0.1 -load libtclturbine.so -load libtcladlb.so -source turbine.tcl -source turbine-engine.tcl -source turbine-engine-adlb.tcl -source helpers.tcl ]
--- lib/turbine-engine.tcl
+++ lib/turbine-engine.tcl
@@ -3,20 +3,6 @@
 
 package provide turbine 0.1
 
-proc turbine_eval { command } {
-
-    set command [ string trim $command ]
-    set prefix "[ string range $command 0 2 ]"
-    if { [ string equal $prefix "tp:" ] } {
-        set proccall [ lrange $command 1 end ]
-        puts "eval: $proccall"
-        eval $proccall
-    } else {
-        puts "exec: $command"
-        eval exec $command
-    }
-}
-
 proc turbine_engine { } {
 
     turbine_push
