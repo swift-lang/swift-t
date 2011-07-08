54e2c557445ee17524e133bf710c45698186d6ac
Justin Wozniak <wozniak@mcs.anl.gov>
2011-07-08 20:03:49 +0000

    Add container2
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@706 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- test/container2.sh
+++ test/container2.sh
@@ -9,12 +9,8 @@ OUTPUT=${THIS%.sh}.out
 bin/turbine ${SCRIPT} >& ${OUTPUT}
 [[ ${?} == 0 ]] || exit 1
 
-# LINES=$( grep -c "exec.* touch ..txt" ${OUTPUT} )
-# (( ${LINES} >= 4 )) || exit 1
-
-# LINES=$( ls [ABCD].txt | wc -l )
-# (( ${LINES} == 4 )) || exit 1
-
-# rm [ABCD].txt || exit 1
+grep -q "enumeration: 0 1" ${OUTPUT} || exit 1
+grep -q "file1.txt"        ${OUTPUT} || exit 1
+grep -q "file2.txt"        ${OUTPUT} || exit 1
 
 exit 0
