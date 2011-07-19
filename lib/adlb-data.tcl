0d057b80588fbb38ba238ddf337c1079bd4e42fa
Justin Wozniak <wozniak@mcs.anl.gov>
2011-07-19 19:30:44 +0000

    Check in before delete
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/distributed@785 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- lib/adlb-data.tcl
+++ lib/adlb-data.tcl
@@ -4,20 +4,4 @@
 namespace eval turbine::adlb::data {
 
     namespace export string_init string_set string_get
-
-    proc string_init { id } {
-        adlb::store $id "string:UNSET"
-    }
-
-    proc string_set { id value } {
-        adlb::store $id "string:$value"
-    }
-
-    proc string_get { id } {
-        puts "get $id"
-        set s [ adlb::retrieve $id ]
-        set i [ string first : $s ]
-        set result [ string range $s $i end ]
-        return $result
-    }
 }
