665677dee980b60059d6c4b4bf2a3191cf63e8bf
Justin Wozniak <wozniak@mcs.anl.gov>
2011-07-20 21:35:42 +0000

    Update for functions.tcl; make more readable
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/distributed@806 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- lib/make-package.tcl
+++ lib/make-package.tcl
@@ -1,4 +1,10 @@
 
-# Generate the turbine TCL package
+# Generate the Turbine TCL package
 
-puts [ ::pkg::create -name turbine -version 0.1 -load libtcladlb.so -load libtclturbine.so -source turbine.tcl -source engine.tcl -source data.tcl -source turbine-lib.tcl -source helpers.tcl ]
+puts [ ::pkg::create -name turbine -version 0.1 \
+       -load libtcladlb.so -load libtclturbine.so \
+       -source turbine.tcl   \
+       -source engine.tcl    \
+       -source data.tcl      \
+       -source functions.tcl \
+       -source helpers.tcl ]
