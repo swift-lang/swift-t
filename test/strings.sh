d1f4873474cb736adbade4e64fd8d43a215c5429
Justin Wozniak <wozniak@mcs.anl.gov>
2011-07-22 18:05:00 +0000

    Fix up test/strings
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/distributed@830 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- test/strings.sh
+++ test/strings.sh
@@ -6,7 +6,7 @@ THIS=$0
 SCRIPT=${THIS%.sh}.tcl
 OUTPUT=${THIS%.sh}.out
 
-bin/turbine ${SCRIPT} >& ${OUTPUT}
+mpiexec -n 3 bin/turbine ${SCRIPT} >& ${OUTPUT}
 [[ ${?} == 0 ]] || exit 1
 
 # grep -q "result: 4" ${OUTPUT} || exit 1
--- test/strings.tcl
+++ test/strings.tcl
@@ -8,24 +8,26 @@
 
 package require turbine 0.1
 
-namespace import turbine::* turbine::data::*
+namespace import turbine::string_*
 
-init
+proc rules { } {
 
-string_init 1
-string_init 2
-# c::string 3
+    string_init 1
+    string_init 2
+    # c::string 3
 
-string_set 1 "hi"
-string_set 2 "bye"
+    string_set 1 "hi"
+    string_set 2 "bye"
 
-set v1 [ string_get 1 ]
-set v2 [ string_get 2 ]
+    set v1 [ string_get 1 ]
+    set v2 [ string_get 2 ]
 
-puts -nonewline "result: "
-turbine::trace 1 2
+    puts -nonewline "result: "
+    turbine::trace 1 2
+}
 
-engine
-finalize
+turbine::init 1
+turbine::start rules
+turbine::finalize
 
 puts OK
