c429a7b2c2ed8ea748b1a34d52fe3d6c310c2ea5
Justin Wozniak <wozniak@mcs.anl.gov>
2011-07-22 17:20:22 +0000

    Fix up test/arithmetic
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/distributed@828 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- test/arithmetic.sh
+++ test/arithmetic.sh
@@ -6,7 +6,7 @@ THIS=$0
 SCRIPT=${THIS%.sh}.tcl
 OUTPUT=${THIS%.sh}.out
 
-bin/turbine ${SCRIPT} >& ${OUTPUT}
+mpiexec -l -n 3 bin/turbine ${SCRIPT} >& ${OUTPUT}
 [[ ${?} == 0 ]] || exit 1
 
 grep -q "trace: 64" ${OUTPUT} || exit 1
