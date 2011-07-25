c63c3d22a5b174003cf7cb32e053d041692fd8cb
Justin Wozniak <wozniak@mcs.anl.gov>
2011-07-25 18:20:23 +0000

    Fix test script
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/distributed@837 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- test/strings.sh
+++ test/strings.sh
@@ -6,9 +6,9 @@ THIS=$0
 SCRIPT=${THIS%.sh}.tcl
 OUTPUT=${THIS%.sh}.out
 
-mpiexec -n 3 bin/turbine ${SCRIPT} >& ${OUTPUT}
+mpiexec -l -n 3 bin/turbine ${SCRIPT} >& ${OUTPUT}
 [[ ${?} == 0 ]] || exit 1
 
-# grep -q "result: 4" ${OUTPUT} || exit 1
+grep -q "hi,bye" ${OUTPUT} || exit 1
 
 exit 0
