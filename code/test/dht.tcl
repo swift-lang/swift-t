a25b76497bb8f45a7a492b5dd8f27c3168d0b861
Justin Wozniak <wozniak@mcs.anl.gov>
2011-08-09 21:40:53 +0000

    DHT test works - will use for performance measurement
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/distributed@895 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/test/dht.tcl
+++ code/test/dht.tcl
@@ -7,10 +7,17 @@ namespace import turbine::string_*
 
 turbine::init $env(TURBINE_ENGINES) $env(ADLB_SERVERS)
 
+set count 10
+
 if { ! [ adlb::amserver ] } {
 
-    adlb::create 12 string
-    adlb::store 12 string:data
+    set size [ adlb::size ]
+    set rank [ adlb::rank ]
+    puts "MPI size: $size"
+    for { set i $rank } { $i <= $count } { incr i $size } {
+        adlb::create $i string:
+        adlb::store $i string:data
+    }
 }
 
 turbine::finalize
