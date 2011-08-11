9481b743aca8cbb3bf5e0b2077954e5bcd46a48c
Justin Wozniak <wozniak@mcs.anl.gov>
2011-08-11 16:28:36 +0000

    Output reduction
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/distributed@919 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/test/dht.tcl
+++ code/test/dht.tcl
@@ -7,13 +7,18 @@ namespace import turbine::string_*
 
 turbine::init $env(TURBINE_ENGINES) $env(ADLB_SERVERS)
 
-set count 10
 
 if { ! [ adlb::amserver ] } {
 
+    set count 10
+    if { [ info exists env(COUNT) ] > 0 } {
+        set count $env(COUNT)
+    }
+    # puts "COUNT: $count"
+
     set size [ adlb::size ]
     set rank [ adlb::rank ]
-    puts "MPI size: $size"
+    # puts "MPI size: $size"
     for { set i $rank } { $i <= $count } { incr i $size } {
         adlb::create $i string:
         adlb::store $i string:data
