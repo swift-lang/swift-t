4d0260f2b5b2671ae85d94b254a2110f6b6e530c
Justin Wozniak <wozniak@mcs.anl.gov>
2011-08-16 18:42:54 +0000

    Output COUNT
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/distributed@951 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/test/dht.tcl
+++ code/test/dht.tcl
@@ -7,17 +7,19 @@ namespace import turbine::string_*
 
 turbine::init $env(TURBINE_ENGINES) $env(ADLB_SERVERS)
 
-
 if { ! [ adlb::amserver ] } {
 
     set count 10
     if { [ info exists env(COUNT) ] > 0 } {
         set count $env(COUNT)
     }
-    # puts "COUNT: $count"
 
     set size [ adlb::size ]
     set rank [ adlb::rank ]
+    if { $rank == 0 } {
+        puts "COUNT: $count"
+    }
+
     # puts "MPI size: $size"
     for { set i $rank } { $i <= $count } { incr i $size } {
         adlb::create $i string:
