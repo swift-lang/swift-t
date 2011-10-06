4b4e73d3c616d1265ab192923e82084b47a5d8b2
Justin Wozniak <wozniak@mcs.anl.gov>
2011-10-06 15:38:46 +0000

    Usage note
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@1469 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/check.sh
+++ code/check.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Input: FILE containing list of file names
+# Output: Warning for each file name that does not exist
+
+# Used to check for the presence of include files
+
 FILE=$1
 
 for f in $( cat ${FILE} )
