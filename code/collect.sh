0b6b05ab7628bae3e58f9a730acb3e4c8e673078
Justin Wozniak <wozniak@mcs.anl.gov>
2011-10-06 15:39:19 +0000

    Bug fix and usage notes
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@1470 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/collect.sh
+++ code/collect.sh
@@ -7,9 +7,12 @@ OUTPUT=$1
 shift
 INPUT=${*}
 
+# Puts one word per line; strip spaces, backslash, colon
+# Strip out object files *.o, *.po, and dependency files *.d
+# Make unique
+
 cat ${INPUT} | \
   fmt -w 1 | cut -f 1  | \
-  sed 's/ //g;s/\\//g' | \
+  sed 's/ //g;s/\\//g;s/://g' | \
   grep -v '.*\.o\>\|.*\.po\>\|.*\.d\>' | \
   sort -u > ${OUTPUT}
-
