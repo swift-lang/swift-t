ce8b9a8bca5fb6ecc7bb8161b221d48778211fdd
Justin Wozniak <wozniak@mcs.anl.gov>
2011-10-12 22:56:05 +0000

    Update test wrt ADLB changes
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@1488 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/test/adlb-noop.tcl
+++ code/test/adlb-noop.tcl
@@ -1,9 +1,14 @@
 
-# Flex Turbine+ADLB but do nothing
+# Flex ADLB but do nothing
 # Nice to have for quick manual experiments
 
 package require turbine 0.1
-turbine::init 1 1
+adlb::init 1 1
 
-turbine::finalize
+if [ adlb::amserver ] {
+    adlb::server
+} else
+{}
+
+adlb::finalize
 puts OK
