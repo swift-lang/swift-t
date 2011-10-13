5df5c7730401a9dd85c91bbce57653fe3385b27c
Justin Wozniak <wozniak@mcs.anl.gov>
2011-10-13 00:05:54 +0000

    Fix up adlb-data test
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@1495 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/test/adlb-data.tcl
+++ code/test/adlb-data.tcl
@@ -1,20 +1,51 @@
 
-# Turbine data abstraction layer
+# Flex ADLB DHT with Turbine data
+# No real Turbine data flow here
 
-namespace eval turbine::adlb::data {
+package require turbine 0.1
 
-    namespace export string_init string_set string_get
+namespace import turbine::string_*
 
-    proc string_init { id } {
-        c::string $id
-    }
-
-    proc string_set { id value } {
-        c::string_set $id $value
-    }
-
-    proc string_get { id } {
-        c::string_get $id
-    }
-
+if [ info exists env(ADLB_SERVERS) ] {
+    set servers $env(ADLB_SERVERS)
+} else {
+    set servers ""
 }
+if { [ string length $servers ] == 0 } {
+    set servers 1
+}
+if [ info exists env(ADLB_ENGINES) ] {
+    set engines $env(ADLB_ENGINES)
+} else {
+    set engines ""
+}
+if { [ string length $engines ] == 0 } {
+    set engines 1
+}
+
+turbine::init $engines $servers
+
+
+if { ! [ adlb::amserver ] } {
+
+    set count 4
+
+    set rank [ adlb::rank ]
+    puts "rank: $rank"
+    set workers [ adlb::workers ]
+
+    for { set i 0 } { $i < $count } { incr i } {
+        set id [ expr $rank + $i * $workers + 1]
+        string_init $id
+        string_set $id "message rank:$rank:$i"
+
+        set id [ expr $rank + $i * $workers + 1 ]
+        puts "get"
+        set msg [ string_get $id ]
+        puts "got: $msg"
+    }
+}
+
+turbine::finalize
+
+puts OK
