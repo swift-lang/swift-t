600f50c726a71382cea1f094fb3303d6f87728f3
Justin Wozniak <wozniak@mcs.anl.gov>
2012-01-06 22:02:50 +0000

    Use new ADLB_Create() functionality in tests
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@1878 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/test/dht.tcl
+++ code/test/dht.tcl
@@ -24,7 +24,7 @@ if { ! [ adlb::amserver ] } {
     # puts "MPI size: $size"
     set r [ expr $rank + 1 ]
     for { set i $r } { $i <= $count } { incr i $size } {
-        adlb::create $i string:
+        adlb::create $i string
         adlb::store $i string:data
     }
 } else {
