458ea4c4998b1b96a74d9ebc302403861315f657
Justin Wozniak <wozniak@mcs.anl.gov>
2012-01-23 20:34:51 +0000

    Test what happens when adlb::retrieve fails
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@2096 dc4e9af1-7f46-4ead-bba6-71afc04862de

---
+++ code/test/adlb-get-fail.tcl
@@ -0,0 +1,15 @@
+
+# Test what happens if we try to get something that does not exist
+# Nice to have for quick manual experiments
+
+package require turbine 0.0.1
+adlb::init 1 1
+
+if [ adlb::amserver ] {
+    adlb::server
+} else {
+    puts "get: `[ adlb::retrieve 1 ]'"
+}
+
+adlb::finalize
+puts OK
