4f4d01ac206b46635d21062cc12eefb7cab2d345
Justin Wozniak <wozniak@mcs.anl.gov>
2012-01-24 17:23:17 +0000

    Turn on GCC attribute format - exposes bug
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@2104 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/src/util/debug-auto.tcl
+++ code/src/util/debug-auto.tcl
@@ -27,8 +27,11 @@ out "#ifndef DEBUG_H\n"
 out "#define DEBUG_H\n\n"
 
 out "void turbine_debug_init(void);\n\n"
+
+out "__attribute__ ((format (printf, 2, 3)))\n"
 out "void turbine_debug(const char* token, " \
                        "const char* format, ...);\n\n"
+
 out "void turbine_debug_finalize(void);\n\n"
 
 # String breaks were necessary to prevent TCL string interpolation
