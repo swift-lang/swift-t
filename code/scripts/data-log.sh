8ca9000f104b35c36dcc620d4bd5c512c9bb1cdc
Justin Wozniak <wozniak@mcs.anl.gov>
2012-01-25 18:34:58 +0000

    Adding scripts/data-log.sh
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@2128 dc4e9af1-7f46-4ead-bba6-71afc04862de

---
+++ code/scripts/data-log.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Initial script to view data assignments from Turbine log
+
+LOG=$1
+
+if [[ ${LOG} == "" ]]
+then
+  echo "Not given: LOG"
+  exit 1
+fi
+
+grep "variable:\|INSERT:" ${LOG}
