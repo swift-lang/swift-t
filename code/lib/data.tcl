cd48e234b35067af3084895101c4cb84271204e4
Justin Wozniak <wozniak@mcs.anl.gov>
2012-01-25 20:41:08 +0000

    New logging message for container insertion
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@2139 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/lib/data.tcl
+++ code/lib/data.tcl
@@ -96,6 +96,8 @@ namespace eval turbine {
     }
 
     proc container_insert { id subscript value } {
+
+        log "insert: <$id>[$subscript]=<$value>"
         adlb::insert $id $subscript $value
     }
 
