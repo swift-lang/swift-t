f1dda607b324073e8290c981adb361a050b0b08c
Justin Wozniak <wozniak@mcs.anl.gov>
2012-02-09 19:07:16 +0000

    Fix test bug
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@2432 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/test/adlb-get-fail.sh
+++ code/test/adlb-get-fail.sh
@@ -7,7 +7,7 @@ SCRIPT=${THIS%.sh}.tcl
 OUTPUT=${THIS%.sh}.out
 
 bin/turbine -n 2 ${SCRIPT} >& ${OUTPUT}
-[[ ${?} != 0 ]] || exit 1
+[[ ${?} == 0 ]] || exit 1
 
 grep "retrieve.*failed" ${OUTPUT} || exit 1
 
