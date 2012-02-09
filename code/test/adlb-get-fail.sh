f36abab88a03d6227a1e93eca00121c47fcfd2ff
Justin Wozniak <wozniak@mcs.anl.gov>
2012-02-09 19:12:35 +0000

    Actually, this is supposed to exit non-zero (due to adlb::retrieve)
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@2433 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/test/adlb-get-fail.sh
+++ code/test/adlb-get-fail.sh
@@ -7,7 +7,8 @@ SCRIPT=${THIS%.sh}.tcl
 OUTPUT=${THIS%.sh}.out
 
 bin/turbine -n 2 ${SCRIPT} >& ${OUTPUT}
-[[ ${?} == 0 ]] || exit 1
+# This script is expected to fail on adlb::retrieve failure
+[[ ${?} == 1 ]] || exit 1
 
 grep "retrieve.*failed" ${OUTPUT} || exit 1
 
