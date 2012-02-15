6b50f370e2191a0ebf89edfb64411a1c481de730
Justin Wozniak <wozniak@mcs.anl.gov>
2012-02-15 15:30:47 +0000

    Whitespace
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@2512 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/scripts/submit/bgp/turbine-cobalt.zsh
+++ code/scripts/submit/bgp/turbine-cobalt.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-# Variables that may have defaults set in the environment: 
+# Variables that may have defaults set in the environment:
 # QUEUE, TURBINE_OUTPUT_ROOT, PROJECT
 
 TURBINE_HOME=$( cd $( dirname $0 )/../../.. ; /bin/pwd )
@@ -12,16 +12,16 @@ PROCS=0
 WALLTIME="00:15:00"
 
 # Job environment
-typeset -T ENV env 
-env=() 
+typeset -T ENV env
+env=()
 
 while getopts "e:n:o:t:" OPTION
  do
  case ${OPTION}
    in
    e) env+=${OPTARG}
-     ;; 
-   n) PROCS=${OPTARG}     
+     ;;
+   n) PROCS=${OPTARG}
      ;;
    o) TURBINE_OUTPUT_ROOT=${OPTARG}
      ;;
@@ -39,7 +39,7 @@ SCRIPT=$1
 START=$( date +%s )
 
 checkvars QUEUE SCRIPT
-  
+
 if [[ ${TURBINE_OUTPUT_ROOT} == "" ]]
 then
   TURBINE_OUTPUT_ROOT=${HOME}/turbine-output
@@ -59,16 +59,13 @@ SCRIPT_NAME=$( basename ${SCRIPT} )
 
 JOB_ID_FILE=${TURBINE_OUTPUT}/jobid.txt
 
-TURBINE_ENGINES=$(( PROCS/64 ))
-ADLB_SERVERS=$((    PROCS/64 ))
-
-print "TURBINE_ENGINES: ${TURBINE_ENGINES}" >> ${LOG}
-print "ADLB_SERVERS:    ${ADLB_SERVERS}"    >> ${LOG}
+TURBINE_ENGINES=$(( 1 )) # PROCS/128 ))
+ADLB_SERVERS=$((    1 )) # PROCS/128 ))
 
 # Turbine-specific environment
 env+=( TCLLIBPATH=${TCLLIBPATH}
-       DEBUG=0
-       LOGGING=0
+       DEBUG=1
+       LOGGING=1
        TURBINE_ENGINES=${TURBINE_ENGINES}
        ADLB_SERVERS=${ADLB_SERVERS}
 )
@@ -82,7 +79,7 @@ cqsub -n ${NODES} \
       -m vn \
       ${TCLSH} ${SCRIPT_NAME} | read JOB_ID
 
-if [[ ${JOB_ID} == "" ]] 
+if [[ ${JOB_ID} == "" ]]
 then
   print "cqsub failed!"
   exit 1
@@ -91,10 +88,10 @@ fi
 print "JOB: ${JOB_ID}"
 {
   print "JOB: ${JOB_ID}"
-  print "PROCS: ${PROCS}" 
-  print "SUBMITTED: $( date_nice )" 
-  print "TURBINE_ENGINES: ${TURBINE_ENGINES}" 
-  print "ADLB_SERVERS: ${ADLB_SERVERS}"
+  print "PROCS: ${PROCS}"
+  print "SUBMITTED: $( date_nice )"
+  print "TURBINE_ENGINES: ${TURBINE_ENGINES}"
+  print "ADLB_SERVERS:    ${ADLB_SERVERS}"
 } >> ${LOG}
 
 print ${JOB_ID} > ${JOB_ID_FILE}
