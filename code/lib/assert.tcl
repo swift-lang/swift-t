d46b0c135ab94cc9d0c3845795e78e218c5c29e4
Tim Armstrong <tim.g.armstrong@gmail.com>
2012-02-22 20:28:41 +0000

    add additional assertion ops for inequalities
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@2679 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/lib/assert.tcl
+++ code/lib/assert.tcl
@@ -45,4 +45,50 @@ namespace eval turbine {
             log "assertEqual: $arg1_value $arg2_value"
         }
     }
+    
+    # assertLT(arg1, arg2, msg)
+    # if arg1 >= arg2 according to TCL's comparison rules
+    # then crash, printing the two values and the provided msg
+    proc assertLT { stack noresult inputs } {
+        set arg1 [ lindex $inputs 0 ]
+        set arg2 [ lindex $inputs 1 ]
+        set msg [ lindex $inputs 2 ]
+        set rule_id [ rule_new ]
+        rule $rule_id "assertLT-$arg1-$arg2-$msg" "$arg1 $arg2 $msg" "" \
+            "tf: assertLT_body $arg1 $arg2 $msg"
+    }
+    
+    proc assertLT_body { arg1 arg2 msg } {
+        set arg1_value [ get $arg1 ]
+        set arg2_value [ get $arg2 ]
+        if { $arg1_value >= $arg2_value } {
+            set msg_value [ get $msg ]
+            error "Assertion failed $arg1_value >= $arg2_value: $msg_value"
+        } else {
+            log "assertLT: $arg1_value $arg2_value"
+        }
+    }
+    
+    # assertLTE(arg1, arg2, msg)
+    # if arg1 >= arg2 according to TCL's comparison rules
+    # then crash, printing the two values and the provided msg
+    proc assertLTE { stack noresult inputs } {
+        set arg1 [ lindex $inputs 0 ]
+        set arg2 [ lindex $inputs 1 ]
+        set msg [ lindex $inputs 2 ]
+        set rule_id [ rule_new ]
+        rule $rule_id "assertLTE-$arg1-$arg2-$msg" "$arg1 $arg2 $msg" "" \
+            "tf: assertLTE_body $arg1 $arg2 $msg"
+    }
+    
+    proc assertLTE_body { arg1 arg2 msg } {
+        set arg1_value [ get $arg1 ]
+        set arg2_value [ get $arg2 ]
+        if { $arg1_value > $arg2_value } {
+            set msg_value [ get $msg ]
+            error "Assertion failed $arg1_value > $arg2_value: $msg_value"
+        } else {
+            log "assertLTE: $arg1_value $arg2_value"
+        }
+    }
 }
