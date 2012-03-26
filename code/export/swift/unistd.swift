5eb476ef703ad5f260a5cd834302b9f104d72746
Justin Wozniak <wozniak@mcs.anl.gov>
2012-03-26 19:32:29 +0000

    Make argv_container() return boolean
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@3169 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/export/swift/unistd.swift
+++ code/export/swift/unistd.swift
@@ -5,7 +5,7 @@
 
 (int c)    argc()                 "turbine" "0.0.2" "argc_get";
 (string s) args()                 "turbine" "0.0.2" "args_get";
-(int b) argv_contains(string key) "turbine" "0.0.2" "argv_contains";
+(boolean b) argv_contains(string key) "turbine" "0.0.2" "argv_contains";
 argv_accept(string... keys)       "turbine" "0.0.2" "argv_accept";
 (string s) argv(string|int key, string... default_val)
 "turbine" "0.0.2" "argv_get";
