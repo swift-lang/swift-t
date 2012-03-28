f545643c773ec853dd89b997549bfed73728e19a
Tim Armstrong <tim.g.armstrong@gmail.com>
2012-03-28 18:43:51 +0000

    Got updating updateable variables working
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@3226 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/lib/updateable.tcl
+++ code/lib/updateable.tcl
@@ -2,54 +2,62 @@
 
 namespace eval turbine {
 
-    # initialise updateable variable o with value in future i
-    # for now, assume floats
-    proc init_updateable { stack o i } {
-        #TODO
-    }
+  # initialise updateable variable o with value in future i
+  # for now, assume floats
+  proc init_updateable { stack o i } {
+    #TODO
+  }
 
-    proc update_min { stack o inputs } {
-        set x [ lindex $inputs 0 ]
-        set val [ lindex $inputs 1 ]
-        rule "update_min-$x-$val" $val \
-            $turbine::LOCAL "update_min_body $x $val"
-    }
-    proc update_min_body { x val } {
-        set val2 [ get_float $val ]
-        update_min_impl $x $val2
-    }
+  proc update_min { stack o inputs } {
+    set x [ lindex $inputs 0 ]
+    set val [ lindex $inputs 1 ]
+    rule "update_min-$x-$val" $val \
+        $turbine::LOCAL "update_min_body $x $val"
+  }
+  proc update_min_body { x val } {
+    set val2 [ get_float $val ]
+    update_min_impl $x $val2
+  }
 
-    proc update_min_impl { x val } {
-        # TODO: update $x with tcl value $val
+  proc update_min_impl { x val } {
+    # TODO: this version has a race condition
+    set old [ turbine::get_float $x ]
+    if { $old > $val } {
+      adlb::store $x $adlb::FLOAT $val
     }
-    
-    proc update_incr { stack o inputs } {
-        set x [ lindex $inputs 0 ]
-        set val [ lindex $inputs 1 ]
-        rule "update_incr-$x-$val" $val \
-            $turbine::LOCAL "update_incr_body $x $val"
-    }
-    proc update_incr_body { x val } {
-        set val2 [ get_float $val ]
-        update_incr_impl $x $val2
-    }
+  }
+  
+  proc update_incr { stack o inputs } {
+    set x [ lindex $inputs 0 ]
+    set val [ lindex $inputs 1 ]
+    rule "update_incr-$x-$val" $val \
+        $turbine::LOCAL "update_incr_body $x $val"
+  }
+  proc update_incr_body { x val } {
+    set val2 [ get_float $val ]
+    update_incr_impl $x $val2
+  }
 
-    proc update_incr_impl { x val } {
-        # TODO: update $x with tcl value $val
-    }
+  proc update_incr_impl { x val } {
+    # TODO: this version has a race condition
+    set old [ turbine::get_float $x ]
+    adlb::store $x $adlb::FLOAT [ expr $val + $old ]
+  }
 
-    proc update_scale { stack o inputs } {
-        set x [ lindex $inputs 0 ]
-        set val [ lindex $inputs 1 ]
-        rule "update_scale-$x-$val" $val \
-            $turbine::LOCAL "update_scale_body $x $val"
-    }
-    proc update_scale_body { x val } {
-        set val2 [ get_float $val ]
-        update_scale_impl $x $val2
-    }
+  proc update_scale { stack o inputs } {
+    set x [ lindex $inputs 0 ]
+    set val [ lindex $inputs 1 ]
+    rule "update_scale-$x-$val" $val \
+        $turbine::LOCAL "update_scale_body $x $val"
+  }
+  proc update_scale_body { x val } {
+    set val2 [ get_float $val ]
+    update_scale_impl $x $val2
+  }
 
-    proc update_scale_impl { x val } {
-        # TODO: update $x with tcl value $val
-    }
+  proc update_scale_impl { x val } {
+    # TODO: this version has a race condition
+    set old [ turbine::get_float $x ]
+    adlb::store $x $adlb::FLOAT [ expr $val * $old ]
+  }
 }
