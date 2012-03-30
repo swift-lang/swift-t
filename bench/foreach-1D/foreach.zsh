4aa1112d3bcf44de714ad7c76a54d8cbed3d5171
Justin Wozniak <wozniak@mcs.anl.gov>
2012-03-30 17:23:22 +0000

    Add some statistics
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@3278 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- bench/foreach-1D/foreach.zsh
+++ bench/foreach-1D/foreach.zsh
@@ -1,5 +1,41 @@
 #!/bin/zsh
 
+# Benchmark parameters
+export TURBINE_ENGINES=1
+export ADLB_SERVERS=1
+TURBINE_WORKERS=1
+PROCS=$(( TURBINE_ENGINES + ADLB_SERVERS + TURBINE_WORKERS ))
+N=1000
+# Delay in milliseconds
+DELAY=0
+
+# System settings
+export TURBINE_DEBUG=0
+export ADLB_DEBUG=0
+export LOGGING=0
 export ADLB_EXHAUST_TIME=1
 export TURBINE_USER_LIB=$( cd ${PWD}/../util ; /bin/pwd )
-turbine -l -n 3 foreach.tcl --N=2 --delay=10
+
+START=$( date +%s )
+turbine -l -n ${PROCS} foreach.tcl --N=${N} --delay=${DELAY}
+STOP=$( date +%s )
+
+TIME=$(( STOP - START - ADLB_EXHAUST_TIME ))
+
+print "N: ${N} TIME: ${TIME}"
+
+if (( TIME ))
+then
+  TOTAL_RATE=$(( N / TIME ))
+  print "TOTAL_RATE: ${TOTAL_RATE}"
+  WORKER_RATE=$(( N / TIME / TURBINE_WORKERS ))
+  print "WORKER_RATE: ${WORKER_RATE}"
+fi
+
+if (( ${DELAY} ))
+then
+  WORK_TIME=$(( N * DELAY/1000 ))
+  TOTAL_TIME=$(( TIME * TURBINE_WORKERS ))
+  UTIL=$(( WORK_TIME / TOTAL_TIME ))
+  print "UTIL: ${UTIL}"
+fi
