def5acaf4fb6ae3b3e9f41b4e7a6c9a8f236751c
Justin Wozniak <wozniak@mcs.anl.gov>
2012-04-04 19:40:37 +0000

    Test fix
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@3311 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/test/string_split.sh
+++ code/test/string_split.sh
@@ -11,7 +11,9 @@ set -x
 bin/turbine -l -n ${PROCS} ${SCRIPT} >> ${OUTPUT} 2>&1
 [[ ${?} == 0 ]] || exit 1
 
-grep -q ".*8.*evil name" ${OUTPUT} || exit 1
-grep -q ".*9.*/usr/bin" ${OUTPUT} || exit 1
+grep -q  "set:.*evil name"           ${OUTPUT} || exit 1
+grep -q  "set:.*/usr/bin"            ${OUTPUT} || exit 1
+# Ensure these do not end up together:
+grep -vq "set:.*evil name.*/usr/bin" ${OUTPUT} || exit 1
 
 exit 0
