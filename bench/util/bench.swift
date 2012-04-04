f412176090ff65345e713fced9d37cc474a5a356
Justin Wozniak <wozniak@mcs.anl.gov>
2012-04-04 21:46:13 +0000

    Add set1_integer
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@3319 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- bench/util/bench.swift
+++ bench/util/bench.swift
@@ -2,3 +2,4 @@
 // Benchmarking utility functions
 
 (float o) set1_float(float i) "bench" "0.0.1" "set1_float";
+(int o) set1_integer(int i) "bench" "0.0.1" "set1_integer";
--- bench/util/bench.tcl
+++ bench/util/bench.tcl
@@ -17,8 +17,21 @@ namespace eval bench {
     }
 
     proc set1_float_body { result delay } {
-        set delay_value [ get $delay ]
+        set delay_value [ get_float $delay ]
         after [ expr round($delay_value) ]
+        set_integer $result 1
+    }
+
+    # usage: set1_integer no_stack result delay
+    # delay in milliseconds: rounded to nearest whole millisecond
+    proc set1_integer { stack result delay } {
+        turbine::rule "set1-$result" $delay $turbine::WORK \
+            "bench::set1_integer_body $result $delay"
+    }
+
+    proc set1_integer_body { result delay } {
+        set delay_value [ get_integer $delay ]
+        after $delay_value
         set_integer $result 1
     }
 }
