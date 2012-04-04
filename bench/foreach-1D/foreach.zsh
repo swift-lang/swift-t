9a912293262132651d233396503d143c8db6fe8e
Justin Wozniak <wozniak@mcs.anl.gov>
2012-04-04 21:18:03 +0000

    Sketch for Cobalt: does not work due to Cobalt --env syntax issue...
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@3314 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- bench/foreach-1D/foreach.zsh
+++ bench/foreach-1D/foreach.zsh
@@ -1,17 +1,34 @@
 #!/bin/zsh
 
+PROGRAM_SWIFT="foreach.swift"
+PROGRAM_TCL=${PROGRAM_SWIFT%.swift}.tcl
+
 # Benchmark parameters
 export TURBINE_ENGINES=1
 export ADLB_SERVERS=1
 TURBINE_WORKERS=1
-PROCS=$(( TURBINE_ENGINES + ADLB_SERVERS + TURBINE_WORKERS ))
 N=1000
 # Delay in milliseconds
 DELAY=0
 
+PROCS=$(( TURBINE_ENGINES + ADLB_SERVERS + TURBINE_WORKERS ))
+
 # Load common features
+
+TURBINE=$( which turbine )
+if [[ ${TURBINE} == "" ]]
+then
+  print "turbine not found!"
+  exit 1
+fi
+
+set -x
+
+TURBINE_HOME=$( cd $( dirname ${TURBINE} )/.. ; /bin/pwd )
+source ${TURBINE_HOME}/scripts/helpers.zsh
 BENCH_UTIL=$( cd $( dirname $0 )/../util ; /bin/pwd )
 source ${BENCH_UTIL}/tools.zsh
+exitcode
 
 # System settings
 export TURBINE_DEBUG=0
@@ -21,13 +38,33 @@ export ADLB_EXHAUST_TIME=1
 export TURBINE_USER_LIB=${BENCH_UTIL}
 
 # Run stc if necessary
-compile foreach.swift foreach.tcl
+compile ${PROGRAM_SWIFT} ${PROGRAM_TCL}
 
 START=$( date +%s )
-turbine -l -n ${PROCS} foreach.tcl --N=${N} --delay=${DELAY}
+
+# MODE MPIEXEC
+# OUTPUT="output.txt"
+# turbine -l -n ${PROCS} foreach.tcl --N=${N} --delay=${DELAY} >& ${OUTPUT}
+
+# MODE COBALT
+
+# LAUNCH IT
+${TURBINE_COBALT} -n ${PROCS} ${PROGRAM_TCL}
+exitcode "turbine-cobalt failed!"
+
 STOP=$( date +%s )
 
-TIME=$(( STOP - START - ADLB_EXHAUST_TIME ))
+read OUTPUT_DIR < output.txt
+OUTPUT=$( ls ${OUTPUT_DIR}/*.output )
+TIME=$( turbine_stats_walltime ${OUTPUT} )
+if [[ ${TIME} == "" ]]
+then
+  print "run failed!"
+  exit 1
+fi
+
+TOOK=$(( STOP - START ))
+# print "TOOK: ${TOOK}"
 
 print "N: ${N} TIME: ${TIME}"
 
