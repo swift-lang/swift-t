c18004e8079fd85588d199f5ea0c58103dad8a5c
Justin Wozniak <wozniak@mcs.anl.gov>
2012-04-05 18:00:05 +0000

    Usage notes; better output handling; use qsub cqsub deprecated by Cobalt team
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@3329 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/scripts/submit/bgp/turbine-cobalt.zsh
+++ code/scripts/submit/bgp/turbine-cobalt.zsh
@@ -1,25 +1,37 @@
 #!/bin/zsh
 
+# usage:
+#  turbine-cobalt -n <PROCS> [-e <ENV>]* [-o <OUTPUT>] -t <WALLTIME>
+#                 <SCRIPT> [<ARG>]*
+
 # Variables that may have defaults set in the environment:
 # QUEUE, TURBINE_OUTPUT_ROOT, PROJECT
 
 TURBINE_HOME=$( cd $( dirname $0 )/../../.. ; /bin/pwd )
-print "TURBINE_HOME: ${TURBINE_HOME}"
+declare TURBINE_HOME
 source ${TURBINE_HOME}/scripts/turbine-config.sh
 
 # Defaults:
 PROCS=0
 WALLTIME="00:15:00"
+TURBINE_OUTPUT_ROOT=${HOME}/turbine-output
+
+# Place to store output directory name
+OUTPUT_TOKEN_FILE=output.txt
 
 # Job environment
 typeset -T ENV env
 env=()
 
-while getopts "e:n:o:t:" OPTION
+while getopts "d:e:n:o:t:" OPTION
  do
  case ${OPTION}
    in
+   d)
+     OUTPUT_TOKEN_FILE=${OPTARG}
+     ;;
    e) env+=${OPTARG}
+     print a: $OPTARG
      ;;
    n) PROCS=${OPTARG}
      ;;
@@ -36,19 +48,22 @@ done
 shift $(( OPTIND-1 ))
 
 SCRIPT=$1
+shift
+ARGS=${*}
+
 START=$( date +%s )
 
 checkvars QUEUE SCRIPT
 
-if [[ ${TURBINE_OUTPUT_ROOT} == "" ]]
-then
-  TURBINE_OUTPUT_ROOT=${HOME}/turbine-output
-fi
+[[ ${PROCS} != 0 ]]
+exitcode "PROCS==0"
 
 RUN=$( date_path )
 
 TURBINE_OUTPUT=${TURBINE_OUTPUT_ROOT}/${RUN}
 print "TURBINE_OUTPUT: ${TURBINE_OUTPUT}"
+declare OUTPUT_TOKEN_FILE
+print ${TURBINE_OUTPUT} > ${OUTPUT_TOKEN_FILE}
 mkdir -pv ${TURBINE_OUTPUT}
 
 LOG=${TURBINE_OUTPUT}/log.txt
@@ -59,25 +74,31 @@ SCRIPT_NAME=$( basename ${SCRIPT} )
 
 JOB_ID_FILE=${TURBINE_OUTPUT}/jobid.txt
 
-TURBINE_ENGINES=$(( 1 )) # PROCS/128 ))
-ADLB_SERVERS=$((    1 )) # PROCS/128 ))
+source ${TURBINE_HOME}/scripts/turbine-config.sh
+exitcode "turbine-config.sh failed!"
 
 # Turbine-specific environment
-env+=( TCLLIBPATH=${TCLLIBPATH}
-       DEBUG=1
-       LOGGING=1
+env+=( TCLLIBPATH="${TCLLIBPATH}"
+       TURBINE_DEBUG=${TURBINE_DEBUG:-1}
+       ADLB_DEBUG=${ADLB_DEBUG:-1}
+       LOGGING=${LOGGING:-1}
        TURBINE_ENGINES=${TURBINE_ENGINES}
        ADLB_SERVERS=${ADLB_SERVERS}
+       ADLB_EXHAUST_TIME=${ADLB_EXHAUST_TIME:-5}
 )
 
+declare TCLSH SCRIPT_NAME
+
 NODES=$(( PROCS/4 ))
-cqsub -n ${NODES} \
-      -t ${WALLTIME} \
-      -q ${QUEUE} \
-      -C ${TURBINE_OUTPUT} \
-      -e ${ENV} \
-      -m vn \
-      ${TCLSH} ${SCRIPT_NAME} | read JOB_ID
+(( PROCS % 4 )) && (( NODES++ ))
+declare NODES
+qsub -n ${NODES} \
+     -t ${WALLTIME} \
+     -q ${QUEUE} \
+     --cwd ${TURBINE_OUTPUT} \
+     --env "${ENV}" \
+     --mode vn \
+      ${TCLSH} ${SCRIPT} ${ARGS} | read JOB_ID
 
 if [[ ${JOB_ID} == "" ]]
 then
@@ -85,7 +106,7 @@ then
   exit 1
 fi
 
-print "JOB: ${JOB_ID}"
+declare JOB_ID
 {
   print "JOB: ${JOB_ID}"
   print "PROCS: ${PROCS}"
