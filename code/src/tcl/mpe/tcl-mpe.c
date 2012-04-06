76ccf1d62430445ad0e69daa74d16f795fd39856
Justin Wozniak <wozniak@mcs.anl.gov>
2012-04-06 19:55:30 +0000

    Tcl MPE bindings and test
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@3362 dc4e9af1-7f46-4ead-bba6-71afc04862de

---
+++ code/src/tcl/mpe/tcl-mpe.c
@@ -0,0 +1,126 @@
+
+/**
+ * Tcl extension for MPE
+ *
+ * @author wozniak
+ * */
+
+#include <assert.h>
+
+#include <tcl.h>
+#include <mpe.h>
+#include <adlb.h>
+
+#include "src/tcl/util.h"
+#include "src/util/debug.h"
+
+/*
+static int
+MPE_Get_IDs_Cmd(ClientData cdata, Tcl_Interp *interp,
+                int objc, Tcl_Obj *const objv[])
+{
+  TCL_ARGS(1);
+
+  int n1, n2;
+
+  MPE_Log_get_state_eventIDs(&n1, &n2);
+
+  Tcl_Obj* items[2];
+  items[0] = Tcl_NewIntObj(n1);
+  items[1] = Tcl_NewIntObj(n2);
+  Tcl_Obj* result = Tcl_NewListObj(2, items);
+  Tcl_SetObjResult(interp, result);
+  return TCL_OK;
+}
+
+static int
+MPE_Describe_Cmd(ClientData cdata, Tcl_Interp *interp,
+                 int objc, Tcl_Obj *const objv[])
+{
+  TCL_ARGS(5);
+
+  int n1, n2;
+  char* state;
+  char* color;
+  int error;
+  error = Tcl_GetIntFromObj(interp, objv[1], &n1);
+  TCL_CHECK(error);
+  error = Tcl_GetIntFromObj(interp, objv[2], &n2);
+  TCL_CHECK(error);
+  state = Tcl_GetStringFromObj(objv[3], NULL);
+  assert(state);
+  color = Tcl_GetStringFromObj(objv[4], NULL);
+  assert(color);
+
+  MPE_Describe_state(n1, n2, state, color);
+
+  return TCL_OK;
+}
+*/
+
+static const char* MPE_CHOOSE_COLOR = "MPE_CHOOSE_COLOR";
+
+static int
+MPE_Create_Cmd(ClientData cdata, Tcl_Interp *interp,
+               int objc, Tcl_Obj *const objv[])
+{
+  TCL_ARGS(2);
+  char* state = Tcl_GetStringFromObj(objv[1], NULL);
+  assert(state);
+
+  int event1, event2;
+  MPE_Log_get_state_eventIDs(&event1, &event2);
+  MPE_Describe_state(event1, event2, state, MPE_CHOOSE_COLOR);
+
+  Tcl_Obj* items[2];
+  items[0] = Tcl_NewIntObj(event1);
+  items[1] = Tcl_NewIntObj(event2);
+  Tcl_Obj* result = Tcl_NewListObj(2, items);
+  Tcl_SetObjResult(interp, result);
+  return TCL_OK;
+}
+
+static int
+MPE_Log_Cmd(ClientData cdata, Tcl_Interp *interp,
+        int objc, Tcl_Obj *const objv[])
+{
+  TCL_CONDITION((objc == 2 || objc == 3),
+                "requires 1 or 2 args!");
+
+  int event;
+  int error = Tcl_GetIntFromObj(interp, objv[1], &event);
+  TCL_CHECK(error);
+
+  char* bytes = NULL;
+  if (objc == 3)
+    bytes = Tcl_GetStringFromObj(objv[2], NULL);
+
+  MPE_Log_event(event, 0, bytes);
+  return TCL_OK;
+}
+
+/**
+   Shorten object creation lines.  mpe:: namespace is prepended
+ */
+#define COMMAND(tcl_function, c_function) \
+    Tcl_CreateObjCommand(interp, "mpe::" tcl_function, c_function, \
+                         NULL, NULL);
+/**
+   Called when Tcl loads this extension
+ */
+int DLLEXPORT
+Tclmpe_Init(Tcl_Interp *interp)
+{
+  if (Tcl_InitStubs(interp, TCL_VERSION, 0) == NULL)
+    return TCL_ERROR;
+
+  if (Tcl_PkgProvide(interp, "MPE", "0.1") == TCL_ERROR)
+    return TCL_ERROR;
+
+  //  COMMAND("get_ids", MPE_Get_IDs_Cmd);
+  // COMMAND("describe", MPE_Describe_Cmd);
+  COMMAND("create", MPE_Create_Cmd);
+  COMMAND("log", MPE_Log_Cmd);
+
+  return TCL_OK;
+}
---
+++ code/test/mpe-3.tcl
@@ -0,0 +1,56 @@
+
+# Flex ADLB/MPE in Tcl
+# No real Turbine data flow here
+
+# This may be used as a benchmark by setting
+# TURBINE_TEST_PARAM_1 in the environment
+
+package require turbine 0.0.1
+
+namespace import turbine::string_*
+
+turbine::defaults
+turbine::init $engines $servers
+
+if { [ info exists env(TURBINE_TEST_PARAM_1) ] } {
+    set iterations $env(TURBINE_TEST_PARAM_1)
+} else {
+    set iterations 4
+}
+
+# Define MPE events
+set L [ mpe::create "test_mpe-3" ]
+set event1 [ lindex $L 0 ]
+set event2 [ lindex $L 1 ]
+
+if { ! [ adlb::amserver ] } {
+
+    set rank [ adlb::rank ]
+    puts "rank: $rank"
+    set workers [ adlb::workers ]
+    if { $rank == 0 } {
+	puts "iterations: $iterations"
+        puts "events $event1 $event2"
+    }
+    puts "workers:    $workers"
+
+    for { set i 1 } { $i <= $iterations } { incr i } {
+
+        mpe::log $event1 "start-loop-body"
+        set id [ expr $rank + $i * ($workers + 1) ]
+        adlb::create $id $adlb::STRING
+        adlb::store $id $adlb::STRING "message rank:$rank:$i"
+        adlb::close $id
+
+        # turbine::c::debug "get"
+        set msg [ adlb::retrieve $id ]
+        # turbine::c::debug "got: $msg"
+        mpe::log $event2 "end-loop-body"
+    }
+} else {
+    adlb::server
+}
+
+turbine::finalize
+
+puts OK
