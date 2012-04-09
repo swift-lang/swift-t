bf09b0244061820e4df42c010bd682770d00e378
Tim Armstrong <tim.g.armstrong@gmail.com>
2012-04-09 17:09:34 +0000

    fix type error in test
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@3373 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- tests/420-pow.swift
+++ tests/420-pow.swift
@@ -1,14 +1,12 @@
 
 #include <builtins.swift>
 
-// THIS-TEST-DOES-NOT-WORK (#199)
-
 main
 {
   int n = 3;
   int d = 2;
 
-  int T = pow_integer(n,d);
+  int T = floor(pow_integer(n,d));
 
   foreach i in [0:T-1]
   {
