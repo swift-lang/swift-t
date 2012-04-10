a8f1c2913ee36cc9f752cbe8ea7b4ad58cc0dff7
Justin Wozniak <wozniak@mcs.anl.gov>
2012-04-10 15:11:10 +0000

    Use set1rA, set1rB
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@3396 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- bench/util/bench.swift
+++ bench/util/bench.swift
@@ -3,4 +3,5 @@
 
 (float o) set1_float(float i) "bench" "0.0.1" "set1_float";
 (int o) set1_integer(int i) "bench" "0.0.1" "set1_integer";
-(int o) set1r_integer(int i) "bench" "0.0.1" "set1r_integer";
+(int o) set1rA_integer(int i) "bench" "0.0.1" "set1rA_integer";
+(int o) set1rB_integer(int i) "bench" "0.0.1" "set1rB_integer";
--- bench/util/bench.tcl
+++ bench/util/bench.tcl
@@ -13,20 +13,29 @@ namespace eval bench {
     variable mpe_ready
 
     # MPE event IDs
-    variable set1r_event_start
-    variable set1r_event_stop
+    variable set1rA_event_start
+    variable set1rA_event_stop
+    variable set1rB_event_start
+    variable set1rB_event_stop
 
     proc mpe_setup { } {
 
         variable mpe_ready
-        variable set1r_event_start
-        variable set1r_event_stop
+        variable set1rA_event_start
+        variable set1rA_event_stop
+        variable set1rB_event_start
+        variable set1rB_event_stop
 
         puts CHECK
         if { ! [ info exists mpe_ready ] } {
-            set L [ mpe::create "set1r" ]
-            set set1r_event_start [ lindex $L 0 ]
-            set set1r_event_stop  [ lindex $L 1 ]
+            set L [ mpe::create "set1rA" ]
+            set set1rA_event_start [ lindex $L 0 ]
+            set set1rA_event_stop  [ lindex $L 1 ]
+
+            set L [ mpe::create "set1rB" ]
+            set set1rB_event_start [ lindex $L 0 ]
+            set set1rB_event_stop  [ lindex $L 1 ]
+
             set mpe_ready 1
             puts OK
         }
@@ -60,23 +69,45 @@ namespace eval bench {
 
     # usage: set1_integer no_stack result delay
     # delay in milliseconds: rounded to nearest whole millisecond
-    proc set1r_integer { stack result delay } {
+    proc set1rA_integer { stack result delay } {
         turbine::rule "set1r-$result" $delay $turbine::WORK \
-            "bench::set1r_integer_body $result $delay"
+            "bench::set1rA_integer_body $result $delay"
     }
 
-    proc set1r_integer_body { result delay } {
+    proc set1rA_integer_body { result delay } {
 
         mpe_setup
-        variable set1r_event_start
-        variable set1r_event_stop
+        variable set1rA_event_start
+        variable set1rA_event_stop
 
         set delay_value [ get_integer $delay ]
         # randomized delay value:
-        mpe::log $set1r_event_start
+        mpe::log $set1rA_event_start
         set rdv [ expr rand() * $delay_value ]
         after [ expr round($rdv) ]
-        mpe::log $set1r_event_stop
+        mpe::log $set1rA_event_stop
+        set_integer $result 1
+    }
+
+    # usage: set1_integer no_stack result delay
+    # delay in milliseconds: rounded to nearest whole millisecond
+    proc set1rB_integer { stack result delay } {
+        turbine::rule "set1r-$result" $delay $turbine::WORK \
+            "bench::set1rB_integer_body $result $delay"
+    }
+
+    proc set1rB_integer_body { result delay } {
+
+        mpe_setup
+        variable set1rB_event_start
+        variable set1rB_event_stop
+
+        set delay_value [ get_integer $delay ]
+        # randomized delay value:
+        mpe::log $set1rB_event_start
+        set rdv [ expr rand() * $delay_value ]
+        after [ expr round($rdv) ]
+        mpe::log $set1rB_event_stop
         set_integer $result 1
     }
 }
