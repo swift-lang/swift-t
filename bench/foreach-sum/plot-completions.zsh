30a4a8cc87bf4bab0d0ebd1db04fa9942e1e61a0
Justin Wozniak <wozniak@mcs.anl.gov>
2012-04-12 19:36:13 +0000

    Progress output
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@3474 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- bench/foreach-sum/plot-completions.zsh
+++ bench/foreach-sum/plot-completions.zsh
@@ -9,11 +9,14 @@ RANK=-1
 export MPE_EVENTS="set1rA"
 ${TIME_COMPLETIONS} ${CLOG} ${RANK} > cA.data
 [[ ${?} == 0 ]] || { print ERROR ; return 1 }
+printf OK
 
 export MPE_EVENTS="set1rB"
 ${TIME_COMPLETIONS} ${CLOG} ${RANK} > cB.data
+printf .
 
 export MPE_EVENTS="sum"
 ${TIME_COMPLETIONS} ${CLOG} ${RANK} > cS.data
+print .
 
 swift_plotter.zsh -s foreach-sum.{cfg,eps} c[ABS].data
