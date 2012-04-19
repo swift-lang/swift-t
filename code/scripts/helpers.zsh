7594e985027e6d86427995af9ac43a2edada8796
Justin Wozniak <wozniak@mcs.anl.gov>
2012-04-19 21:44:52 +0000

    Move helper function here
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@3596 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/scripts/helpers.zsh
+++ code/scripts/helpers.zsh
@@ -98,6 +98,20 @@ nanos()
   date +%s.%N
 }
 
+compile()
+{
+  SWIFT=$1
+  TCL=$2
+  shift 2
+
+  if [[ ! -f ${TCL} || ${SWIFT} -nt ${TCL} ]]
+  then
+    stc ${*} ${SWIFT} ${TCL}
+    return ${?}
+  fi
+  return 0
+}
+
 within()
 {
   local TIME=$1
