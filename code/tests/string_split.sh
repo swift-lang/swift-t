11cdd8a912e8265839a8f95134efdda508bd6e72
Justin Wozniak <wozniak@mcs.anl.gov>
2012-05-21 15:49:51 +0000

    Fix test
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@4130 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/tests/string_split.sh
+++ code/tests/string_split.sh
@@ -11,9 +11,9 @@ set -x
 bin/turbine -l -n ${PROCS} ${SCRIPT} >> ${OUTPUT} 2>&1
 [[ ${?} == 0 ]] || exit 1
 
-grep -q  "set:.*evil name"           ${OUTPUT} || exit 1
-grep -q  "set:.*/usr/bin"            ${OUTPUT} || exit 1
+grep -q  "store:.*evil name"           ${OUTPUT} || exit 1
+grep -q  "store:.*/usr/bin"            ${OUTPUT} || exit 1
 # Ensure these do not end up together:
-grep -vq "set:.*evil name.*/usr/bin" ${OUTPUT} || exit 1
+grep -vq "store:.*evil name.*/usr/bin" ${OUTPUT} || exit 1
 
 exit 0
