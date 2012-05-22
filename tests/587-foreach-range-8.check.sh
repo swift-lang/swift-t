fde6828e07ebd54e96e765e6669c24affbb63e3d
Tim Armstrong <tim.g.armstrong@gmail.com>
2012-05-22 02:18:00 +0000

    Fix check script to handle MPI output with extra logging info
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@4140 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- tests/587-foreach-range-8.check.sh
+++ tests/587-foreach-range-8.check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-COUNT=`grep "trace: " ${TURBINE_OUTPUT} | wc -l`
+COUNT=`grep -E '(\[[0-9]*\])? trace: [0-9]+$' ${TURBINE_OUTPUT} | wc -l`
 if [ ${COUNT} -ne 100 ]; then
     echo "Expected 100 trace statements in ${TURBINE_OUTPUT}, but only saw ${COUNT}"
     exit 1
