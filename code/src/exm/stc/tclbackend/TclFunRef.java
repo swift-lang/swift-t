6db37cdde6b3c4c4bce448ad18ea1a1464de2721
Tim Armstrong <tim.g.armstrong@gmail.com>
2012-06-15 22:10:44 +0000

    Add missing source file
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@4252 dc4e9af1-7f46-4ead-bba6-71afc04862de

---
+++ code/src/exm/stc/tclbackend/TclFunRef.java
@@ -0,0 +1,11 @@
+package exm.stc.tclbackend;
+
+class TclFunRef {
+  public final String pkg;
+  public final String symbol;
+  public TclFunRef(String pkg, String symbol) {
+    super();
+    this.pkg = pkg;
+    this.symbol = symbol;
+  }
+}
