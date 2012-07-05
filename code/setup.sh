21cb1a07f9522a7fe72a2674a805f9a1a706d91c
Justin Wozniak <wozniak@mcs.anl.gov>
2012-07-05 16:39:13 +0000

    More feedback
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@4363 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/setup.sh
+++ code/setup.sh
@@ -7,7 +7,10 @@ crash()
   exit 1
 }
 
+echo "Running autoconf..."
 autoconf   || crash "autoconf failed!"
+echo "Running autoheader..."
 autoheader || crash "autoheader failed!"
+echo "turbine: setup.sh: OK"
 
 exit 0
