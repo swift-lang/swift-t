8e79fa6d732b9e10b206c7b569b0e295fad0b928
Justin Wozniak <wozniak@mcs.anl.gov>
2012-08-14 18:44:44 +0000

    Better configure checks; new USE_TCLSH_LOCAL (for knight)
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@4538 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/maint/find-tcl.zsh
+++ code/maint/find-tcl.zsh
@@ -1,33 +1,39 @@
-#!/bin/zsh
+#!/usr/bin/env zsh
 
 # Find working Tcl 8.5
-# Currently used by Makefile
+# Currently used by configure
 
 DIR=$1
 
 source scripts/helpers.zsh
 if [[ ${?} != 0 ]]
 then
-  print "Could not source helpers.zsh!"
+  print "\n Could not source helpers.zsh!"
   exit 1
 fi
 
 [[ -n ${DIR} ]]
-exitcode "Not given: Tcl directory" > /dev/stderr
+exitcode "\n Not given: Tcl directory" > /dev/stderr
 
 [[ -d ${DIR} ]]
-exitcode "Could not find Tcl directory: ${DIR}" > /dev/stderr
+exitcode "\n Could not find Tcl directory: ${DIR}" > /dev/stderr
 
 FILES=( ${DIR}/bin/tclsh8.5 ${DIR}/bin/tclsh )
 for F in ${FILES}
 do
   if [[ -x ${F} ]]
   then
-    # Run and get Tcl version
-    VERSION=$( print 'puts $tcl_version' | ${F} )
-    exitcode "Could not run: ${F}"
-    if [[ ${VERSION} == "8.5" ]]
+    if [[ ${NO_RUN} == 1 ]]
     then
+      # Skip trying to run: we are cross-compiling
+      VERSION=8.5
+    else
+      # Run and get Tcl version
+      VERSION=$( print 'puts $tcl_version' | ${F} )
+      exitcode "\n Could not run: ${F}"
+    fi
+    if [[ ${VERSION} == "8.5" ]]
+      then
       print ${F}
       return 0
     fi
