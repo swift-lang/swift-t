9190877557927211c7dfb2696a4e2fd3c6eafbf6
Justin Wozniak <wozniak@mcs.anl.gov>
2012-09-05 20:05:01 +0000

    More robust test check
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@4744 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/tests/container-enumerate.sh
+++ code/tests/container-enumerate.sh
@@ -11,9 +11,13 @@ set -x
 bin/turbine -l -n ${PROCS} ${SCRIPT} >> ${OUTPUT} 2>&1
 [[ ${?} == 0 ]] || exit 1
 
-grep -q "subscripts: 0 1" ${OUTPUT} || exit 1
-grep -q "members: 3 4"    ${OUTPUT} || exit 1
-grep -q "dict: 0 3 1 4"   ${OUTPUT} || exit 1
-grep -q "count: 2"   ${OUTPUT} || exit 1
+# Read member TDs to search for them later
+M1=$( sed -n '/.*member1:.*/{s/.*member1: \(.*\)/\1/;p}' ${OUTPUT} )
+M2=$( sed -n '/.*member2:.*/{s/.*member2: \(.*\)/\1/;p}' ${OUTPUT} )
+
+grep -q "subscripts: 0 1"       ${OUTPUT} || exit 1
+grep -q "members: ${M1} ${M2}"  ${OUTPUT} || exit 1
+grep -q "dict: 0 ${M1} 1 ${M2}" ${OUTPUT} || exit 1
+grep -q "count: 2"              ${OUTPUT} || exit 1
 
 exit 0
