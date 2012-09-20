db178757913c122673d3036e8064e86b9215d95f
Justin Wozniak <wozniak@mcs.anl.gov>
2012-09-20 21:02:33 +0000

    Fix debug environment variables
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/lb/branches/exps@4893 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/src/debug.c
+++ code/src/debug.c
@@ -11,19 +11,25 @@ void
 debug_check_environment()
 {
   char* v;
-  v = getenv("ADLB_DEBUG");
-  if (v == NULL)
-    return;
-  if (strcmp(v, "0") == 0)
-    xlb_debug_enabled = false;
-  else
-    xlb_debug_enabled = true;
 
   v = getenv("ADLB_TRACE");
-  if (v == NULL)
-    return;
-  if (strcmp(v, "0") == 0)
-    xlb_trace_enabled = false;
-  else
-    xlb_trace_enabled = true;
+  if (v != NULL)
+  {
+    if (strcmp(v, "0") == 0)
+      xlb_trace_enabled = false;
+    else
+      xlb_trace_enabled = true;
+  }
+
+  v = getenv("ADLB_DEBUG");
+  if (v != NULL)
+  {
+    if (strcmp(v, "0") == 0)
+    {
+      xlb_debug_enabled = false;
+      xlb_trace_enabled = false;
+    }
+    else
+      xlb_debug_enabled = true;
+  }
 }
