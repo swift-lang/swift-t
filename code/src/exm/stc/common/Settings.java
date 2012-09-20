70aa2b64e4c9c99086104151a51d4fabbe70040d
Tim Armstrong <tim.g.armstrong@gmail.com>
2012-09-20 19:51:41 +0000

    fix handling of stc optimisation levels
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@4885 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/src/exm/stc/common/Settings.java
+++ code/src/exm/stc/common/Settings.java
@@ -60,7 +60,7 @@ public class Settings
     defaults.setProperty(OPT_UNROLL_LOOPS, "true");
     defaults.setProperty(OPT_DISABLE_ASSERTS, "false");
     defaults.setProperty(OPT_FORWARD_DATAFLOW, "true");
-    defaults.setProperty(OPT_WAIT_COALESCE, "true");
+    defaults.setProperty(OPT_WAIT_COALESCE, "false");
     defaults.setProperty(OPT_CONTROLFLOW_FUSION, "true");
     defaults.setProperty(OPT_NUM_PASSES, "5");
     defaults.setProperty(ARRAY_REF_SWITCHEROO, "false");
