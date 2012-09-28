c7215065677d8f205d8dde48928abd98a1cb014b
Tim Armstrong <tim.g.armstrong@gmail.com>
2012-09-28 23:03:00 +0000

    Filling in logic for app function generation.
    Not tested or operational yet, but doesn't break anything else
    (ie. tests still pass).
    
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@4969 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/lib/files.tcl
+++ code/lib/files.tcl
@@ -3,30 +3,30 @@
 namespace eval turbine {
 
     # Extract file status future from handle
-    proc _filestatus { file_handle } {
+    proc get_file_status { file_handle } {
       return [ lindex $file_handle 0 ]
     }
 
     # Extract filename future from handle
-    proc _filepath { file_handle } {
+    proc get_file_path { file_handle } {
       return [ lindex $file_handle 1 ]
     }
 
     # return tcl bool value
-    proc _is_mapped { file_handle } {
+    proc is_file_mapped { file_handle } {
       return [ lindex $file_handle 2 ]
     }
 
     # get the filename from the file handle
     proc filename2 { stack out in } {
       set file_handle [ lindex $in 0 ]
-      copy_string NOSTACK $out [ _filepath $file_handle ]
+      copy_string NOSTACK $out [ get_file_path $file_handle ]
     }
 
     proc input_file { stack out filepath } {
       set outfile [ lindex $out 0 ]
       puts "\[ $outfile \] $filepath"
-      set mapped [ _is_mapped $outfile ]
+      set mapped [ is_file_mapped $outfile ]
       puts $mapped
       if { $mapped } {
         error "file \[ $outfile \] was already mapped, cannot use input_file"
@@ -44,8 +44,8 @@ namespace eval turbine {
       if { ! [ file exists $filepath_val ] } {
         error "input_file: file $filepath_val does not exist"
       }
-      store_string [ _filepath $outfile ] $filepath_val
-      store_void [ _filestatus $outfile ]
+      store_string [ get_file_path $outfile ] $filepath_val
+      store_void [ get_file_status $outfile ]
     }
 
     # initialise an unmapped file to a temporary location
@@ -53,13 +53,13 @@ namespace eval turbine {
     # assigns the filename future in the file_handle, and also
     # returns the chosen filename
     proc init_unmapped { file_handle } {
-      set mapped [ _is_mapped $file_handle ]
+      set mapped [ is_file_mapped $file_handle ]
       if { $mapped } {
         error "argument error: called init_unmapped on mapped file:
               <$file_handle>"
       }
       set filename [ mktemp ]
-      store_string [ _filepath $file_handle ] $filename
+      store_string [ get_file_path $file_handle ] $filename
       return $filename
     }
 
@@ -69,32 +69,32 @@ namespace eval turbine {
       set dst [ lindex $outputs 0 ]
       set src [ lindex $inputs 0 ]
       puts "dst: $dst"
-      set mapped [ _is_mapped $dst ]
+      set mapped [ is_file_mapped $dst ]
       if { $mapped } {
         # is mapped, so need to make sure that file is at mapped path
-        set dstpath [ _filepath $dst ]
-        set srcpath [ _filepath $src ]
-        set srcstatus [ _filestatus $src ]
+        set dstpath [ get_file_path $dst ]
+        set srcpath [ get_file_path $src ]
+        set srcstatus [ get_file_status $src ]
         rule "copy_file-$dst-$src" "$dstpath $srcpath $srcstatus" \
             $turbine::WORK [ list copy_file_body $dst $src ]
       } else {
         # not mapped.  As shortcut, just make them both point to the 
         # same file and update status once src file is closed
-        copy_void NO_STACK [ _filestatus $dst ] [ _filestatus $src ]
-        copy_string NO_STACK [ _filepath $dst ] [ _filepath $src ]
+        copy_void NO_STACK [ get_file_status $dst ] [ get_file_status $src ]
+        copy_string NO_STACK [ get_file_path $dst ] [ get_file_path $src ]
       }
     }
 
     proc copy_file_body { dst src } {
-      set dstpath [ _filepath $dst ]
+      set dstpath [ get_file_path $dst ]
       set dstpath_val [ retrieve_string $dstpath ]
-      set srcpath [ _filepath $src ]
+      set srcpath [ get_file_path $src ]
       set srcpath_val [ retrieve_string $srcpath ]
       # do the copy: srcpath to dstpath
       # TODO: is this the best way to do this?
       file copy -force $srcpath_val $dstpath_val
       # signal that output is now available
-      store_void [ _filestatus $dst ]
+      store_void [ get_file_status $dst ]
     }
 
     # return the filename of a unique temporary file
@@ -103,7 +103,7 @@ namespace eval turbine {
     }
 
     proc close_file { handle } {
-      store_void [ _filestatus $handle ]  
+      store_void [ get_file_status $handle ]  
     }
 
 }
