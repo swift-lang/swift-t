68e08e43f68dcb1b5819442297683f860c75c87b
Justin Wozniak <wozniak@mcs.anl.gov>
2012-10-01 18:31:27 +0000

    Drop unnecessary output
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@4980 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/tests/adlb-containers.tcl
+++ code/tests/adlb-containers.tcl
@@ -14,7 +14,6 @@ if { ! [ adlb::amserver ] } {
     set c [ adlb::unique ]
     adlb::create $c $adlb::CONTAINER integer
 
-    puts "loop"
     set iterations [ expr 5 + $c ]
     for { set i [ expr $c + 1 ] } { $i < $iterations } { incr i } {
         set s $i
