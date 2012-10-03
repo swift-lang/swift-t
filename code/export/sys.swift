ec66f0cca25d5745475f7b9adce0ac7966fc123c
Tim Armstrong <tim.g.armstrong@gmail.com>
2012-10-03 03:26:25 +0000

    Add to data type section, add stubs for file
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@5019 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/export/sys.swift
+++ code/export/sys.swift
@@ -23,7 +23,7 @@ argv_accept(string... keys)
     "turbine" "0.0.2" "argv_accept"
     [ "turbine::argv_accept_impl [ list <<keys>> ]" ];
 @pure
-(string s) argv(string|int key, string... default_val)
+(string s) argv(string key, string... default_val)
     "turbine" "0.0.2" "argv_get"
     [ "set <<s>> [ turbine::argv_get_impl <<key>> <<default_val>> ]" ];
 
