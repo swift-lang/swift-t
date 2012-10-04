c0b52a0c1f50ff9d21a237c8fab4980077ae0c6a
Justin Wozniak <wozniak@mcs.anl.gov>
2012-10-04 19:12:16 +0000

    More accurate number format test
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@5043 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- tests/810-numberformat.swift
+++ tests/810-numberformat.swift
@@ -1,12 +1,14 @@
 
+// Test that toint(empty string) results in error
+// THIS-TEST-SHOULD-NOT-RUN
+
 #include <builtins.swift>
 #include <io.swift>
 #include <sys.swift>
 
 main {
-  // Note that argv("N") = ""
-  // int N = toint(argv("N"));
   int N = toint("");
   int V = N-1;
   printf("V: %i", V);
+  trace(V);
 }
