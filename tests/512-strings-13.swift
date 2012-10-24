8aab38a73eb7e4bff4aeedde8ef4b7080196fa13
Tim Armstrong <tim.g.armstrong@gmail.com>
2012-10-24 23:57:58 +0000

    Change isnum to bool result, add pure annotation to new string functions
    Fix minor test/string function bugs
    
    Avoid test num clash
    
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@5187 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- tests/512-strings-13.swift
+++ tests/512-strings-13.swift
@@ -6,40 +6,40 @@
 main {
     string x = "121";  
 
-    int a = isnum("011");
+    boolean a = isnum("011");
     assertEqual(1, a, "a");
 
-    int b = isnum("4242");
+    boolean b = isnum("4242");
     assertEqual(1, b, "b");
 
-    int c = isnum("999999999999999999999");
+    boolean c = isnum("999999999999999999999");
     assertEqual(1, c, "c");
     
-    int d = isnum(" 42");
+    boolean d = isnum(" 42");
     assertEqual(1, d, "d");
     
-    int e = isnum("42 ");
+    boolean e = isnum("42 ");
     assertEqual(1, e, "e");
 
-    int e1 = isnum(x);
+    boolean e1 = isnum(x);
     assertEqual(1, e1, "e1");
 
     // Fail cases 
 
-    int f = isnum("4 2");
+    boolean f = isnum("4 2");
     assertEqual(0, f, "f");
 
-    int g = isnum("a1");
+    boolean g = isnum("a1");
     assertEqual(0, g, "g");
 
-    int h = isnum("");
+    boolean h = isnum("");
     assertEqual(0, h, "h");
 
     // isnum will not accept floating pt values.
-    int i = isnum("4.2");
+    boolean i = isnum("4.2");
     assertEqual(0, i, "i");
 
-    int j = isnum(".77");
+    boolean j = isnum(".77");
     assertEqual(0, j, "j");
 
     
