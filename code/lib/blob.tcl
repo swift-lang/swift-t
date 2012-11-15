9701623e3665a6d7677151d5ecc740e6afae685d
Justin Wozniak <wozniak@mcs.anl.gov>
2012-11-15 05:47:33 +0000

    New blob functionality
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@5304 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/lib/blob.tcl
+++ code/lib/blob.tcl
@@ -11,14 +11,14 @@ namespace eval turbine {
   proc blob_size_body { out blob } {
     set blob_val [ retrieve_blob $blob ]
     set sz [ blob_size $blob_val ]
-    store_integer $out $sz 
+    store_integer $out $sz
     adlb::blob_free $blob
   }
 
   proc blob_size { blob_val } {
     return [ lindex $blob_val 1 ]
   }
-    
+
   proc blob_from_string { stack result input } {
     rule "bfs-$input-$result" $input $turbine::LOCAL \
       "blob_from_string_body $input $result"
@@ -42,20 +42,44 @@ namespace eval turbine {
     rule "bff-$input-$result" $input $turbine::LOCAL \
       "blob_from_floats_body $input $result"
   }
-
   proc blob_from_floats_body { container result } {
 
-    set type [ container_typeof $container ]
-    set N  [ adlb::container_size $container ]
-    c::log "bff_body start"
+      set type [ container_typeof $container ]
+      set N  [ adlb::container_size $container ]
+      c::log "bff_body start"
+      complete_container $container \
+          "blob_from_floats_store $result $container $N"
+  }
+  # This is called when every entry in container is set
+  proc blob_from_floats_store { result container N } {
     set A [ list ]
     for { set i 0 } { $i < $N } { incr i } {
       set td [ container_lookup $container $i ]
       set v  [ retrieve_float $td ]
       lappend A $v
     }
-
     adlb::store_blob_floats $result $A
     turbine::close_datum $result
+  }
+
+  # Assumes A is closed
+  proc complete_container { A action } {
+      set n [ adlb::container_size $A ]
+      log "complete_container: <$A> size: $n"
+      complete_container_continue $A $action 0 $n
+  }
+  proc complete_container_continue { A action i n } {
+      log "complete_container_continue: <$A> $i/$n"
+      if { $i < $n } {
+          set x [ container_lookup $A $i ]
+          rule "complete_container_continue-$A" [ list $x ] \
+              $turbine::LOCAL \
+              "complete_container_continue_body $A {$action} $i $n"
+      } else {
+          eval $action
+      }
+  }
+  proc complete_container_continue_body { A action i n } {
+      complete_container_continue $A $action [ incr i ] $n
   }
 }
