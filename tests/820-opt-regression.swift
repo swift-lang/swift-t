9953c14356386023faeff93247a4cf022381d2ab
Tim Armstrong <tim.g.armstrong@gmail.com>
2012-11-15 23:31:10 +0000

    Add regression test for optimiser bug found in sudoku
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@5315 dc4e9af1-7f46-4ead-bba6-71afc04862de

---
+++ tests/820-opt-regression.swift
@@ -0,0 +1,24 @@
+#include <builtins.swift>
+#include <io.swift>
+
+/*
+ * Regression test to check for bug where optimizer inserted
+ * wait into itself
+ */
+
+type struct {
+  int member;
+}
+
+(struct b) make() {
+  b.member = 1;
+}
+
+main {
+  struct X = make();
+  wait (X.member) {
+    if (X.member == 0) {
+      printf("DONE!");
+    }
+  }
+}
