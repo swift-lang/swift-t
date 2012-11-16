bde5b396cbab4f80201f41241ab6dddbea6d43c6
Tim Armstrong <tim.g.armstrong@gmail.com>
2012-11-16 00:04:12 +0000

    Regression test for optimiser bug with unused return value of multi-return function
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@5319 dc4e9af1-7f46-4ead-bba6-71afc04862de

---
+++ tests/821-opt-regression.swift
@@ -0,0 +1,14 @@
+#include <builtins.swift>
+
+@pure
+(int x, int y) doubleresult() {
+  x = 1;
+  y = 1;
+}
+
+main {
+  int a, b;
+  a, b = doubleresult();
+  // Regression test for dead code elim where only one result is used.
+  trace(a);
+}
