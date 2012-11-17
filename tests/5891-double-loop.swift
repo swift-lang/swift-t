f5f4be84c6e3f0df635cdff092a9ea53931687da
Justin Wozniak <wozniak@mcs.anl.gov>
2012-11-17 16:27:51 +0000

    Simpler test to demonstrate container slots bug
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@5322 dc4e9af1-7f46-4ead-bba6-71afc04862de

---
+++ tests/5891-double-loop.swift
@@ -0,0 +1,22 @@
+
+
+#include <builtins.swift>
+
+main
+{
+  int A[][];
+
+  int rows = 3;
+  int columns = 3;
+  foreach i in [0:rows-1]
+  {
+    A[i][0] = 0;
+  }
+  foreach i in [0:rows-1]
+  {
+    foreach j in [0:columns-1]
+    {
+      A[i][j] = 1;
+    }
+  }
+}
