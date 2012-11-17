8eb3ed09cd117f4a253459aacaad04bfda31b2d3
Tim Armstrong <tim.g.armstrong@gmail.com>
2012-11-17 20:10:06 +0000

    Extend test to try to cause failure.  No errors yet
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@5324 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- tests/5891-double-loop.swift
+++ tests/5891-double-loop.swift
@@ -1,13 +1,15 @@
 
 
 #include <builtins.swift>
+#include <string.swift>
+#include <io.swift>
 
 main
 {
   int A[][];
 
-  int rows = 3;
-  int columns = 3;
+  int rows = 10;
+  int columns = 10;
   foreach i in [0:rows-1]
   {
     A[i][0] = 0;
@@ -18,5 +20,18 @@ main
     {
       A[i][j+1] = 1;
     }
+  }
+
+  print_square_array(A, rows, columns);
+}
+
+print_square_array(int A[][], int rows, int cols) {
+  foreach i in [0:rows-1] {
+    string row_str;
+    for (row_str = "", int j = 0; j < cols;
+                j = j + 1, row_str=next_row_str) {
+      string next_row_str = sprintf("%s %0.4f", row_str, A[i][j]);
+    }
+    printf("row %i: %s", i, row_str);
   }
 }
