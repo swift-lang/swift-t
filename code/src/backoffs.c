b93d36524c9fd3e2d1c6c2b7a9cf49be38f671f4
Justin Wozniak <wozniak@mcs.anl.gov>
2012-11-22 16:13:57 +0000

    More flexibility in backoff speeds
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/lb/trunk@5362 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/src/backoffs.c
+++ code/src/backoffs.c
@@ -11,17 +11,27 @@
 #include "backoffs.h"
 #include "tools.h"
 
-// Allow debugging user to define either FAST or SLOW:
-#define FAST
-#ifdef SLOW
+// Progress speeds:
+#define SLOW   0 // for fine-tuned debugging
+#define MEDIUM 1 // good for use with valgrind
+#define FAST   2 // normal - do not use with valgrind (thrashes)
+#define SPEED  FAST
+// All backoffs in seconds
+#if SPEED == SLOW
        double xlb_max_idle          = 10;
        double xlb_steal_backoff     = 8;
 static double backoff_server_max    = 2;
 static double backoff_sync          = 1;
 static double backoff_sync_rejected = 1;
-#else // FAST
+#elif SPEED == MEDIUM
+       double xlb_max_idle          = 4;
+       double xlb_steal_backoff     = 0.5;
+static double backoff_server_max    = 0.001;
+static double backoff_sync          = 0.01;
+static double backoff_sync_rejected = 0.01;
+#elif SPEED == FAST
        double xlb_max_idle          = 1;
-       double xlb_steal_backoff     = 0.02;
+       double xlb_steal_backoff     = 0.01;
 static double backoff_server_max    = 0.000001;
 static double backoff_sync          = 0.00001;
 static double backoff_sync_rejected = 0.0001;
