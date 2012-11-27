71c8c587c0bed20ddce896656d253be6689e5c06
armstron <armstron@dc4e9af1-7f46-4ead-bba6-71afc04862de>
2012-11-27 23:13:06 +0000

    fix checkout script
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/dev@5427 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- git-svn/checkout.sh
+++ git-svn/checkout.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 
-for subrepo in c-utils lb turbine stc
+subrepos="c-utils lb turbine stc"
+for subrepo in $subrepos
 do
   git svn clone --stdlayout --tags=release \
           --prefix="svn/" \
           "https://svn.mcs.anl.gov/repos/exm/sfw/$subrepo" "$subrepo" &
 
-  # Add remote for github repository
-  git remote add github git@github.com:timarmstrong/exm-$subrepo.git
 done
 
 git svn clone "https://svn.mcs.anl.gov/repos/exm/sfw/dev" dev &
 
 wait
+
+for subrepo in $subrepos
+do
+  pushd $subrepo > /dev/null
+  # Add remote for github repository
+  git remote add github git@github.com:timarmstrong/exm-$subrepo.git
+  popd > /dev/null
+done
