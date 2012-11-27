7496073a244ff7a1e07826ec8e9d40c8744ba05d
armstron <armstron@dc4e9af1-7f46-4ead-bba6-71afc04862de>
2012-11-27 22:10:34 +0000

    Add git remote for github
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/dev@5426 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- git-svn/checkout.sh
+++ git-svn/checkout.sh
@@ -5,6 +5,9 @@ do
   git svn clone --stdlayout --tags=release \
           --prefix="svn/" \
           "https://svn.mcs.anl.gov/repos/exm/sfw/$subrepo" "$subrepo" &
+
+  # Add remote for github repository
+  git remote add github git@github.com:timarmstrong/exm-$subrepo.git
 done
 
 git svn clone "https://svn.mcs.anl.gov/repos/exm/sfw/dev" dev &
--- git-svn/push.sh
+++ git-svn/push.sh
@@ -3,7 +3,7 @@ for subrepo in c-utils lb turbine stc dev
 do
   echo Pushing $subrepo        
   pushd $subrepo > /dev/null
-  git push --all --tags origin  
+  git push --all --tags github
   popd > /dev/null
 set -e
 
