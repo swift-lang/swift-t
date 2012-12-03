cb4cb4069ace98700791ef782296d1933b3132b0
Tim Armstrong <tim.g.armstrong@gmail.com>
2012-12-03 17:44:26 +0000

    Draft of code to recursively unpack arrays for app args.
    Doesn't work yet but doesn't break anything.
    
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@5494 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/lib/app.tcl
+++ code/lib/app.tcl
@@ -3,10 +3,51 @@
 # Functions for launching external apps
 namespace eval turbine {
   
-  # Run external execution
+  # Run external appplication
   # cmd: executable to run
   # args: command line args as strings
   proc exec_external { cmd args } {
     exec $cmd {*}$args >@stdout 2>@stderr
   }
+
+  # Unpack arguments from closed container of any nesting into flat list
+  # Container must be deep closed (i.e. all contents closed)
+  proc unpack_args { container nest_level is_file } {
+    set res [ list ]
+    unpack_args_rec $container $nest_level $is_file res
+    return $res
+  }
+
+  proc unpack_args_rec { container nest_level is_file res_var } {
+    upvar 1 $res_var res
+
+    if { $nest_level == 0 } {
+      unpack_unnested_container $container $is_file res
+    } else {
+      # Iterate in key order
+      set contents [ adlb::enumerate $container members all 0 ]
+      set sorted_keys [ lsort -integer [ dict keys $contents ] ]
+      foreach key $sorted_keys {
+        set inner [ dict get $contents $key ]
+        unpack_args_rec $inner [ expr $nest_level - 1 ] $is_file res
+      }
+    }
+  }
+
+  proc unpack_unnested_container { container is_file res_var } {
+    upvar 1 $res_var res
+
+    # Iterate in key order
+    set contents [ adlb::enumerate $container members all 0 ]
+    set sorted_keys [ lsort -integer [ dict keys $contents ] ]
+    foreach key $sorted_keys {
+      set member [ dict get $contents $key ]
+      if { $is_file } {
+        lappend res [ retrieve_string [ get_file_name $member ] ]
+      } else {
+        lappend res [ retrieve $member ]
+      }
+    }
+  }
+
 }
