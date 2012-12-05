a6ba465d7d61b8fe397fb3bba6fc531a94cd90ea
Tim Armstrong <tim.g.armstrong@gmail.com>
2012-12-05 06:13:40 +0000

    Get multi-dimensional array expansion working
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@5513 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- tests/639-app.swift
+++ tests/639-app.swift
@@ -1,15 +1,31 @@
 // Test app array expansion with multi-d arrays
 
 #include <builtins.swift>
-// SKIP-THIS-TEST
 
 main {
   void done = echo([["one", "two"], ["three"], [], ["four", "five"]]);
   wait (done) {
-    trace("DONE");
+    trace("DONE1");
+  }
+
+  void done2 = echo2([
+      [
+        ["ein"], ["zwei", "drei"], ["vier"]
+      ],
+      [ ["fuenf", "sechs"] ],
+      [ ["sieben"] ],
+      [ [] ],
+      [
+        ["acht"], ["neun"], ["zehn"]
+      ]]);
+  wait (done2) {
+    trace("DONE2");
   }
 }
-
 app (void signal) echo (string args[][]) {
+    "/usr/bin/env" "echo" args; 
+}
+
+app (void signal) echo2 (string args[][][]) {
     "/usr/bin/env" "echo" args; 
 }
