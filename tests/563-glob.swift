4d5f2ba028f48a557c799f986073f07ede952e54
Tim Armstrong <tim.g.armstrong@gmail.com>
2012-12-06 23:48:37 +0000

    Change glob to return files
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@5557 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- tests/563-glob.swift
+++ tests/563-glob.swift
@@ -7,10 +7,10 @@
 //usage: stc 563-glob.swift -S=/home/zzhang/*.swift
 main
 {
-  string s[];	
+  file s[];	
   s = glob(argv("S"));
   foreach f in s
   {
-    printf("file: %s", f);	
+    printf("file: %s", filename(f));	
   }
 }
