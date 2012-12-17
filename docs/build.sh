95d0b53256d4739af7afbc03647f239a0c1b0325
Justin Wozniak <wozniak@mcs.anl.gov>
2012-12-17 20:47:12 +0000

    Fill in build.sh
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@5629 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- docs/build.sh
+++ docs/build.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-asciidoc --attribute stylesheet=${PWD}/swift.css swift.txt
+if ! readlink swift.css > /dev/null
+then
+  echo "Create a soft link to stc/.../docs/swift.css!"
+  exit 1
+fi
+
+asciidoc --attribute stylesheet=${PWD}/swift.css internals.txt
+asciidoc --attribute stylesheet=${PWD}/swift.css sites.txt
