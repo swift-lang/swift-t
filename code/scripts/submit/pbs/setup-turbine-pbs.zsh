0842e9d5381f0c6d605264d391d76cfdc8380e7c
Justin Wozniak <wozniak@mcs.anl.gov>
2013-01-08 22:29:20 +0000

    Initial sketch of PBS scripts...
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@5759 dc4e9af1-7f46-4ead-bba6-71afc04862de

---
+++ code/scripts/submit/pbs/setup-turbine-pbs.zsh
@@ -0,0 +1,32 @@
+#!/bin/zsh
+
+TURBINE=$( which turbine ) 
+if [[ ${?} != 0 ]] 
+then
+  print "Could not find Turbine in PATH!"
+  return 1
+fi
+
+set -x
+
+export TURBINE_HOME=$( cd $(dirname ${TURBINE})/.. ; /bin/pwd )
+
+source ${TURBINE_HOME}/scripts/helpers.zsh
+if [[ ${?} != 0 ]] 
+then
+  print "Broken Turbine installation!"
+  declare TURBINE_HOME
+  return 1
+fi
+
+TURBINE_PBS_M4=${TURBINE_HOME}/scripts/submit/pbs/turbine.pbs.m4
+TURBINE_PBS=${1:-turbine.pbs}
+export WALLTIME=${WALLTIME:-00:15:00}
+
+touch ${TURBINE_PBS}
+exitcode "Could not write to: ${TURBINE_PBS}"
+
+m4 < ${TURBINE_PBS_M4} > ${TURBINE_PBS}
+exitcode "Errors in M4 processing!"
+
+
---
+++ code/scripts/submit/pbs/turbine.pbs.m4
@@ -0,0 +1,17 @@
+changecom(`dnl')#!/bin/sh
+
+#PBS -N turbine
+#PBS -l nodes=1:ppn=4
+#PBS -l walltime=esyscmd(`printf $WALLTIME')
+#PBS -j oe
+
+set -x
+
+cd $PBS_O_WORKDIR
+
+TURBINE_HOME=esyscmd(`printf $TURBINE_HOME')
+
+source ${TURBINE_HOME}/scripts/turbine-config.sh
+
+mpiexec ${TCLSH} ~/t.tcl
+
