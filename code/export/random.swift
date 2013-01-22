7c3458ff4dc191e87ed8104ef1f478f07a894b40
Justin Wozniak <wozniak@mcs.anl.gov>
2013-01-22 23:38:39 +0000

    New Swift/T function srand()
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@6025 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/export/random.swift
+++ code/export/random.swift
@@ -1,9 +1,15 @@
 #ifndef RAND_SWIFT
 #define RAND_SWIFT
+
 (float o) random() "turbine" "0.0.2" "random"
     [ "set <<o>> [ expr rand() ]" ];
+
 // inclusive start, exclusive end
 (int o) randint(int start, int end) "turbine" "0.0.2" "randint"
     [ "set <<o>> [ turbine::randint_impl <<start>> <<end>> ]" ];
+
+(void v) srand(int seed)
+"turbine" "0.0.2" "random"
+[ "expr srand(<<seed>>) ; turbine::log \"srand <<seed>>\"" ];
 
 #endif // RAND_SWIFT
