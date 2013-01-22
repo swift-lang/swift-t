9e5476fda69e66155595f5ee4948564cbcf566f6
Justin Wozniak <wozniak@mcs.anl.gov>
2013-01-22 20:23:07 +0000

    Adding src/exm/stc/tclbackend/Target.java
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@6013 dc4e9af1-7f46-4ead-bba6-71afc04862de

---
+++ code/src/exm/stc/tclbackend/Target.java
@@ -0,0 +1,47 @@
+package exm.stc.tclbackend;
+
+import exm.stc.tclbackend.tree.Token;
+
+/**
+ * The target of a Turbine rule 
+ * Becomes the target of the ADLB task
+ * The target is either ADLB_ANY_RANK or an integer rank
+ * @author wozniak
+ * */
+public class Target
+{
+  boolean rankAny = false;
+  int targetRank = -1;
+  
+  static final Token ADLB_RANK_ANY = new Token("$adlb::RANK_ANY");
+  
+  Target(boolean rankAny, int targetRank)
+  {
+    this.rankAny = rankAny;
+    this.targetRank = targetRank;
+  }
+  
+  /**
+     Targets task to any rank
+   */
+  static Target rankAny()
+  {
+    return new Target(true, -1);
+  }
+  
+  /**
+     Targets task to specific target rank
+   */
+  static Target rank(int targetRank)
+  {
+    return new Target(false, targetRank);
+  }
+  
+  Token toTcl()
+  {
+    if (rankAny)
+      return ADLB_RANK_ANY;
+    else
+      return new Token(targetRank);
+  }
+}
