7815d7d6f7cae7b9a277fe1a1e39b32c468e406e
Justin Wozniak <wozniak@mcs.anl.gov>
2013-02-08 18:53:11 +0000

    Do not use srand() - it is gone
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@6231 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- tests/568-hostmap-1.swift
+++ tests/568-hostmap-1.swift
@@ -2,12 +2,11 @@
 #include <builtins.swift>
 #include <files.swift>
 #include <io.swift>
-#include <random.swift>
 #include <string.swift>
 #include <sys.swift>
 
 /**
-   Run as turbine ... 568.tcl $RANDOM
+   Run as TURBINE_SRAND=<seed> turbine ... 568.tcl
 */
 
 app (file o) hostname()
@@ -17,13 +16,9 @@ app (file o) hostname()
 
 main
 {
-  void v = srand(toint(args()));
-  wait (v)
-  {
-    file tmp<"tmp.txt"> = hostname();
-    string name = trim(readFile(tmp));
-    printf("name: %s", name);
-    host_id rank = hostmap_one(name);
-    printf("rank: %i", rank);
-  }
+  file tmp<"tmp.txt"> = hostname();
+  string name = trim(readFile(tmp));
+  printf("name: %s", name);
+  host_id rank = hostmap_one(name);
+  printf("rank: %i", rank);
 }
