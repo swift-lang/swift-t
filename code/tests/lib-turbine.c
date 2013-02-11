4418b8e974ade2bf4b6c2ace7db9d408ac44fabb
Justin Wozniak <wozniak@mcs.anl.gov>
2013-02-11 21:31:00 +0000

    Allow Turbine to run in embedded mode (#380)
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/branches/issue-380@6280 dc4e9af1-7f46-4ead-bba6-71afc04862de

---
+++ code/tests/lib-turbine.c
@@ -0,0 +1,72 @@
+
+#include <stdio.h>
+
+#include <mpi.h>
+#include <tcl.h>
+
+#include <tools.h>
+
+int
+main()
+{
+  printf("HI\n");
+
+  int mpi_argc = 0;
+  char** mpi_argv = NULL;
+
+  MPI_Init(&mpi_argc, &mpi_argv);
+
+  Tcl_Interp* interp = Tcl_CreateInterp();
+  Tcl_Init(interp);
+
+  // Create communicator for ADLB
+  MPI_Comm* adlb_comm;
+  adlb_comm = malloc(sizeof(MPI_Comm));
+  MPI_Comm_dup(MPI_COMM_WORLD, adlb_comm);
+
+  int rank;
+  MPI_Comm_rank(*adlb_comm, &rank);
+
+  // Store communicator in Tcl for turbine::init to find
+  Tcl_Obj* TURBINE_ADLB_COMM =
+      Tcl_NewStringObj("TURBINE_ADLB_COMM", -1);
+  Tcl_Obj* adlb_comm_ptr = Tcl_NewLongObj((long) adlb_comm);
+  Tcl_ObjSetVar2(interp, TURBINE_ADLB_COMM, NULL, adlb_comm_ptr, 0);
+
+  // Set argc/argv to empty
+  Tcl_Obj* argc  = Tcl_NewStringObj("argc", -1);
+  Tcl_Obj* zero  = Tcl_NewIntObj(0);
+  Tcl_ObjSetVar2(interp, argc, NULL, zero, 0);
+  Tcl_Obj* argv  = Tcl_NewStringObj("argv", -1);
+  Tcl_Obj* empty = Tcl_NewStringObj("", -1);
+  Tcl_ObjSetVar2(interp, argv, NULL, empty, 0);
+
+  // Slurp the user script
+  char* script = slurp("tests/noop.tcl");
+  if (rank == 0)
+    printf("script: %s\n", script);
+  int rc = Tcl_Eval(interp, script);
+
+  char* v = getenv("TCLLIBPATH");
+  printf("TCLLIBPATH: %s\n", v);
+
+  // int rc = Tcl_Eval(interp, "puts EVAL");
+  // int rc = Tcl_Eval(interp, "error ERROR");
+  if (rc != TCL_OK)
+  {
+    Tcl_Obj* error_dict = Tcl_GetReturnOptions(interp, rc);
+    Tcl_Obj* error_info = Tcl_NewStringObj("-errorinfo", -1);
+    Tcl_Obj* error_msg;
+    Tcl_DictObjGet(interp, error_dict, error_info, &error_msg);
+    char* msg_string = Tcl_GetString(error_msg);
+    printf("Tcl error: %s\n", msg_string);
+  }
+
+  Tcl_DeleteInterp(interp);
+  free(script);
+  free(adlb_comm);
+
+  MPI_Finalize();
+
+  return 0;
+}
