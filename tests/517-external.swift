b3246d56ad74bdab288b8c08ec94ae985fefeffd
Tim Armstrong <tim.g.armstrong@gmail.com>
2013-02-15 00:09:03 +0000

    External data type plus tests
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@6345 dc4e9af1-7f46-4ead-bba6-71afc04862de

---
+++ tests/517-external.swift
@@ -0,0 +1,13 @@
+#include <builtins.swift>
+// THIS-TEST-SHOULD-NOT-COMPILE
+
+app (external o) echo (string msg[]) {
+    "echo" "msg:" msg
+}
+
+main {
+    external x = echo(["hello"]);
+    void y = x;
+    // Check that external type is specialization of void
+    external z = y; // This should fail typechecking
+}
