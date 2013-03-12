35ff63fc2239906d3933e7d1208f6929e2536b51
Tim Armstrong <tim.g.armstrong@gmail.com>
2013-03-12 04:25:18 +0000

    Add regression test for passing updateable bug
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@6513 dc4e9af1-7f46-4ead-bba6-71afc04862de

---
+++ tests/962-updateable.swift
@@ -0,0 +1,13 @@
+#include <builtins.swift>
+
+//COMPILE-ONLY-TEST
+// Regression test for bug passing updateablefloats
+main {
+  updateable_float x = 1.0;
+
+  f(x);
+}
+
+f(updateable_float x) {
+  trace(x);
+}
