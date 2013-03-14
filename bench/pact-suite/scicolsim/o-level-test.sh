19f97cb898647dacefd9636a5dbcfe5ea241ddd8
Tim Armstrong <tim.g.armstrong@gmail.com>
2013-03-14 16:27:54 +0000

    Add test script to collect stats with incremental optimizations added
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@6531 dc4e9af1-7f46-4ead-bba6-71afc04862de

---
+++ bench/pact-suite/scicolsim/o-level-test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+SCS=~/ExM/scicolsim.git/
+
+i=1
+while read olevel; do
+  echo $i: $olevel
+
+  TCL=annealing-exm.olevel$i.tcl
+  stc $olevel -C annealing-exm.olevel$i.ic -I $SCS/src $SCS/src/annealing-exm.swift $TCL 
+
+  export TURBINE_USER_LIB=$SCS 
+  export TURBINE_DEBUG=0
+  export ADLB_DEBUG=1
+  export TURBINE_LOG=0
+  turbine -n6 $TCL \
+              --graph_file=movie_graph.txt \
+              --annealingcycles=25 \
+              --evoreruns=100 --reruns_per_task=1 \
+              --minrange=58 --maxrange=58 \
+              --n_epochs=1 --n_steps=1 | ../scripts/opcounts.py | tee annealing-exm.olevel$i.counts
+  i=$(($i+1))
+done < o-levels.txt
