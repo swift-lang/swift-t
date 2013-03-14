6b29c3acf774473e4c39f5bb5951f19de7cb61f9
Tim Armstrong <tim.g.armstrong@gmail.com>
2013-03-14 05:16:44 +0000

    Got sudoku to run successfully
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@6525 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- bench/pact-suite/sudoku/run.sh
+++ bench/pact-suite/sudoku/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export TURBINE_USER_LIB=/home/tga/ExM/exm.git/apps/sudoku/
-../scripts/opcount_bench.sh $1 $2 ~/ExM/exm.git/apps/sudoku/coord.swift --board=100x100_99s --dfsquota=1000
+../scripts/opcount_bench.sh $1 $2 ~/ExM/exm.git/apps/sudoku/coord.swift --board=100x100_116s --dfsquota=100 --boardsize=100
