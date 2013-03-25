fb2fa0d0d038e91d7b602c327b414d916a047bbe
Justin Wozniak <wozniak@mcs.anl.gov>
2013-03-25 18:57:09 +0000

    Experiments with printf chaining in matrix/vector output (WIP)
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@6628 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/export/matrix.swift
+++ code/export/matrix.swift
@@ -8,15 +8,56 @@
 (float A[][]) matrix_from_blob_fortran(blob b, int m, int n)
 "turbine" "0.0.2" "matrix_from_blob_fortran";
 
-matrix_print(float A[][], int rows)
+(void v)
+vector_print(float x[], int rows)
 {
+  printf("vector rows: %i", rows);
   foreach i in [0:rows-1]
   {
-    printf("matrix rows: %i", rows);
+    printf("row %i: %0.4f", i, x[i]);
+  }
+  v = make_void();
+}
+
+(void v)
+vector_print_integer(int x[], int rows)
+{
+  int i;
+  void t;
+  printf("vector rows: %i", rows) =>
+    for (i = 0; i < rows; i = i+1)
+  {
+    t = printf("vpi: row %i: %i", i, x[i]);
+  };
+  wait(i) { v = make_void(); }
+}
+
+(void v)
+matrix_print(float A[][], int rows)
+{
+  int i;
+  printf("matrix rows: %i", rows) =>
+  for (i = 0; i < rows; i = i+1)
+  {
     string s;
-    for (s = "", int j = 0; j < rows;
+    for (s = "", int j = 0; j < size(A[i]);
            j = j+1,
            s = sprintf("%s %0.4f", s, A[i][j]))
+    {}
+    printf("row %i: %s", i, s);
+  }
+  wait(i) { v = make_void(); }
+}
+
+matrix_print_integer(int A[][], int rows)
+{
+  printf("matrix rows: %i", rows);
+  foreach i in [0:rows-1]
+  {
+    string s;
+    for (s = "", int j = 0; j < size(A[i]);
+           j = j+1,
+           s = sprintf("%s %i", s, A[i][j]))
     {}
     printf("row %i: %s", i, s);
   }
