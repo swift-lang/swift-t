67ebc85e2ed06f03025b4838901408e828611b5b
Tim Armstrong <tim.g.armstrong@gmail.com>
2013-03-31 18:49:35 +0000

    Qualify name
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@6762 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/src/exm/stc/tclbackend/Turbine.java
+++ code/src/exm/stc/tclbackend/Turbine.java
@@ -463,9 +463,9 @@ class Turbine
   private static TclTree adlbWorkType(TaskMode type) {
     switch (type) {
       case CONTROL:
-        return new Value("CONTROL");
+        return new Value("turbine::CONTROL");
       case WORKER:
-        return new Value("WORK");
+        return new Value("turbine::WORK");
       default:
         throw new STCRuntimeError("Can't create task of type " + type);
     }
