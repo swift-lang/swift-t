c1b4bb6b03f8946622a12bc864ca3bca4c9fcb16
Tim Armstrong <tim.g.armstrong@gmail.com>
2013-04-12 15:38:47 +0000

    Make heap test compile cleanly
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/c-utils/trunk@7120 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- tests/heap.c
+++ tests/heap.c
@@ -25,37 +25,37 @@ int main() {
 
   heap_init(&h, 128);
 
-  heap_add(&h, 1, 1);
-  heap_add(&h, 234, 234);
-  heap_add(&h, 3, 3);
+  heap_add(&h, 1, (void *)1);
+  heap_add(&h, 234, (void *)234);
+  heap_add(&h, 3, (void *)3);
   heap_decrease_key(&h, 0, h.array[0].key - 1);
   heap_decrease_key(&h, 0, h.array[0].key - 1);
   heap_decrease_key(&h, 0, h.array[0].key - 1);
   heap_decrease_key(&h, 0, h.array[0].key - 1);
-  heap_add(&h, 453, 453);
-  heap_add(&h, 2, 2);
-  heap_add(&h, -1, -1);
-  heap_add(&h, 3, 3);
+  heap_add(&h, 453, (void *)453);
+  heap_add(&h, 2, (void *)2);
+  heap_add(&h, -1, (void *)-1);
+  heap_add(&h, 3, (void *)3);
   heap_decrease_key(&h, 4, h.array[4].key - 1);
   heap_increase_key(&h, 2, h.array[2].key + 2);
   heap_increase_key(&h, 0, h.array[0].key + 21);
-  heap_add(&h, 234, 234);
-  heap_add(&h, 453, 453);
+  heap_add(&h, 234, (void *)234);
+  heap_add(&h, 453, (void *)453);
   heap_decrease_key(&h, 4, h.array[4].key - 1);
   heap_decrease_key(&h, 4, h.array[4].key - 1);
   heap_decrease_key(&h, 7, h.array[7].key - 12333);
-  heap_add(&h, 2, 2);
-  heap_add(&h, -1, -1);
-  heap_add(&h, 234, 234);
-  heap_add(&h, 54, 54);
-  heap_add(&h, 254, 253);
+  heap_add(&h, 2, (void *)2);
+  heap_add(&h, -1, (void *)-1);
+  heap_add(&h, 234, (void *)234);
+  heap_add(&h, 54, (void *)54);
+  heap_add(&h, 254, (void *)253);
 
   heap_check(&h);
 
   // Heap sort
   while (heap_size(&h) > 0) {
-    printf("(%li, %li)\n", heap_root_key(&h),
-                           heap_root_val(&h));
+    printf("(%i, %li)\n", heap_root_key(&h),
+                           (long)heap_root_val(&h));
     heap_check(&h);
     heap_del_root(&h);
   }
