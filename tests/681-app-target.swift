c915ab3f665bf6c794c6f9960ccecdd8d8be1fc2
Tim Armstrong <tim.g.armstrong@gmail.com>
2013-04-25 18:39:49 +0000

    Add test for @location and app functions
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@7289 dc4e9af1-7f46-4ead-bba6-71afc04862de

---
+++ tests/681-app-target.swift
@@ -0,0 +1,24 @@
+// Test app location dispatch
+
+import assert;
+import files;
+import io;
+import string;
+import sys;
+
+app (file o) hostname() {
+  "hostname" @stdout=o;
+}
+
+(string o) extract_hostname(file f) {
+  o = trim(readFile(f));
+}
+
+main {
+  foreach i in [1:100] {
+    string host1 = extract_hostname(hostname());
+    // Run on same host
+    string host2 = extract_hostname(@location=hostmap_one(host1) hostname());
+    assertEqual(host1, host2, sprintf("Check hostnames same trial %i", i));
+  }
+}
