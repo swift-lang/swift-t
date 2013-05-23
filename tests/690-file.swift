e58f7f89ec36b461bfa163fa92047bd8e7e6113a
Tim Armstrong <tim.g.armstrong@gmail.com>
2013-05-23 18:34:51 +0000

    Update test to be correct
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@7596 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- tests/690-file.swift
+++ tests/690-file.swift
@@ -1,16 +1,15 @@
-
+import assert;
 import io;
+
+// Check that we can extract filename from file
 
 (string output)
 echo_filename(file input) "funcs" "0.0"
-[ "set <<output>> <<input>>" ];
-
-// SKIP-THIS-TEST: Issue #453
-// Issue is filename substitution into Tcl
-// This works:  [ lindex <<input>> 0 ]
+[ "set <<output>> [ turbine::local_file_path <<input>> ]" ];
 
 main {
   file f = input_file("alice.txt");
   string s = echo_filename(f);
   printf("filename: %s", s);
+  assertEqual(s, "alice.txt", "Filename correct");
 }
