3bea898a5324a6af6c8980053a18d2b51cae1ce7
Justin Wozniak <wozniak@mcs.anl.gov>
2013-06-05 16:10:55 +0000

    Save packages
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@7688 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- tests/clean.sh
+++ tests/clean.sh
@@ -4,8 +4,8 @@
 
 # Note: *funcs.tcl are required for Swift/T builtin function tests
 
-TCL=$( ls *.tcl | grep -v "funcs.tcl\|make-package.tcl" )
-rm ${TCL}
+TCL=$( ls *.tcl | grep -v "funcs.tcl\|make-package.tcl\|pkgIndex.tcl" )
+rm -f ${TCL}
 rm -f *.ic
 rm -f *.out
 rm -f *.stc.*
