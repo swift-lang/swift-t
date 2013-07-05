89f05561caf6b94fa1d8bc46379f9f5b697cb88d
Justin Wozniak <wozniak@mcs.anl.gov>
2013-07-05 19:42:22 +0000

    Support different Tcl versions
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@8032 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- docs/examples/4/build.sh
+++ docs/examples/4/build.sh
@@ -1,6 +1,13 @@
-#!/bin/sh
+#!/bin/bash -e
+
+TURBINE=$( which turbine )
+TURBINE_HOME=$( dirname $( dirname ${TURBINE} ) )
+source ${TURBINE_HOME}/scripts/turbine-config.sh
+
+set -x
 
 BLAS="$HOME/Downloads/BLAS/blas_LINUX.a"
+TCL_INCLUDE=${TCL}/include
 
 # Wrap the Fortran in C++
 fortwrap.py --array-as-ptr --no-vector --no-fmat mvm.f
@@ -11,11 +18,11 @@ sed -i '11i#include "FortFuncs.h"' FortFuncs_wrap.cxx
 
 # Compile everything
 g++      -c -fPIC -I . FortFuncs.cpp
-g++      -c -fPIC -I /usr/include/tcl8.5 FortFuncs_wrap.cxx
+g++      -c -fPIC -I ${TCL_INCLUDE} FortFuncs_wrap.cxx
 gfortran -c -fPIC mvm.f
 
 # Build the shared object
-g++ -shared -o libmvm.so FortFuncs_wrap.o FortFuncs.o mvm.o $BLAS -lgfortran
+g++ -shared -o libmvm.so FortFuncs_wrap.o FortFuncs.o mvm.o ${BLAS} -lgfortran
 
 # Make the Tcl package
-tclsh make-package.tcl > pkgIndex.tcl
+${TCLSH} make-package.tcl > pkgIndex.tcl
