d02ec8595a1e8db8c5e4bc8ac9aa1d1bc1f7aaf9
armstron <armstron@dc4e9af1-7f46-4ead-bba6-71afc04862de>
2013-08-08 16:02:25 +0000

    Fix mpi build script
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/dev@8525 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- mpi_build.sh
+++ mpi_build.sh
@@ -8,11 +8,11 @@ source ${THISDIR}/build-vars.sh
 CONF_FLAGS=
 
 if [ ! -z "$MPICH2_SOCK" ]; then
-  CONF_FLAGS+="--with-device=ch3:sock"
+  CONF_FLAGS+=" --with-device=ch3:sock"
 fi
 
 if [ -z "$ENABLE_FORTRAN" ]; then
-  CONF_FLAGS+="--disable-f77 --disable-fc"
+  CONF_FLAGS+=" --disable-f77 --disable-fc"
 fi
 
 ./configure --disable-graphics --enable-shared --enable-lib-depend --with-mpe --prefix=${MPICH_INST} CFLAGS=-fPIC CXXFLAGS=-fPIC ${CONF_FLAGS}
