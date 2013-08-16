c4f75a76ac0e6ce8390e8773185e1991ae2c3edf
Tim Armstrong <tim.g.armstrong@gmail.com>
2013-08-16 01:20:22 +0000

    Improve test to exercise logic with unmapped files
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@8634 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- tests/565-file_write.swift
+++ tests/565-file_write.swift
@@ -2,14 +2,22 @@
 import io;
 import files;
 import sys;
-
+import assert;
 
 main
 {
+  // Test with mapped file
   file f <"test.tmp">;
   f = write("hello world!");
   string s;
   s = read(f);
   printf("%s", s);
+  assertEqual(s, "hello world!", "mapped");
+  
+  // Test with unmapped file
+  file f2 = write("testing!");
+  string s2 = read(f2);
+  printf("%s", s2);
+  assertEqual(s2, "testing!", "unmapped");
 }
 
