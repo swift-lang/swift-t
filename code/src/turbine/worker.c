bc7bf494966b16ba7d267b285259b721e6179a3a
Justin Wozniak <wozniak@mcs.anl.gov>
2013-08-19 19:36:50 +0000

    Support for TURBINE_WORKER_SERVICE (#524)
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@8667 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/src/turbine/worker.c
+++ code/src/turbine/worker.c
@@ -6,22 +6,45 @@
  *      Author: wozniak
  */
 
+#define _GNU_SOURCE // for asprintf()
+#include <signal.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #include <adlb.h>
 #include <tcl.h>
 
+#include <list.h>
+#include <log.h>
+#include <tools.h>
+
 #include "src/util/debug.h"
 #include "src/turbine/turbine.h"
 
-static void
-task_error(Tcl_Interp* interp, int tcl_rc, char* command);
+/** Did we launch a service process? */
+static bool service_launched;
+/** The PID of the service process */
+static pid_t service_pid;
+
+static void service_init(void);
+static void service_finalize(void);
+
+static void task_error(Tcl_Interp* interp, int tcl_rc, char* command);
+
+#define service_log(format, args...) \
+  log_printf("TURBINE_WORKER_SERVICE: " format, ## args)
 
 turbine_code
 turbine_worker_loop(Tcl_Interp* interp, void* buffer, int buffer_size,
                     int work_type)
 {
   int rc;
+
+  service_init();
+
   while (true)
   {
     MPI_Comm task_comm;
@@ -57,6 +80,9 @@ turbine_worker_loop(Tcl_Interp* interp, void* buffer, int buffer_size,
       return TURBINE_ERROR_EXTERNAL;
     }
   }
+
+  service_finalize();
+
   return TURBINE_SUCCESS;
 }
 
@@ -73,4 +99,100 @@ task_error(Tcl_Interp* interp, int tcl_rc, char* command)
   // printf("%s\n", msg);
   Tcl_AddErrorInfo(interp, msg);
   free(msg);
+}
+
+static void service_launch(char* cmd);
+
+static void
+service_init()
+{
+  char* cmd = getenv("TURBINE_WORKER_SERVICE");
+  if (cmd == NULL) return;
+
+  service_pid = fork();
+  if (service_pid != 0)
+    service_log("pid: %i", service_pid);
+  else
+    service_launch(cmd);
+
+  service_launched = true;
+
+  return;
+}
+
+static void
+service_launch(char* cmd)
+{
+  service_log("command: %s", cmd);
+  struct list* words = list_split_words(cmd);
+
+  char* args[list_size(words)+1];
+  int i = 0;
+  for (struct list_item* item = words->head; item;
+       item = item->next, i++)
+    args[i] = item->data;
+  args[i] = NULL;
+
+  list_free(words);
+
+  int rc = execvp(args[0], args);
+  if (rc == -1)
+    printf("ERROR: could not launch TURBINE_WORKER_SERVICE: %s\n",
+           cmd);
+}
+
+static bool service_shutdown(int* status);
+
+static void
+service_finalize()
+{
+  if (!service_launched) return;
+
+  int status;
+  pid_t pid = waitpid(service_pid, &status, WNOHANG);
+
+  if (pid == 0)
+  {
+    bool b = service_shutdown(&status);
+    if (!b) return;
+  }
+
+  if (! WIFEXITED(status))
+    service_log("warning: service exited abnormally");
+  service_log("service exit code: %i", WEXITSTATUS(status));
+
+  return;
+}
+
+static bool
+service_shutdown(int* status)
+{
+  service_log("child is running: sending SIGTERM");
+  int rc = kill(service_pid, SIGTERM);
+  if (rc != 0)
+    service_log("warning: could not kill service");
+
+  pid_t pid;
+  int s;
+  // Wait for process to exit (up to 2 seconds, exponential backoff)
+  double delay = 0.01;
+  while (delay < 1)
+  {
+    pid = waitpid(service_pid, &s, WNOHANG);
+    if (pid == 0)
+    {
+      // service_log("service still running: %0.2f", delay);
+      time_delay(delay);
+      delay *= 2;
+    }
+    else
+    {
+      *status = s;
+      return true;
+    }
+  }
+
+  printf("WARNING: Turbine worker service did not exit!");
+
+  return false;
 }
