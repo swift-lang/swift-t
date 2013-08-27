7ce6ec64546fbd03cc529850f9f2c5f26aafcac2
Tim Armstrong <tim.g.armstrong@gmail.com>
2013-08-27 22:25:05 +0000

    Split passing and failing parts of test 514
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@8745 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- tests/514-string-multiline.swift
+++ tests/514-string-multiline.swift
@@ -1,6 +1,5 @@
 
 import io;
-import assert;
 
 f() "turbine" "0.0"
 [----
@@ -30,10 +29,4 @@ A\101
 
 printf("OUTPUT: %s %s %s", t1, t2, t3);
   f();
-
-  string t4 =
-"""
-aaa \\
-""";
-  assertEqual(t4, "\naaa \\\n", "t4");
 }
---
+++ tests/514c-string-multiline.swift
@@ -0,0 +1,11 @@
+import assert;
+// SKIP-THIS-TEST
+
+main () {
+
+  string t4 =
+"""
+aaa \\
+""";
+  assertEqual(t4, "\naaa \\\n", "t4");
+  }
