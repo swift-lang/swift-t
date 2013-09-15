1d884e7e8256969c53424b2e431f2a3850e38edb
Tim Armstrong <tim.g.armstrong@gmail.com>
2013-09-15 23:43:21 +0000

    Fix run script for sudoku
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@9146 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- bench/pact-suite/sudoku/run.sh
+++ bench/pact-suite/sudoku/run.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-export TURBINE_USER_LIB=/home/tga/ExM/exm.git/apps/sudoku/
-../scripts/opcount_bench.sh $1 $2 ~/ExM/exm.git/apps/sudoku/coord.swift --board=100x100_116s --dfsquota=100 --boardsize=100
+SUDOKU_HOME=/home/tim/ExM/exm.git/apps/sudoku/
+export TURBINE_USER_LIB=$SUDOKU_HOME
+
+SCRIPT_DIR=`cd $(dirname $0); pwd`
+
+cd $SUDOKU_HOME/puzzles
+"$SCRIPT_DIR/../scripts/opcount_bench.sh" "$1" "$2" "$SUDOKU_HOME/coord.swift" --board=100x100_116s --dfsquota=100 --boardsize=100
