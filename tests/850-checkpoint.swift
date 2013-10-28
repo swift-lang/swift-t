937aeffb16f7f0e94a4096c6e898c922c48cbd97
Tim Armstrong <tim.g.armstrong@gmail.com>
2013-10-28 23:00:39 +0000

    Augment checkpoint test with additional scalar data types and multiple in/out arguments
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@9621 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- tests/850-checkpoint.swift
+++ tests/850-checkpoint.swift
@@ -1,18 +1,48 @@
-// Basic test for checkpointing
+import assert;
 
+// Basic test for checkpointing
 main {
  
- trace(f(1));
- trace(f(2));
+  trace(f(1));
+  trace(f(2));
 
- foreach i in [1:100] {
+  foreach i in [1:100] {
     trace(f(i));
- }
+    t1, t2 = g(i);
+    trace(t1, t2);
+    trace(h(i, fromint(i*10), blob_from_string(fromint(i*1000))));
+  }
+
+
+  assertEqual(f(1), 2, "f(1)");
+
+  x1, x2 = g(3);
+  assertEqual(x1, 4, "x1");
+  assertEqual(x2, 5, "x2");
+
+
+  // 10 + 10 + 4
+  tot = h(10, "word", blob_from_string("word word"));
+  assertEqual(tot, 24, "tot");
 }
 
 
+// Single scalar arg
 @checkpoint
-(int o) f ( int i) {
-
+(int o) f (int i) {
     o = i + 1;
+}
+
+// Single scalar arg, multiple outputs
+@checkpoint
+(int o1, int o2) g (int i) "turbine" "0.0" [
+  "lassign [ list [ expr <<i>> + 1 ] [ expr <<i>> + 2 ] ] <<o1>> <<o2>>"
+];
+
+import blob;
+import string;
+// Multiple scalar args, including blob
+@checkpoint
+(int o) h (int i, string s, blob b) {
+  o = i + blob_size(b) + strlen(s);
 }
