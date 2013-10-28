cdb28888def024a27248b16b9b96da654fe9c874
Tim Armstrong <tim.g.armstrong@gmail.com>
2013-10-28 20:09:50 +0000

    Initialise checkpointing from ADLB
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@9613 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/lib/checkpoint.tcl
+++ code/lib/checkpoint.tcl
@@ -16,22 +16,71 @@
 # Functions for checkpointing
 namespace eval turbine {
 
-  # Can be DISABLED, W, RW, depending on whether we are doing nothing
+  # Can be DISABLED, R, W, RW, depending on whether we are doing nothing
   #  with checkpoints (DISABLED), writing checkpoints for a fresh run (W),
-  #  or reading/writing checkpoints after a restore (RW)
+  #  or reading/writing checkpoints after a restore (RW), or reading
+  #  old checkpoints but not writing fresh ones (R)
   variable xpt_mode
 
   # Initialize checkpointing, getting settings from environment
+  # Environment variables are:
+  # TURBINE_XPT_FILE: file to log to
+  # TURBINE_XPT_RESTORE: colon-separated list of files to restore
+  # TURBINE_XPT_FLUSH: flush mode
+  # TURBINE_XPT_INDEX_MAX: max size in bytes
   proc xpt_init { } {
-    # TODO: get from env vars 
-    set xpt_filename "tmp.xpt"
+    variable xpt_mode
+    # Defaults
+    # Default to periodic_flush
     set flush_mode periodic_flush
-
-    # TODO: look for checkpoint_restore
-    
     # Default to 1mb
-    set max_index_val [ expr 1024 * 1024 ]
-    adlb::xpt_init $xpt_filename $flush_mode $max_index_val
+    set xpt_index_max [ expr 1024 * 1024 ]
+    set xpt_filename ""
+    # xpt_restore is list of files to restore
+    set xpt_restore [ list ]
+
+    if [ info exists ::env(TURBINE_XPT_FILE) ] {
+      set xpt_filename $::env(TURBINE_XPT_FILE)
+    }
+    if [ info exists ::env(TURBINE_XPT_RESTORE) ] {
+      set xpt_restore [ split $::env(TURBINE_XPT_RESTORE) ":" ]
+    }
+    if [ info exists ::env(TURBINE_XPT_FLUSH) ] {
+      set flush_mode $::env(TURBINE_XPT_FLUSH)
+    }
+    if [ info exists ::env(TURBINE_XPT_INDEX_MAX) ] {
+      set xpt_index_max $::env(TURBINE_XPT_INDEX_MAX) 
+      if { ! [ string is integer $xpt_index_max ] } {
+        error "TURBINE_XPT_INDEX_MAX must be integer: \"${xpt_index_max}\""
+      }
+    }
+
+    adlb::xpt_init $xpt_filename $flush_mode $xpt_index_max
+
+    foreach restore_file $xpt_restore {
+      # TODO: other args for xpt_restore?
+      adlb::xpt_restore $restore_file
+    }
+
+    #Determine mode based on what was provided
+    if { [ llength $xpt_restore ] > 0 } {
+      if { $xpt_filename != "" } {
+        set xpt_mode RW
+      }
+      else {
+        set xpt_mode R
+      }
+    } else {
+      if { $xpt_filename != "" } {
+        set xpt_mode W
+      } else {
+        set xpt_mode DISABLED
+      }
+    }
+  }
+
+  proc xpt_restore {} {
+    variable xpt_mode
   }
 
   proc xpt_finalize { } {
