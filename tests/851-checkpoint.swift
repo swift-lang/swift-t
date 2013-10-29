180781e6b7240512e7e143f0ba07fe0c01b9536b
Tim Armstrong <tim.g.armstrong@gmail.com>
2013-10-29 22:27:21 +0000

    Add test for checkpointing of arrays
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@9631 dc4e9af1-7f46-4ead-bba6-71afc04862de

---
+++ tests/851-checkpoint.swift
@@ -0,0 +1,31 @@
+// Test checkpointing for data structures
+// SKIP-THIS-TEST
+
+import assert;
+import math;
+import stats;
+import blob;
+
+main {
+
+  int a;
+  float A[];
+  bag<int> abag;
+
+  a, A, abag = arrayf(10, [blob_from_string("hello"),
+                           blob_from_string("world")]);
+  assertEquals(a, 10 + 6, "a");
+  assertEquals(A[0], 6.0, "A[0]");
+  assertEquals(bag_size(abag), 2, "bag_size(abag)");
+}
+
+
+@checkpoint
+(int a, float A[], bag<int> abag) arrayf (int b, blob B[]) {
+
+  foreach x, i in B {
+    A[i] = itof(blob_size(x));
+    abag += blob_size(x);
+  }
+  a = b + round(sum_float(A)/itof(size(A)));
+}
