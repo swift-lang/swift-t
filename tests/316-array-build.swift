818139582a13d75003736a32d3f1ef460d67ea6e
Justin Wozniak <wozniak@mcs.anl.gov>
2013-11-05 18:00:54 +0000

    Tests now work
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@9769 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- tests/316-array-build.swift
+++ tests/316-array-build.swift
@@ -6,7 +6,7 @@ import io;
 (file o[]) task(file i, int n) "turbine" "0.1"
 [
 """
-set f [ swift_filename <<i>> ]
+set f [ swift_filename &<<i>> ]
 exec ./316-array-build.task.sh $f <<n>>
 set L [ glob test-316-*.data ]
 swift_array_build <<o>> $L file
--- tests/317-array-build.swift
+++ tests/317-array-build.swift
@@ -5,7 +5,7 @@ import unix;
 (file o[]) task(file i, int n) "turbine" "0.1"
 [
 """
-set f [ swift_filename <<i>> ]
+set f [ swift_filename &<<i>> ]
 exec ./317-array-build.task.sh $f <<n>>;
 set L [ glob test-317-*.data ];
 swift_array_build <<o>> $L file;
