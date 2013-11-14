f64ce75e7c43b838f14fb4c4f1fb744151a36c20
Tim Armstrong <tim.g.armstrong@gmail.com>
2013-11-14 23:39:07 +0000

    Fixup scicolsim launch script
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@10001 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- bench/pact-suite/scicolsim/run.sh
+++ bench/pact-suite/scicolsim/run.sh
@@ -6,16 +6,16 @@ if [[ $MODE == OPCOUNT ]]; then
 fi
 
 
-ARGS+="--graph_file=movie_graph.txt \
+ARGS+=" --graph_file=movie_graph.txt \
       --annealingcycles=25 \
       --evoreruns=100 --reruns_per_task=1 \
       --minrange=58 --maxrange=108 --rangeinc=50"
 
 # FROM PACT'13 PAPER:
-ARGS+="--graph_file=movie_graph.txt \
-      --annealingcycles=25 \
-      --evoreruns=100 --reruns_per_task=1 \
-      --minrange=58 --maxrange=58"
+#ARGS+=" --graph_file=movie_graph.txt \
+#      --annealingcycles=25 \
+#      --evoreruns=100 --reruns_per_task=1 \
+#      --minrange=58 --maxrange=58"
 
 ../scripts/opcount_bench.sh $1 $MODE ~/ExM/scicolsim.git/src/annealing-exm.swift \
               $ARGS
