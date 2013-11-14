99f44f5d3bb1d597d25ac1a566b66a256e69d02b
Tim Armstrong <tim.g.armstrong@gmail.com>
2013-11-14 14:29:16 +0000

    Wavefront ADLB implementation: working without any memory leaks
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@9969 dc4e9af1-7f46-4ead-bba6-71afc04862de

---
+++ bench/pact-suite/wavefront/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+INST=/home/tim/ExM/inst
+LB=$INST/lb
+CUTILS=$INST/c-utils
+MPICH=$INST/mpich2
+
+mpicc -O2 -Wall -std=c99 -L $LB/lib -L $CUTILS/lib -I $MPICH/include -I $LB/include -I $CUTILS/include -o wavefront wavefront.c -ladlb -lexmcutils -Wl,-rpath,$LB/lib,-rpath,$CUTILS/lib
