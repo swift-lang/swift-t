9205fb4aecc04b6d052a1827f7e00aac70ddb9d4
armstron <armstron@dc4e9af1-7f46-4ead-bba6-71afc04862de>
2013-11-25 16:06:15 +0000

    Insert wait statement around loop continue
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/dev@10181 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- git-svn/update-autotools.sh
+++ git-svn/update-autotools.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
-autotools_subrepos="c-utils lb turbine"
+autotools_dirs="c-utils lb/code turbine/code"
 
-for subrepo in $autotools_subrepos; do
-  if ! git diff-index --quiet HEAD --; then
+for dir in $autotools_dirs; do
+  echo "Entering $dir"
+  pushd $dir > /dev/null
+  git checkout master 
+  git branch -D __autotools_update &> /dev/null
+  git checkout -b __autotools_update github/master
+  if git diff-index --quiet HEAD --; then
     ./setup.sh
     git commit -a -m "Regenerate build scripts"
   else
     echo "Not updating scripts: uncommitted changes"
   fi
+  popd > /dev/null
 done
