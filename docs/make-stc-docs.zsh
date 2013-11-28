494f1e2ecde7939755e0f6558732654b082f8c31
Justin Wozniak <wozniak@mcs.anl.gov>
2013-11-28 16:37:46 +0000

    Add more snippets
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@10227 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- docs/make-stc-docs.zsh
+++ docs/make-stc-docs.zsh
@@ -1,6 +1,26 @@
 #!/bin/zsh -e
 
-examples/snippet.pl -n=1 < examples/6/f.c > examples/6/f-snip-1.c
+# MAKE-STC-DOCS
 
+# 1) Extracts snippets for code samples
+# 2) Runs asciidoc
+
+snip()
+{
+  N=$1
+  FILE=$2
+  # Insert -snip-NUMBER into file name
+  OUTPUT=${FILE/\./-snip-${N}.}
+  examples/snippet.pl -n=${N} ${FILE} > ${OUTPUT}
+}
+
+snip 1 examples/6/f.c
+snip 1 examples/8/func.f90
+snip 1 examples/8/prog-f90.f90
+snip 1 examples/8/prog-swift.swift
+
+uptodate swift.html swift.txt || \
 asciidoc --attribute stylesheet=${PWD}/swift.css swift.txt
+
+uptodate leaf.html leaf.txt || \
 asciidoc --attribute stylesheet=${PWD}/swift.css leaf.txt
