c5344538faaf87117112962aefd7b72f0800587f
Tim Armstrong <tim.g.armstrong@gmail.com>
2013-12-18 23:29:12 +0000

    Cleanup and add copyright
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@10432 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/scripts/mkstatic/files2arrays_mkhdr.sh
+++ code/scripts/mkstatic/files2arrays_mkhdr.sh
@@ -1,9 +1,62 @@
 #!/usr/bin/env bash
+
+# Copyright 2013 University of Chicago and Argonne National Laboratory
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+
 # Create C header with an array containing references to all variable names
 # Usage: files2arrays_mkhdr <C array name> <file2array files>
 
-master_arr=$1
-shift
+set -e
+
+master_arr=""
+modifiers=""
+
+usage () {
+  echo "Usage: $0 [ -v <c master array variable name> ]\
+    [ -m <extern array variable modifiers> ]\
+     <input file>* " >&2
+  exit 1
+}
+
+while getopts "v:m:" opt; do
+  case $opt in 
+    v) 
+      if [[ $master_arr != "" ]]; then
+        echo "-v specified twice" >&2
+        usage
+      fi
+      master_arr=$OPTARG
+      ;;
+    m)
+      if [[ $modifiers != "" ]]; then
+        echo "-m specified twice" >&2
+        usage
+      fi
+      modifiers=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      usage
+  esac
+done
+
+shift $((OPTIND - 1))
+
+if [ -z "$modifiers" ]; then
+  # Default is const with global linking visibility
+  modifiers="const"
+fi
 
 count=0
 arrnames=()
@@ -36,8 +89,8 @@ echo
 # Print out header file
 for arrname in "${arrnames[@]}"
 do
-  echo "extern const char $arrname[];"
-  echo "extern const size_t ${arrname}_len;"
+  echo "extern $modifiers char $arrname[];"
+  echo "extern $modifiers size_t ${arrname}_len;"
 done
 echo
 
