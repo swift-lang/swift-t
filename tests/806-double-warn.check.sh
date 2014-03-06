b3220e8d6aba68369bdcb502d94f34b52aa54869
Tim Armstrong <tim.g.armstrong@gmail.com>
2014-03-06 16:36:36 +0000

    Avoid emitting double warning
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@11177 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- tests/806-double-warn.check.sh
+++ tests/806-double-warn.check.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash -ex
+#!/usr/bin/env bash
+set -e
 
 warn_count=$(grep "WARN" ${STC_ERR_FILE} | wc -l)
 
@@ -7,6 +8,7 @@ echo "Warnings: $warn_count"
 if (( warn_count != 1 ))
 then
   echo "Expected only 1 warning in ${STC_ERR_FILE} but got ${warn_count}"
+  cat ${STC_ERR_FILE}
   exit 1
 fi
 
