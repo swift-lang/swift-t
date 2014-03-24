a7725d6eaf13b1af909b10bc638ac96ad78ab5ac
Tim Armstrong <tim.g.armstrong@gmail.com>
2014-03-24 14:47:24 +0000

    Default to transferring only read refcount with container reference
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/branches/issue-586@11422 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/src/tcl/adlb/tcl-adlb.c
+++ code/src/tcl/adlb/tcl-adlb.c
@@ -3653,7 +3653,7 @@ ADLB_Reference_Impl(ClientData cdata, Tcl_Interp *interp,
   TCL_CHECK(rc);
 
   // TODO: optionally take num of read/write references to transfer
-  adlb_refcounts transfer_rc = ADLB_READWRITE_RC;
+  adlb_refcounts transfer_rc = ADLB_READ_RC;
 
   // DEBUG_ADLB("adlb::container_reference: <%"PRId64">[%s] => <%"PRId64">\n",
   //            id, subscript, reference);
