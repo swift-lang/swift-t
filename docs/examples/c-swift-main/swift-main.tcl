206c71170597f773835ad75bd002f7c8eb8f9cac
Justin Wozniak <wozniak@mcs.anl.gov>
2014-03-25 18:37:44 +0000

    Order arguments
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@11458 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- docs/examples/c-swift-main/swift-main.tcl
+++ docs/examples/c-swift-main/swift-main.tcl
@@ -8,12 +8,16 @@ namespace eval swift_main {
     proc swift_main_wrap_impl { rc A } {
 
         set length [ adlb::container_size $A ]
-        set tds [ adlb::enumerate $A members all 0 ]
+        set tds [ adlb::enumerate $A dict all 0 ]
         set argv [ list ]
-        for { set i 0 } { $i < $length } { incr i } {
-            set td [ lindex $tds $i ]
+        # Fill argv with blanks
+        dict for { i td } $tds {
+            lappend argv 0
+        }
+        # Set values at ordered list positions
+        dict for { i td } $tds {
             set s [ adlb::retrieve $td ]
-            lappend argv $s
+            lset argv $i $s
         }
         set rc_value [ swift_main_extension {*}$argv ]
         turbine::store_integer $rc $rc_value
