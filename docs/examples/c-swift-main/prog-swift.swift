58657c8fafbe7103b544fa3f6a87b758183afd6e
Ketan Maheswari <ketancmaheshwari@gmail.com>
2014-03-26 15:26:02 +0000

    edits to about and prog-swift
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@11472 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- docs/examples/c-swift-main/prog-swift.swift
+++ docs/examples/c-swift-main/prog-swift.swift
@@ -1,11 +1,13 @@
 
 import io;
-
+/*
+ret_type swift_app_func() pkg_name version tcl_func
+*/
 (int v) swift_main(string A[]) "swift_main" "0.0" "swift_main_wrap";
 
 main
 {
   string A[] = [ "arg1", "arg2", "arg3" ];
-  rc = swift_main(A);
+  rc = swift_main(A); // rc gets its type implicitly
   printf("exit code: %i", rc);
 }
