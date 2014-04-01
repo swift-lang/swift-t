0e2060cf4eef0dc01e207beb330b4443828e1b3d
Tim Armstrong <tim.g.armstrong@gmail.com>
2014-04-01 22:39:11 +0000

    Move turbine engine code to adlb and get to compile
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/lb/branches/issue-586-engine@11627 dc4e9af1-7f46-4ead-bba6-71afc04862de

---
+++ code/src/turbine.d
@@ -0,0 +1,65 @@
+src/turbine.d src/turbine.o: src/turbine.c /usr/include/stdc-predef.h \
+ /usr/include/x86_64-linux-gnu/bits/predefs.h /usr/include/assert.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h /usr/include/stdio.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include/stddef.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h /usr/include/libio.h \
+ /usr/include/_G_config.h /usr/include/wchar.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include/stdarg.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/sys_errlist.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio2.h /usr/include/stdlib.h \
+ /usr/include/x86_64-linux-gnu/bits/waitflags.h \
+ /usr/include/x86_64-linux-gnu/bits/waitstatus.h /usr/include/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap-16.h \
+ /usr/include/x86_64-linux-gnu/sys/types.h /usr/include/time.h \
+ /usr/include/x86_64-linux-gnu/sys/select.h \
+ /usr/include/x86_64-linux-gnu/bits/select.h \
+ /usr/include/x86_64-linux-gnu/bits/sigset.h \
+ /usr/include/x86_64-linux-gnu/bits/time.h \
+ /usr/include/x86_64-linux-gnu/bits/select2.h \
+ /usr/include/x86_64-linux-gnu/sys/sysmacros.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h /usr/include/alloca.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-float.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib.h /usr/include/string.h \
+ /usr/include/xlocale.h /usr/include/x86_64-linux-gnu/bits/string.h \
+ /usr/include/x86_64-linux-gnu/bits/string2.h \
+ /usr/include/x86_64-linux-gnu/bits/string3.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h /usr/include/inttypes.h \
+ src/adlb.h /usr/include/unistd.h \
+ /usr/include/x86_64-linux-gnu/bits/posix_opt.h \
+ /usr/include/x86_64-linux-gnu/bits/environments.h \
+ /usr/include/x86_64-linux-gnu/bits/confname.h /usr/include/getopt.h \
+ /usr/include/x86_64-linux-gnu/bits/unistd.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include/stdbool.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include-fixed/limits.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include-fixed/syslimits.h \
+ /usr/include/limits.h /usr/include/x86_64-linux-gnu/bits/posix1_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/local_lim.h \
+ /usr/include/linux/limits.h \
+ /usr/include/x86_64-linux-gnu/bits/posix2_lim.h \
+ /home/tim/ExM/inst/mpich2/include/mpi.h \
+ /home/tim/ExM/inst/mpich2/include/mpio.h \
+ /home/tim/ExM/inst/mpich2/include/mpi.h src/adlb-defs.h \
+ /home/tim/ExM/inst/c-utils/include/list_i.h \
+ /home/tim/ExM/inst/c-utils/include/list.h \
+ /home/tim/ExM/inst/c-utils/include/list_l.h \
+ /home/tim/ExM/inst/c-utils/include/c-utils-types.h \
+ /home/tim/ExM/inst/c-utils/include/version.h \
+ /home/tim/ExM/inst/c-utils/include/c-utils.h \
+ /home/tim/ExM/inst/c-utils/include/log.h \
+ /home/tim/ExM/inst/c-utils/include/table.h \
+ /home/tim/ExM/inst/c-utils/include/table_bp.h \
+ /home/tim/ExM/inst/c-utils/include/binkeys.h \
+ /home/tim/ExM/inst/c-utils/include/jenkins-hash.h \
+ /home/tim/ExM/inst/c-utils/include/table_lp.h \
+ /home/tim/ExM/inst/c-utils/include/tools.h src/turbine.h \
+ src/turbine-defs.h src/adlb-defs.h src/adlb_types.h src/debug.h config.h \
+ src/common.h src/adlb_types.h
---
+++ code/src/turbine.h
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2013 University of Chicago and Argonne National Laboratory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+/**
+ *  TURBINE
+ *
+ *  Created on: May 4, 2011
+ *      Author: wozniak
+ * */
+
+#ifndef TURBINE_H
+#define TURBINE_H
+
+#include <mpi.h>
+
+#include <turbine-defs.h>
+
+typedef enum
+{
+  /** No action */
+  TURBINE_ACTION_NULL = 0,
+  /** Act locally */
+  TURBINE_ACTION_LOCAL = 1,
+  /** Act on a remote engine */
+  TURBINE_ACTION_CONTROL = 2,
+  /** Act on a worker */
+  TURBINE_ACTION_WORK = 3
+} turbine_action_type;
+
+typedef int64_t turbine_transform_id;
+
+typedef struct {
+  char *key;
+  size_t length;
+} turbine_subscript;
+
+static const turbine_subscript TURBINE_NO_SUB = { .key = NULL, .length = 0 };
+
+typedef struct {
+  turbine_datum_id td;
+  turbine_subscript subscript;
+} td_sub_pair;
+
+/**
+   If the user parallel task is being released, this
+   
+   if (rc will be set to the communicator to use.
+   If task is not parallel, this is MPI_COMM_SELF
+*/
+extern MPI_Comm turbine_task_comm;
+
+turbine_code turbine_init(int amserver, int rank, int size);
+
+turbine_code turbine_engine_init(void);
+
+extern bool turbine_engine_initialized;
+static inline bool turbine_is_engine(void) {
+  return turbine_engine_initialized;
+}
+
+/**
+   @param id Output: the ID of the new rule
+   @return TURBINE_SUCCESS/TURBINE_ERROR_*
+           On error, id is undefined
+ */
+turbine_code turbine_rule(const char* name,
+                          int inpbt_tds,
+                          const turbine_datum_id* input_td_list,
+                          int input_td_subs,
+                          const td_sub_pair* input_td_sub_list,
+                          turbine_action_type action_type,
+                          const void* action,
+                          size_t action_len,
+                          int priority,
+                          int target,
+                          int parallelism,
+                          turbine_transform_id* id);
+
+turbine_code turbine_rules_push(void);
+
+/*
+  Should be called when turbine engine is notified that an id is closed
+ */
+turbine_code turbine_close(turbine_datum_id id);
+
+/*
+  Should be called when turbine engine is notified that an id/subscript
+  is closed
+ */
+turbine_code turbine_sub_close(turbine_datum_id id, const void *subscript,
+                               size_t subscript_len);
+
+/*
+  action_type: this is TURBINE_ACTION_NULL if no ready actions.
+               otherwise it will have the type of action and
+               the output arguments will be filled in
+  action: the string action.  Caller is responsible for freeing
+ */
+turbine_code turbine_pop(turbine_action_type* action_type,
+                         turbine_transform_id *id,
+                         void** action, size_t *action_len,
+                         int* priority, int* target,
+                         int* parallelism);
+
+#define TURBINE_CODE_STRING_MAX 64
+
+/*
+  Convert code to string.
+  output: buffer of at least TURBINE_CODE_STRING_MAX bytes
+ */
+int turbine_code_tostring(char* output, turbine_code code);
+
+void turbine_finalize(void);
+
+// Temporary hack to get working ok
+// TODO: move elsewhere
+#include "debug.h"
+#define DEBUG_TURBINE(s, args...) DEBUG("ENGINE:" s, ## args)
+
+#endif
