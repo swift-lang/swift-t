67d34744b7fc6dd2044ae9dd322eb5250b5f25b8
Justin Wozniak <wozniak@mcs.anl.gov>
2014-04-03 18:37:14 +0000

    Correct error message retrieval
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@11740 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- tests/jenkins-results.zsh
+++ tests/jenkins-results.zsh
@@ -60,13 +60,32 @@ do
     message "Status: $line"
     print "  <testcase name=\"${T_swift}\" >"
     print "     <failure type=\"generic\">"
-    print "     ${T_swift}   ${T_tcl}"
-    print "OUTPUT from ${T_swift%.swift}.out"
-    print "<![CDATA["
-    cat  ${T_swift%.swift}.out
-    print "]]>"
-    print "OUTPUT from ${T_swift%.swift}.stc.out"
-    cat  ${T_swift%.swift}.stc.out
+    # print "Error case: ${T_swift}"
+
+    STC_OUTPUT=${T_swift%.swift}.stc.out
+    STC_ERROR=${T_swift%.swift}.stc.err
+
+    # STC stdout from failed case
+    print "STC output from ${T_swift}:"
+    cat ${STC_OUTPUT}
+    print
+
+    # STC stderr from failed case
+    print "STC error from ${T_swift}:"
+    cat ${STC_ERROR}
+    print
+
+    # stdout from failed run
+    TURBINE_OUTPUT=${T_swift%.swift}.out
+    if [[ -f ${TURBINE_OUTPUT} ]]
+    then
+      print "Turbine output from ${T_swift}:"
+      print "<![CDATA["
+      cat ${OUTPUT}
+      print "]]>"
+    else
+      print NOT HERE
+    fi
     print "     </failure> "
     print "  </testcase>"
   fi
