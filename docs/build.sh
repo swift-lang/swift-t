a7adca6ca3e7ec1c9486479b63a94f80efffa373
Ketan Maheswari <ketancmaheshwari@gmail.com>
2014-04-07 19:49:50 +0000

    adding width limits in asciidoc compile lines
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@11824 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- docs/build.sh
+++ docs/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-asciidoc --attribute stylesheet=${PWD}/swift.css swift.txt
-asciidoc --attribute stylesheet=${PWD}/swift.css internals.txt
+asciidoc --attribute stylesheet=${PWD}/swift.css -a max-width=750px -a textwidth=80 swift.txt
+asciidoc --attribute stylesheet=${PWD}/swift.css -a max-width=750px -a textwidth=80 internals.txt
