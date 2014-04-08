94af7a985910944ebadb65fab669c6d335655788
Ketan Maheswari <ketancmaheshwari@gmail.com>
2014-04-08 22:21:47 +0000

    genleaf improvement
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@11922 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- docs/examples/5/extension.c
+++ docs/examples/5/extension.c
@@ -1,24 +1,20 @@
-
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <tcl.h>
 
-#include "swift-main.h"
+#include "ex2.h"
 
-static int
-swift_main_extension(ClientData cdata, Tcl_Interp *interp,
-                     int objc, Tcl_Obj*const objv[])
-{
+static int leaf_main_extension(ClientData cdata, Tcl_Interp *interp,
+                     int objc, Tcl_Obj*const objv[]) {
   // Create argc/argv from Tcl objects:
   int argc = objc;
-  char** argv = malloc(argc * sizeof(argv));
+  char** argv = (char**) malloc(argc * sizeof(argv));
   for (int i = 0; i < argc; i++)
     argv[i] = Tcl_GetString(objv[i]);
 
   // Call the user function:
-  int rc = swift_main(argc, argv);
+  int rc = leaf_main(argc, argv);
 
   // Return the exit code as the Tcl return code:
   Tcl_Obj* result = Tcl_NewIntObj(rc);
@@ -28,15 +24,13 @@ swift_main_extension(ClientData cdata, Tcl_Interp *interp,
   free(argv);
 }
 
-int
-Swift_main_Init(Tcl_Interp* interp)
-{
+int Userlib_Init(Tcl_Interp* interp) {
   int rc;
 
-  Tcl_PkgProvide(interp, "swift_main", "0.0");
+  Tcl_PkgProvide(interp, "leaf_main", "0.0");
 
   Tcl_CreateObjCommand(interp,
-                       "swift_main_extension", swift_main_extension,
+                       "leaf_main_extension", leaf_main_extension,
                        NULL, NULL);
   return TCL_OK;
 }
--- docs/examples/5/make-package.tcl
+++ docs/examples/5/make-package.tcl
@@ -1,8 +1,7 @@
-
-set name     swift_main
+set name     leaf_main
 set version  0.0
-set leaf_so  libswift_main.so
-set leaf_tcl swift-main.tcl
+set leaf_so  userlib.so
+set leaf_tcl user-leaf.tcl
 
-puts [ ::pkg::create -name $name -version $version \
-           -load $leaf_so -source $leaf_tcl ]
+puts [ ::pkg::create -name $name -version $version            -load $leaf_so -source $leaf_tcl ]
+
