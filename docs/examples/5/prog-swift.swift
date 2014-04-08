844fd7c3534f0ba823f689cdb404e8d54023c726
Justin Wozniak <wozniak@mcs.anl.gov>
2014-04-08 16:57:19 +0000

    Whitespace
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@11886 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- docs/examples/5/prog-swift.swift
+++ docs/examples/5/prog-swift.swift
@@ -1,13 +1,7 @@
-
 import io;
-/*
-ret_type swift_app_func() pkg_name version tcl_func
-*/
 (int v) swift_main(string A[]) "swift_main" "0.0" "swift_main_wrap";
-
-main
-{
+main {
   string A[] = [ "arg1", "arg2", "arg3" ];
-  rc = swift_main(A); // rc gets its type implicitly
+  rc = swift_main(A);
   printf("exit code: %i", rc);
 }
