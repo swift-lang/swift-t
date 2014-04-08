da21bec00d48a797a5baae559eb3d2b756896d02
Ketan Maheswari <ketancmaheshwari@gmail.com>
2014-04-08 02:14:20 +0000

    slight name change
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@11854 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- docs/examples/c-swift-main/ex2/make-package.tcl
+++ docs/examples/c-swift-main/ex2/make-package.tcl
@@ -2,7 +2,7 @@
 set name     power_main
 set version  0.0
 set leaf_so  libpower_main.so
-set leaf_tcl power-main.tcl
+set leaf_tcl power-leaf.tcl
 
 puts [ ::pkg::create -name $name -version $version \
            -load $leaf_so -source $leaf_tcl ]
