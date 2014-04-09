275989d64f5914560ec1e2a6520e24308b28cb4d
Tim Armstrong <tim.g.armstrong@gmail.com>
2014-04-09 05:18:32 +0000

    DFS and BFS implementations for UTS
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/branches/issue-586@11931 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- bench/pact-suite/uts/uts-src/uts_leaf.c
+++ bench/pact-suite/uts/uts-src/uts_leaf.c
@@ -7,29 +7,90 @@
 
 // TODO: inline functions here?
 
-// Max stack size before returning
-#define STACK_RETURN_THRESHOLD 1024
-
-// Max nodes to process before returning
-#define MAX_NODES_PROCESSED (1024 * 1024)
+// Max node count before returning
+#define MAX_NODE_RETURN_THRESHOLD (10 * 1024)
 
 // Max nodes we'll hold in memory before returning
-#define MAX_STACK_SIZE (STACK_RETURN_THRESHOLD + MAXNUMCHILDREN)
+#define NODE_ARRAY_SIZE (MAX_NODE_RETURN_THRESHOLD + MAXNUMCHILDREN)
 
 #define COMPUTE_GRANULARITY 1
 
-static struct node_t node_stack[MAX_STACK_SIZE];
+static struct node_t nodes[NODE_ARRAY_SIZE];
 
 // TODO: Tcl wrapper function
 
-/*
- * Perform step
- * TODO: dfs versus bfs
- * node_stack_size: output, number of valid nodes in node_stack
- */
-static bool uts_step(struct node_t *init_node,
-                    int *node_stack_size)
+static inline void uts_child(struct node_t *child, int child_ix,
+                        int parent_height, int parent_type,
+                        struct state_t *parent_state)
 {
+  for (int i = 0; i < COMPUTE_GRANULARITY; i++)
+  {
+    rng_spawn(parent_state->state, child->state.state, child_ix);
+  }
+  child->height = parent_height + 1;
+  child->type = uts_child_type_inline(parent_height);
+}
+
+/*
+ * Perform step doing bfs tranversal
+ * nodes_size: output, number of valid nodes in nodes
+ */
+static bool uts_step_bfs(struct node_t *init_node,
+    int max_nodes, int max_steps, int *head, int *tail)
+{
+  if (max_nodes > MAX_NODE_RETURN_THRESHOLD)
+  {
+    max_nodes = MAX_NODE_RETURN_THRESHOLD;
+  }
+  int processed = 0;
+  int n = 0; // Node count in queue
+  int h = 0; // Queue head node
+
+  struct node_t *curr = init_node;
+  do
+  {
+    int height = curr->height;
+    int type = curr->type;
+    int num_children = uts_numChildren(curr);
+    assert(num_children <= MAXNUMCHILDREN);
+
+    struct state_t state = curr->state;
+
+    // curr is unused from here onwards, can safely clobber space in stack
+    for (int i = 0; i < num_children; i++)
+    {
+      struct node_t *child = &nodes[(h + n) % NODE_ARRAY_SIZE];
+      uts_child(child, i, height, type, &state);
+      n++;
+    }
+
+    processed++;
+
+    // Pop next node off queue head
+    curr = &nodes[h]; // Invalid if n < 0
+    h = (h + 1) % NODE_ARRAY_SIZE;
+    n--;
+  }
+  while (n >= 0 && n < max_nodes &&
+         processed < max_steps);
+
+  // Successful exit
+  *head = h;
+  *tail = (h + n) % NODE_ARRAY_SIZE;
+  return true;
+}
+
+/*
+ * Perform step doing dfs tranversal
+ * nodes_size: output, number of valid nodes in nodes
+ */
+static bool uts_step_dfs(struct node_t *init_node, int max_nodes,
+    int max_steps, int *nodes_size)
+{
+  if (max_nodes > MAX_NODE_RETURN_THRESHOLD)
+  {
+    max_nodes = MAX_NODE_RETURN_THRESHOLD;
+  }
   int n = 0; // Node count in stack
   int processed = 0;
 
@@ -46,25 +107,20 @@ static bool uts_step(struct node_t *init_node,
     // curr is unused from here onwards, can safely clobber space in stack
     for (int i = 0; i < num_children; i++)
     {
-      struct node_t *child = &node_stack[n++];
-      for (int j = 0; j < COMPUTE_GRANULARITY; j++)
-      {
-        rng_spawn(state.state, child->state.state, i);
-      }
-      child->height = height + 1;
-      child->type = uts_child_type_inline(height);
+      struct node_t *child = &nodes[n++];
+      uts_child(child, i, height, type, &state);
     }
 
     processed++;
 
     // Pop next node off stack to process
     n--;
-    curr = &node_stack[n]; // Invalid if n < 0
+    curr = &nodes[n]; // Invalid if n < 0
   }
-  while (n >= 0 && n < STACK_RETURN_THRESHOLD &&
-         processed < MAX_NODES_PROCESSED);
+  while (n >= 0 && n < max_nodes &&
+         processed < max_steps);
 
   // Successful exit
-  *node_stack_size = n;
+  *nodes_size = n;
   return true;
 }
