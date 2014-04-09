54b1ba5839f3e9da9f55a01efd845c9aea64fc73
Ketan Maheswari <ketancmaheshwari@gmail.com>
2014-04-09 22:08:03 +0000

    path fix
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@11961 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- docs/examples/5/rna/build-tukey.sh
+++ docs/examples/5/rna/build-tukey.sh
@@ -1,4 +1,4 @@
-TCL_INSTALL=/lustre/beagle/wozniak/Public/tcl-8.5.11
+TCL_INSTALL=/home/wozniak/Public/sfw/x86/tcl-8.5.12
 g++ -I $TCL_INSTALL/include/ -fPIC -g -c extension.cpp
 g++ -fopenmp -fPIC -c thfribo-main.cpp
 g++ -fopenmp -shared -o libthfribo_main.so thfribo-main.o extension.o -L $TCL_INSTALL/lib/ -l tcl8.5 -Wl,-rpath -Wl,$TCL_INSTALL/lib 
