8ce567d767e0c33413e9cf46b9abbe8aa1bdfbd8
Tim Armstrong <tim.g.armstrong@gmail.com>
2014-04-09 15:11:57 +0000

    Build script for embarrassing benchmark
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/branches/issue-586@11944 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- bench/pact-suite/embarrassing/build.sh
+++ bench/pact-suite/embarrassing/build.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+set -e
+
 INST=$HOME/ExM/inst
+TURBINE=$INST/turbine
 LB=$INST/lb
 CUTILS=$INST/c-utils
 
-mpicc -std=c99 -O2 -L $LB/lib -L $CUTILS/lib -I $LB/include -I $CUTILS/include -o embarrassing embarrassing.c -ladlb -lexmcutils -Wl,-rpath,$LB/lib,-rpath,$CUTILS/lib
+source $TURBINE/scripts/turbine-build-config.sh
+
+CC=mpicc
+CFLAGS="-std=c99 -O2 ${TURBINE_INCLUDES}"
+LDFLAGS="${TURBINE_LIBS} ${TURBINE_RPATH}"
+
+MKSTATIC=$TURBINE/scripts/mkstatic/mkstatic.tcl
+
+${CC} ${CFLAGS} embarrassing.c  ${LDFLAGS} -o embarrassing 
+
+${MKSTATIC} embarrassing_lognorm.manifest -c embarrassing_lognorm_tcl.c
+${CC} ${CFLAGS} embarrassing_lognorm_tcl.c  ${LDFLAGS} -o embarrassing_lognorm_tcl
+
+echo "OK."
