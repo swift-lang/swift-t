e0246e8447c9104a61490bbb6a19afc0e7007a2f
Tim Armstrong <tim.g.armstrong@gmail.com>
2014-04-10 19:20:37 +0000

    Rewrite fib to use ADLB_Put_rule
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/branches/issue-586@12020 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- bench/pact-suite/fib/fib.cpp
+++ bench/pact-suite/fib/fib.cpp
@@ -15,7 +15,6 @@ extern "C" {
 
 #include <unistd.h>
 
-#include <map>
 #include <utility>
 using namespace std;
 
@@ -23,29 +22,9 @@ using namespace std;
 #define WORKT 0
 #define CONTROL 1
 
-typedef struct {
-  adlb_datum_id fn;
-  adlb_datum_id fn1;
-  adlb_datum_id fn2;
-  bool got1, got2;
-} fib_blocked;
-
-
 void mystore(adlb_datum_id id, long val) {
-    int *ranks;
-    int count;
-    //printf("<%ld> = %ld\n", id, val);
     ADLB_Store(id, ADLB_NO_SUB, ADLB_DATA_TYPE_INTEGER,
                &val, sizeof(long), ADLB_WRITE_RC, ADLB_NO_RC);
-    if (count > 0) {
-      char buf[128];
-      int len = sprintf(buf, "close %ld", id);
-      for (int r = 0; r < count; r++) {
-        int rank = ranks[r];
-        //printf("Notify %i about %ld\n", rank, id);
-        ADLB_Put(buf, len+1, rank, -1, CONTROL, 1, 1);
-      }
-    }
 }
 
 adlb_datum_id spawnfib(int N) {
@@ -59,30 +38,45 @@ adlb_datum_id spawnfib(int N) {
 }
 
 void spawnfib2(adlb_datum_id *f1, adlb_datum_id *f2, int N1, int N2) {
+      int rc;
       char buf[1024];
-      adlb_datum_id ids[2];
       ADLB_create_spec specs[2];
       for (int i = 0; i < 2; i++) {
         specs[i].id = ADLB_DATA_ID_NULL;
         specs[i].type = ADLB_DATA_TYPE_INTEGER;
         specs[i].props = DEFAULT_CREATE_PROPS;
       }
-      ADLB_Multicreate(specs, 2);
+      rc = ADLB_Multicreate(specs, 2);
+      assert(rc == ADLB_SUCCESS);
       int len = sprintf(buf, "fib %i %li\n", N1, specs[0].id);
-      ADLB_Put(buf, len+1, ADLB_RANK_ANY, -1, CONTROL, 1, 1);
+      rc = ADLB_Put(buf, len+1, ADLB_RANK_ANY, -1, CONTROL, 1, 1);
+      assert(rc == ADLB_SUCCESS);
       len = sprintf(buf, "fib %i %li\n", N2, specs[1].id);
-      ADLB_Put(buf, len+1, ADLB_RANK_ANY, -1, CONTROL, 1, 1);
+      rc = ADLB_Put(buf, len+1, ADLB_RANK_ANY, -1, CONTROL, 1, 1);
+      assert(rc == ADLB_SUCCESS);
       *f1 = specs[0].id;
       *f2 = specs[1].id;
 }
 
-// True if already present
-bool subscribe(adlb_datum_id id) {
-  int subscribed;
-  adlb_code code = ADLB_Subscribe(id, ADLB_NO_SUB, &subscribed);
-  assert(code == ADLB_SUCCESS);
-  return subscribed == 0;
+void spawnadd(adlb_datum_id result, adlb_datum_id f1, adlb_datum_id f2)
+{
+  char buf[1024];
+  adlb_datum_id wait_ids[] = {f1, f2};
+  int len = sprintf(buf, "add %li %li %li\n", result, f1, f2);
+  int rc = ADLB_Put_rule(buf, len+1, ADLB_RANK_ANY, -1, CONTROL, 1, 1,
+                         "", wait_ids, 2, NULL, 0);
+  assert(rc == ADLB_SUCCESS);
 }
+
+void spawnfin(adlb_datum_id result)
+{
+  char buf[1024];
+  int len = sprintf(buf, "fin %li\n", result);
+  int rc = ADLB_Put_rule(buf, len+1, ADLB_RANK_ANY, -1, CONTROL, 1, 1,
+                         "", &result, 1, NULL, 0);
+  assert(rc == ADLB_SUCCESS);
+}
+
 
 long getnum(adlb_datum_id id) {
           long result_val;
@@ -97,19 +91,15 @@ long getnum(adlb_datum_id id) {
 
 int main(int argc, char *argv[])
 {
-  FILE *fp;
-  int rc, i, done;
-  char c, cmdbuffer[1024];
+  int rc, done;
 
-  int am_server, am_debug_server;
-  int num_servers, use_debug_server, aprintf_flag;
+  int am_server;
+  int num_servers;
   MPI_Comm app_comm;
   int my_world_rank, my_app_rank;
 
   int num_types = 2;
   int type_vect[2] = {WORKT, CONTROL};
-
-  int quiet = 1;
 
   double start_time, end_time;
 
@@ -121,12 +111,10 @@ int main(int argc, char *argv[])
 
   MPI_Comm_rank( MPI_COMM_WORLD, &my_world_rank );
 
-  aprintf_flag = 0;		/* no output from adlb itself */
   num_servers = 1;		/* one server should be enough */
   if (getenv("ADLB_SERVERS") != NULL) {
     num_servers = atoi(getenv("ADLB_SERVERS"));
   }
-  use_debug_server = 0;		/* default: no debug server */
   rc = ADLB_Init(num_servers, num_types, type_vect, &am_server, MPI_COMM_WORLD, &app_comm);
   if ( !am_server ) /* application process */
   {
@@ -143,8 +131,6 @@ int main(int argc, char *argv[])
   }
   else
   {
-    map<long, fib_blocked*> waitmap;
-    adlb_datum_id result = ADLB_DATA_ID_NULL;
     if (argc != 2 && argc != 3) {
       printf("usage: %s <n> <sleep>", argv[0]);
       ADLB_Fail(-1);
@@ -158,11 +144,7 @@ int main(int argc, char *argv[])
     }
 
     if ( my_app_rank == 0 ) {
-      result = spawnfib(N);
-      if (subscribe(result)) {
-        printf("Finished strangely early...\n");
-        exit(1);
-      }
+      spawnfin(spawnfib(N));
     }
   
     done = 0;
@@ -190,60 +172,21 @@ int main(int argc, char *argv[])
           mystore(id, 0); 
         } else if (i == 1) {
           mystore(id, 1);
-          long f = i;
         } else {
           adlb_datum_id f1, f2;
           spawnfib2(&f1, &f2, i - 1, i - 2);
-          fib_blocked *entry = (fib_blocked*)malloc(sizeof(fib_blocked));
-          entry->fn = id;
-          entry->fn1 = f1;
-          entry->fn2 = f2;
-          entry->got1 = subscribe(f1);
-          entry->got2 = subscribe(f2);
-          
-          if (entry->got1 && entry->got2) {
-            long val1 = getnum(entry->fn1);
-            long val2 = getnum(entry->fn2);
-            if (sleep > 0.0) {
-                usleep((long)(sleep * 1000000));
-            }
-            mystore(entry->fn, val1 + val2);
-            //printf("Subscribed right away: %ld + %ld = %ld\n", val1, val2, val1 + val2);
-            free(entry);
-          } else {
-            waitmap[f1] = entry;
-            waitmap[f2] = entry;
-          }
+          spawnadd(id, f1, f2);
         }
-      } else if (strncmp(cmdbuffer, "close ", 5) == 0) {
+      } else if (strncmp(cmdbuffer, "add ", 4) == 0) {
+        adlb_datum_id dst, src1, src2;
+        sscanf(cmdbuffer, "add %li %li %li", &dst, &src1, &src2);
+        long val1 = getnum(src1);
+        long val2 = getnum(src2);
+        mystore(dst, val1 + val2);
+      } else if (strncmp(cmdbuffer, "fin ", 4) == 0) {
         adlb_datum_id id;
-        sscanf(cmdbuffer, "close %li", &id);
-        if (id == result) {
-          printf("Fib(%i) = %ld\n", N, getnum(id));
-        } else {
-          fib_blocked *entry = waitmap[id];
-          if (entry == NULL) {
-            printf("Rank %i Unknown entry %ld\n", my_app_rank, id);
-            exit(1);
-          }
-          if (entry->fn1 == id) {
-            entry->got1 = true;
-          }
-          if (entry->fn2 == id) {
-            entry->got2 = true;
-          }
-          if (entry->got1 && entry->got2) {
-            long val1 = getnum(entry->fn1);
-            long val2 = getnum(entry->fn2);
-            if (sleep > 0.0) {
-                usleep((long)(sleep * 1000000));
-            }
-            mystore(entry->fn, val1 + val2);
-            //printf("Later: %ld + %ld = %ld\n", val1, val2, val1 + val2);
-            waitmap.erase(id);
-            free(entry);
-          }
-        }
+        sscanf(cmdbuffer, "fin %li", &id);
+        printf("Fib(%i) = %ld\n", N, getnum(id));
       } else {
         printf("Unknown buffer %s\n", cmdbuffer);
         exit(1);
