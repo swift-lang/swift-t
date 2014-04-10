27305ac0adb4d37eea7d61b09d54f2825949fed9
Tim Armstrong <tim.g.armstrong@gmail.com>
2014-04-10 17:54:01 +0000

    Update build script
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/branches/issue-586@12012 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- bench/pact-suite/embarrassing/build.sh
+++ bench/pact-suite/embarrassing/build.sh
@@ -12,20 +12,38 @@ source $TURBINE/scripts/turbine-build-config.sh
 
 CC=mpicc
 CFLAGS="-std=c99 -Wall -O2 ${TURBINE_INCLUDES}"
-LDFLAGS="${TURBINE_LIBS} ${TURBINE_RPATH}"
+LDFLAGS=""
+LIBS="${TURBINE_LIBS} ${TURBINE_RPATH}"
 
 MKSTATIC=$TURBINE/scripts/mkstatic/mkstatic.tcl
 
-${CC} ${CFLAGS} embarrassing.c  ${LDFLAGS} -o embarrassing 
-${CC} ${CFLAGS} -D LOGNORM embarrassing.c  ${LDFLAGS} -o embarrassing_lognorm 
+${CC} ${CFLAGS} ${LDFLAGS} embarrassing.c ${LIBS} -o embarrassing 
+${CC} ${CFLAGS} ${LDFLAGS} -D LOGNORM embarrassing.c ${LIBS} -o embarrassing_lognorm 
 
 STC=$INST/stc/bin/stc
-STC_OPTLEVEL=${STC_OPTLEVEL:--O2}
-STC_FLAGS="$STC_OPTLEVEL"
-STC_FLAGS+=" -T no-engine"
+STC=$INST/stc/bin/stc
 
-${STC} ${STC_FLAGS} -C embarrassing_lognorm.ic embarrassing_lognorm.swift
-${MKSTATIC} embarrassing_lognorm.manifest -c embarrassing_lognorm_tcl.c
-${CC} ${CFLAGS} embarrassing_lognorm_tcl.c  ${LDFLAGS} -o embarrassing_lognorm_tcl
+for OPT in 0 1 2 3
+do
+  STC_OPTLEVEL="-O$OPT"
+  STC_FLAGS="$STC_OPTLEVEL"
+  STC_FLAGS+=" -T no-engine"
+  
+  echo -n O$OPT
+  PREFIX=embarrassing_lognorm
+  PREFIX_OPT=${PREFIX}.O${OPT}
+  PREFIX_TCL=${PREFIX}_tcl.O${OPT}
+  ${STC} ${STC_FLAGS} -C ${PREFIX_OPT}.ic ${PREFIX}.swift ${PREFIX_OPT}.tcl
+  echo -n .
+  ${MKSTATIC} ${PREFIX}.manifest --main-script ${PREFIX_OPT}.tcl -c ${PREFIX_TCL}.c
+  echo -n .
+
+  ${CC} ${CFLAGS} ${LDFLAGS} ${PREFIX_TCL}.c \
+    $(${MKSTATIC} ${PREFIX}.manifest --link-objs --link-flags) \
+    ${LIBS} -o ${PREFIX_TCL}
+  echo -n
+
+done
+
 
 echo "OK."
