f126f0f823ab5e8704e6a4aa32b61de6260a9aa4
Tim Armstrong <tim.g.armstrong@gmail.com>
2014-04-10 05:26:38 +0000

    Test script for blue waters
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/branches/issue-586@11983 dc4e9af1-7f46-4ead-bba6-71afc04862de

---
+++ bench/pact-suite/embarrassing/bw-aprun.sh
@@ -0,0 +1,128 @@
+# Copyright 2013 University of Chicago and Argonne National Laboratory
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+
+#PBS -N Swift
+#PBS -q normal
+#PBS -l walltime=1:00:00
+
+### Set the job size using appropriate directives for this system
+### Blue Waters mode
+#PBS -l nodes=4:ppn=32
+### End job size directives selection
+
+#PBS -o ${PBS_JOBID}.pbs.out
+# Merge stdout/stderr
+#PBS -j oe
+# Disable mail
+#PBS -m n
+
+set -x
+
+. /opt/modules/default/init/bash
+module unload PrgEnv-cray
+module load PrgEnv-gnu
+
+# Set variables required for turbine-config.sh
+export TURBINE_HOME=/u/sciteam/tarmstro/soft/exm-sc14/v1/turbine
+TURBINE_STATIC_EXEC=0
+EXEC_SCRIPT=1
+
+# Setup configuration for turbine
+source ${TURBINE_HOME}/scripts/turbine-config.sh
+BENCH_SRC=/mnt/a/u/sciteam/tarmstro/exm.sfw.git/stc/branches/issue-586/bench/pact-suite/embarrassing
+SCRIPT=${BENCH_SRC}/embarrassing_lognorm_tcl
+ADLB_PROG=${BENCH_SRC}/embarrassing_lognorm
+
+# Put output in directory job was launched from
+TURBINE_OUTPUT=${PBS_O_WORKDIR}
+
+export TURBINE_USER_LIB=
+export TURBINE_LOG=0
+export TURBINE_DEBUG=
+export ADLB_DEBUG=
+
+export ADLB_SERVERS=2
+export ADLB_PERF_COUNTERS=1
+export ADLB_PRINT_TIME=1
+
+# For adlb version
+export CONTROL_RATIO=8
+
+# Round-robin placement to ensure servers distributed nicely
+export MPICH_RANK_REORDER_METHOD=0
+
+# Output header
+echo "Turbine: turbine-aprun.sh"
+date "+%m/%d/%Y %I:%M%p"
+echo
+
+NODES=${PBS_NUM_NODES}
+PPN=${PBS_NUM_PPN}
+PROCS=$((NODES * PPN))
+
+# Log the parameters
+echo "TURBINE_HOME: ${TURBINE_HOME}"
+echo "SCRIPT:       ${SCRIPT}"
+echo "ADLB_PROG:    ${ADLB_PROG}"
+echo "PROCS:        ${PROCS}"
+echo "NODES:        ${NODES}"
+echo "PPN:          ${PPN}"
+echo
+echo "ADLB_SERVERS:    ${ADLB_SERVERS}"
+
+# Be sure we are in an accessible directory
+cd ${TURBINE_OUTPUT}
+
+OPT_LEVELS="O0 O1 O2 O3 adlb"
+
+
+for opt in ${OPT_LEVELS}
+do
+  APRUN_NODES=$NODES
+
+  while ((APRUN_NODES > 0))
+  do
+    N=10000
+    M=$((NODES*2500))
+    mu=-8.515
+    sigma=1
+    if [ $opt = adlb ]
+    then
+      ARGS="${N} ${M} ${mu} ${sigma}"
+      PROG=${ADLB_PROG}
+    else
+      ARGS="--N=${N} --M=${M} --mu=${mu} --sigma=${sigma}"
+      PROG=${SCRIPT}.${opt}
+    fi
+    echo "Run ${PROG} with args: ${ARGS}"
+    aprun -n ${PROCS} -N ${PPN} -cc none -d 1 ${TCLSH} ${PROG} ${ARGS} \
+            2>&1 > "${PBS_JOBID}.${opt}.p${PROCS}.aprun.out" &
+
+    if (( APRUN_NODES == NODES ))
+    then
+      # Don't have any free nodes
+      wait
+    fi
+
+    # Launch smaller sized jobs in powers of two
+    # The remainder will add up to one less than the total number of
+    # nodes allocated, so that the remainder will fit in the largest
+    # job's allocate
+    APRUN_NODES=$((APRUN_NODES/2))
+  done
+
+  # Wait for jobs for this opt level
+  wait
+done
+
