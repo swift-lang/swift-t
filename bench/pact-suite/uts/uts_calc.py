828d31bffb526c7d1210d33ed9701f7250542dbf
Tim Armstrong <tim.g.armstrong@gmail.com>
2014-04-10 22:37:46 +0000

    Draft of UTS submit script for blue waters
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/branches/issue-586@12031 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- bench/pact-suite/uts/uts_calc.py
+++ bench/pact-suite/uts/uts_calc.py
@@ -2,7 +2,9 @@
 # Estimate number of nodes for geometric uts with linear shape
 
 b_0 = 4.0
-gen_mx = 22
+gen_mx = 45
+nodes = 8192
+cores_per_node = 32
 
 # level is from 0 onwards
 def level_nodes(level):
@@ -18,4 +20,10 @@ for level in range(gen_mx+1):
   total += this_level_count
 
 print "Total count: %d" % total
-
+node_secs = float(total) / 1e6
+print "Core-seconds (at 1M nodes/sec): %.1f" % (node_secs)
+secs = node_secs / (nodes * cores_per_node)
+print "Seconds (at %d nodes and %d cores/node): %.1f" % \
+      (nodes, cores_per_node, secs )
+mins = secs / 60.0
+print "Minutes: %.1f" % mins
