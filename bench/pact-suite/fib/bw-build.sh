c9e2accd96d86f04eb3a3a5b2b02015030410da2
Tim Armstrong <tim.g.armstrong@gmail.com>
2014-04-10 19:20:35 +0000

    Get fib to build on bw
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/branches/issue-586@12018 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- bench/pact-suite/fib/bw-build.sh
+++ bench/pact-suite/fib/bw-build.sh
@@ -1,8 +1,57 @@
 #!/bin/bash
 
-INST=$HOME/soft/exm-dev
+set -e
+
+INST=$HOME/soft/exm-sc14/v1/
+#INST=$HOME/soft/exm-sc14/v1-debug/
+TURBINE=$INST/turbine
 LB=$INST/lb
 CUTILS=$INST/c-utils
-MPICH_INST=/opt/cray/mpt/default/gni/mpich2-gnu/48/
+STC_INST=$INST/stc
 
-g++ -O2 -std=c++0x -L $LB/lib -L $CUTILS/lib -L $MPICH_INST/lib -I $MPICH_INST/include -I $LB/include -I $CUTILS/include -o fib fib.cpp -lmpich -ladlb -lexmcutils -Wl,-rpath,$LB/lib,-rpath,$CUTILS/lib,-rpath,$MPICH_INST
+source $TURBINE/scripts/turbine-build-config.sh
+
+CC=cc
+CPP=c++
+CFLAGS="-std=c99 -Wall -O2 ${TURBINE_INCLUDES}"
+CPPFLAGS="-std=c++0x -Wall -O2 ${TURBINE_INCLUDES}"
+LDFLAGS=""
+LIBS="${TURBINE_LIBS}"
+
+MKSTATIC=$TURBINE/scripts/mkstatic/mkstatic.tcl
+
+echo -n ADLB
+CPROG=fib
+${CPP} ${CPPFLAGS} ${LDFLAGS} ${CPROG}.cpp ${LIBS} -o ${CPROG}
+echo -n .
+echo .
+
+STC=$STC_INST/bin/stc
+
+for OPT in 0 1 2 3
+do
+  STC_OPTLEVEL="-O$OPT"
+  STC_FLAGS="$STC_OPTLEVEL"
+  STC_FLAGS+=" -T no-engine"
+  
+  echo -n O$OPT
+  PREFIX=fib
+  PREFIX_OPT=${PREFIX}.O${OPT}
+  PREFIX_TCL=${PREFIX}_tcl.O${OPT}
+  ${STC} ${STC_FLAGS} -C ${PREFIX_OPT}.ic ${PREFIX}.swift ${PREFIX_OPT}.tcl
+  echo -n .
+
+  MANIFEST="dummy.manifest --ignore-no-manifest"
+  ${MKSTATIC} ${MANIFEST} --main-script ${PREFIX_OPT}.tcl -c ${PREFIX_TCL}.c
+  echo -n .
+
+  TCL_LDFLAGS="-dynamic" # Can't be linked statically
+  
+  ${CC} ${CFLAGS} ${TCL_LDFLAGS} ${LDFLAGS} ${PREFIX_TCL}.c \
+    $(${MKSTATIC} ${MANIFEST} --link-objs --link-flags) \
+    ${LIBS} -o ${PREFIX_TCL}
+  echo -n
+
+done
+
+echo "DONE"
