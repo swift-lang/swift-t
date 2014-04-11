d792275addfa794a0aebad1e7e9c96f4f79734a0
Ketan Maheswari <ketancmaheshwari@gmail.com>
2014-04-11 16:35:27 +0000

    latest debug version of thfribo
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@12043 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- docs/examples/5/rna/build-vesta.sh
+++ docs/examples/5/rna/build-vesta.sh
@@ -1,6 +1,12 @@
-TCL_INSTALL=/home/wozniak/Public/sfw/x86/tcl-8.5.12
-g++ -I $TCL_INSTALL/include/ -fPIC -g -c extension.cpp
-g++ -fopenmp -fPIC -c thfribo-main.cpp
-g++ -fopenmp -shared -o libthfribo_main.so thfribo-main.o extension.o -L $TCL_INSTALL/lib/ -l tcl8.5 -Wl,-rpath -Wl,$TCL_INSTALL/lib 
-tclsh make-package.tcl > pkgIndex.tcl
-stc -j /home/wozniak/Public/sfw/x86/jdk1.7.0_25/bin/java -r $PWD thfribo-swift.swift
+#!/bin/bash
+
+#set -x
+
+TCL_INSTALL=/home/wozniak/Public/tcl-8.5.12-bgq
+#g++ -I $TCL_INSTALL/include/ -fPIC -g -c extension.cpp
+/bgsys/drivers/ppcfloor/gnu-linux/bin/powerpc64-bgq-linux-g++ -I $TCL_INSTALL/include -fPIC -g -c extension.cpp
+/bgsys/drivers/ppcfloor/gnu-linux/bin/powerpc64-bgq-linux-g++ -fopenmp -fPIC -c thfribo-main.cpp
+#g++ -fopenmp -shared -o libthfribo_main.so thfribo-main.o extension.o -L $TCL_INSTALL/lib/ -l tcl8.5 -Wl,-rpath -Wl,$TCL_INSTALL/lib 
+/bgsys/drivers/ppcfloor/gnu-linux/bin/powerpc64-bgq-linux-g++ -fopenmp -shared -o libthfribo_main.so thfribo-main.o extension.o -L $TCL_INSTALL/lib -l tcl8.5 -Wl,-rpath -Wl,$TCL_INSTALL/lib
+$TCL_INSTALL/bin/tclsh8.5 make-package.tcl > pkgIndex.tcl
+stc -t checkpointing -r $PWD thfribo-swift.swift
