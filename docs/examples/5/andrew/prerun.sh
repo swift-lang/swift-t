0bf8a6201732cebcc9182177ae648ef85f2e053c
Ketan Maheswari <ketancmaheshwari@gmail.com>
2014-04-14 20:27:43 +0000

    return TCL_OK
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@12152 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- docs/examples/5/andrew/prerun.sh
+++ docs/examples/5/andrew/prerun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ln -s file1.txt  $TURBINE_OUTPUT/file1.txt 
-ln -s file2.txt $TURBINE_OUTPUT/file2.txt 
+cp file1.txt  $TURBINE_OUTPUT/file1.txt 
+cp file2.txt $TURBINE_OUTPUT/file2.txt 
 
