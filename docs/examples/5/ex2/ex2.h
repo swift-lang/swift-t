c690cc23862435348366a6a6700ff4577da14f1f
Ketan Maheswari <ketancmaheshwari@gmail.com>
2014-04-16 16:50:44 +0000

    remove example
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@12235 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- docs/examples/5/ex2/ex2.h
+++
@@ -1,2 +0,0 @@
-int power_main(int argc, char** argv);
-int leaf_main(int argc, char** argv);
--- docs/examples/5/ex2/make-package.tcl
+++
@@ -1,7 +0,0 @@
-set name     leaf_main
-set version  0.0
-set leaf_so  userlib.so
-set leaf_tcl user-leaf.tcl
-
-puts [ ::pkg::create -name $name -version $version            -load $leaf_so -source $leaf_tcl ]
-
