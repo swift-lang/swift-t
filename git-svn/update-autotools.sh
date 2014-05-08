bf9d44b21fbbc47a63ed14ac3bf839bcee4402c8
armstron <armstron@dc4e9af1-7f46-4ead-bba6-71afc04862de>
2014-05-08 15:03:51 +0000

    Fix autotools git script ot be more robust
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/dev@12730 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- git-svn/update-autotools.sh
+++ git-svn/update-autotools.sh
@@ -5,6 +5,9 @@ set -e
 SCRIPT_DIR=$(dirname $0)
 source "${SCRIPT_DIR}/repos.sh"
 
+CHANGES=0
+ERROR=0
+
 for dir in $autotools_dirs; do
   echo "Entering $dir"
   pushd $dir > /dev/null
@@ -13,11 +16,35 @@ for dir in $autotools_dirs; do
     echo "Removed old autotools temporary branch"
   fi
   git checkout -q -b __autotools_update github/master
-  if git diff-index --quiet HEAD --; then
+  if git diff-index --exit-code HEAD ; then
     ./setup.sh
-    git commit -a -m "Regenerate build scripts"
+    if git diff --exit-code --name-status ; then
+      echo "No changes in generated scripts"
+    else
+      git commit -a -m "Regenerate build scripts"
+      CHANGES=1
+    fi
   else
     echo "Not updating scripts: uncommitted changes"
+    ERROR=1
   fi
   popd > /dev/null
+
+  echo
 done
+
+echo
+
+if (( CHANGES )) ; then
+  echo "Some generated files changed.  You can push them now."
+fi
+
+if (( ERROR )) ; then
+  echo "There was at least one error encountered previously."
+fi
+
+if (( !CHANGES && !ERROR )) ; then
+  echo "No changes in generated files and no errors.  No need to push anything."
+fi
+
+echo DONE
