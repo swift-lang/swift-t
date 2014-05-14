4839b966474dca17c6838ea2773b906439f6aa95
Tim Armstrong <tim.g.armstrong@gmail.com>
2014-05-14 16:17:14 +0000

    Extend test for empty globs
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@12768 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- tests/563-glob.swift
+++ tests/563-glob.swift
@@ -2,6 +2,7 @@
 import io;
 import files;
 import sys;
+import assert;
 
 //usage: stc 563-glob.swift -S=/home/zzhang/*.swift
 main
@@ -12,4 +13,8 @@ main
   {
     printf("file: %s", filename(f));	
   }
+
+  file t[];
+  t = glob("A.pattern.that.does.not.match.anything.*");
+  assertEqual(size(t), 0, "size(t)");
 }
