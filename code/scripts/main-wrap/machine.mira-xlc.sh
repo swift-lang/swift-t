943873956eadb31389cc0f5698eb3d2cf731f6bf
Justin Wozniak <wozniak@mcs.anl.gov>
2014-06-04 17:25:02 +0000

    Move genleaf to Turbine scripts location
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@12968 dc4e9af1-7f46-4ead-bba6-71afc04862de

---
+++ code/scripts/main-wrap/machine.mira-xlc.sh
@@ -0,0 +1,12 @@
+
+# MIRA XLC
+
+TCL_HOME=/home/wozniak/Public/sfw/ppc64/tcl-8.5.12
+TCL_VERSION=8.5
+
+CC=bgxlc
+STC=/home/wozniak/Public/sfw/ppc64/stc/bin/stc
+
+TURBINE_HOME=/home/wozniak/Public/sfw/ppc64/turbine
+
+source $GENLEAF_HOME/flags.xlc.sh
