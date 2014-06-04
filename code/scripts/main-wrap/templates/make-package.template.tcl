b88577db7bdabeaedef64a927f9ebe56cbab6526
Justin Wozniak <wozniak@mcs.anl.gov>
2014-06-04 23:42:14 +0000

    Handle static and dynamic Tcl packages
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@13017 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/scripts/main-wrap/templates/make-package.template.tcl
+++ code/scripts/main-wrap/templates/make-package.template.tcl
@@ -5,11 +5,17 @@ define(`getenv', `esyscmd(printf -- "$`$1' ")')
 
 # dnl Receive USER_LEAF from environment for m4 processing
 set USER_LEAF getenv(USER_LEAF)
+set STATIC    getenv(STATIC) 
 
 set name     leaf_main
 set version  0.0
 set leaf_so  lib${USER_LEAF}.so
 set leaf_tcl ${USER_LEAF}.tcl
 
-exec echo [ ::pkg::create -name $name -version $version \
-            -source $leaf_tcl ] > pkgIndex.tcl
+if { ! $STATIC } { 
+    exec echo [ ::pkg::create -name $name -version $version \
+                    -load $leaf_so -source $leaf_tcl ] > pkgIndex.tcl 
+} else { 
+     exec echo [ ::pkg::create -name $name -version $version \
+                     -source $leaf_tcl ] > pkgIndex.tcl 
+}
