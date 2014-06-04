bc9c4b3728f7e59705166ba3bc661045fac35594
Justin Wozniak <wozniak@mcs.anl.gov>
2014-06-04 18:30:36 +0000

    Initial use of templates in genleaf
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@12997 dc4e9af1-7f46-4ead-bba6-71afc04862de

---
+++ code/scripts/main-wrap/templates/make-package.template.tcl
@@ -0,0 +1,15 @@
+
+# Define convenience macros
+changecom(`dnl')
+define(`getenv', `esyscmd(printf -- "$`$1' ")')
+
+# dnl Receive USER_LEAF from environment for m4 processing
+set USER_LEAF getenv(USER_LEAF)
+
+set name     leaf_main
+set version  0.0
+set leaf_so  ${USER_LEAF}.so
+set leaf_tcl ${USER_LEAF}.tcl
+
+exec echo [ ::pkg::create -name $name -version $version \
+           -load $leaf_so -source $leaf_tcl ] > pkgIndex.tcl
