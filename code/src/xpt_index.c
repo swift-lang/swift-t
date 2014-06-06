0f4212de2f504d4f1ac70cf99e23bdfa6961d1a8
Justin Wozniak <wozniak@mcs.anl.gov>
2014-06-06 14:15:51 +0000

    C syntax fix for GCC 4.4.7
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/lb/trunk@13033 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/src/xpt_index.c
+++ code/src/xpt_index.c
@@ -41,9 +41,10 @@ adlb_code xlb_xpt_index_init(void)
     // setup checkpoint index using sharded container on servers
     adlb_datum_id container_id = id_for_rank(xlb_comm_rank);
 
-    adlb_type_extra extra = { .CONTAINER.key_type = ADLB_DATA_TYPE_BLOB,
-                              .CONTAINER.val_type = ADLB_DATA_TYPE_BLOB,
-                              .valid = true };
+    adlb_type_extra extra;
+    extra.CONTAINER.key_type = ADLB_DATA_TYPE_BLOB;
+    extra.CONTAINER.val_type = ADLB_DATA_TYPE_BLOB,
+    extra.valid = true;
     adlb_create_props props = { .read_refcount = 1, .write_refcount = 1,
                                 .permanent = true };
     dc = xlb_data_create(container_id, ADLB_DATA_TYPE_CONTAINER, &extra,
