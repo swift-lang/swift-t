c3de366db3fa4e838960178e25d08d8e4a247748
Justin Wozniak <wozniak@mcs.anl.gov>
2014-06-25 14:32:55 +0000

    Fix settings
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@13176 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/scripts/main-wrap/settings/machine.mira-gcc.sh
+++ code/scripts/main-wrap/settings/machine.mira-gcc.sh
@@ -2,7 +2,7 @@
 # MIRA GCC
 
 #TCL_HOME=/home/wozniak/Public/sfw/ppc64/tcl-8.5.12
-TCL_HOME=/home/wozniak/Public/sfw/ppc64/bgxlc/tcl-8.5.12
+TCL_HOME=/home/wozniak/Public/sfw/ppc64/bgxlc/static/tcl-8.5.12
 TCL_VERSION=8.5
 
 CC=/bgsys/drivers/ppcfloor/gnu-linux/bin/powerpc64-bgq-linux-gcc
@@ -12,8 +12,8 @@ MPI_HOME=/bgsys/drivers/V1R2M1/ppc64/comm
 MPI_LIB_NAME=mpich-gcc
 MPICC=/bgsys/drivers/V1R2M1/ppc64/comm/bin/gcc/mpicc
 
-TURBINE_HOME=/home/wozniak/Public/sfw/ppc64/turbine
-ADLB_HOME=/home/wozniak/Public/sfw/ppc64/lb
-C_UTILS_HOME=/home/wozniak/Public/sfw/ppc64/c-utils
+TURBINE_HOME=/home/wozniak/Public/sfw/ppc64/static/turbine
+ADLB_HOME=/home/wozniak/Public/sfw/ppc64/static/lb
+C_UTILS_HOME=/home/wozniak/Public/sfw/ppc64/static/c-utils
 
 source $GENLEAF_HOME/settings/flags.gcc.sh
