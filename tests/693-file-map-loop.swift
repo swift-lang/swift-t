3a264109166ec781d9318ff56bc64202eb806ac1
Tim Armstrong <tim.g.armstrong@gmail.com>
2014-07-01 20:23:42 +0000

    Add test for spurious warnings from mapping
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@13315 dc4e9af1-7f46-4ead-bba6-71afc04862de

---
+++ tests/693-file-map-loop.swift
@@ -0,0 +1,16 @@
+
+// SKIP-THIS-TEST
+// see issue #717
+
+app (file out) runcommand (string instr){
+   "/bin/sh" "-c" instr @stdout=out;
+}
+
+import string;
+
+main () {
+    foreach i in [0:31]{
+        file f<sprintf("f-%i.txt", i)> = runcommand("echo hello world");
+    }
+}
+
