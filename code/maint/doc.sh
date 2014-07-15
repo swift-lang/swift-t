9ef1419875e5aec75bb292e72f2767acaf66502c
Justin Wozniak <wozniak@mcs.anl.gov>
2014-07-15 19:44:44 +0000

    Initial blob docs
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@13418 dc4e9af1-7f46-4ead-bba6-71afc04862de

---
+++ code/maint/doc.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -eu
+
+# DOC.SH
+# Extracts DOC() statements from target file
+# Primarily used to generate Asciidoc from C code 
+
+# doc.m4 diverts all input except that within DOC() statements
+# To avoid confusing your C editor, you may want to comment 
+# like this: /** DOC(asciidoc text here) */
+
+# m4 macros: 
+# DOCT():     Simple text output (doc-text)
+# DOCN():     Simple text output plus newline (doc-newline)
+# DOCNN():    Simple text output plus two newlines
+# DOCD():     Use Asciidoc's definition syntax (doc-definition)
+# DOC_CODE(): Code snippet
+
+# If your doc text contains comma, you should quote it: 
+# DOCT(`text, with comma')
+
+# We delete leading spaces from the asciidoc file 
+# (outside of code snippets)
+
+MAINT=$( cd $( dirname $0 ) ; /bin/pwd ) 
+
+usage()
+{
+  echo   "doc.sh: usage: "
+  printf "\t doc.sh <INPUT> <OUTPUT>\n"
+}
+
+if [[ ${#*} != 2 ]] 
+then 
+  usage
+  exit 1
+fi
+
+INPUT=$1
+OUTPUT=$2
+
+m4 -P ${MAINT}/doc.m4 ${INPUT} > ${OUTPUT}
