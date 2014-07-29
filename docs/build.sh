b54a8d35e0a20e35102ebfd15fc21d1afddf8a0d
Justin Wozniak <wozniak@mcs.anl.gov>
2014-07-29 16:38:39 +0000

    Update build scripts
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@13563 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- docs/build.sh
+++ docs/build.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-asciidoc --attribute stylesheet=${PWD}/swift.css -a max-width=750px -a textwidth=80 guide.txt
-asciidoc --attribute stylesheet=${PWD}/swift.css -a max-width=750px -a textwidth=80 internals.txt
+make -f build.mk
