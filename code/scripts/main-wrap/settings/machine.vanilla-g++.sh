6496e6d3ea981ac340919ddbdc2ccb9718b5264c
Ketan Maheswari <ketancmaheshwari@gmail.com>
2014-07-31 19:58:10 +0000

    vanilla g++
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@13595 dc4e9af1-7f46-4ead-bba6-71afc04862de

---
+++ code/scripts/main-wrap/settings/machine.vanilla-g++.sh
@@ -0,0 +1,24 @@
+
+# VANILLA
+
+TCL_VERSION=${TCL_VERSION:-8.5}
+TCLSH=$( which tclsh$TCL_VERSION ; true )
+[[ $TCLSH == "" ]] && crash "machine.vanilla.sh: Could not find tclsh$TCL_VERSION"
+TCL_HOME=$(cd $(dirname $TCLSH)/.. ; /bin/pwd)
+
+TURBINE=$( which turbine ; true )
+[[ $TURBINE == "" ]] && crash "machine.vanilla.sh: Could not find turbine"
+TURBINE_HOME=$(cd $(dirname $TURBINE)/.. ; /bin/pwd)
+ADLB_HOME=$(cd $TURBINE_HOME/../lb 2>/dev/null || cd $TURBINE_HOME/../adlb 2>/dev/null; /bin/pwd)
+C_UTILS_HOME=$(cd $TURBINE_HOME/../c-utils ; /bin/pwd)
+
+MPIEXEC=$( which mpiexec )
+[[ $MPIEXEC == "" ]] && crash "machine.vanilla.sh: Could not find mpiexec"
+MPI_HOME=$(cd $(dirname $MPIEXEC)/.. ; /bin/pwd)
+MPICC=$( which mpicc ; true )
+[[ $MPICC == "" ]] && crash "machine.vanilla.sh: Could not find mpicc"
+
+CC=g++
+STC=stc
+
+source $GENLEAF_HOME/settings/flags.g++.sh
