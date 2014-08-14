a054d8ca23a4567837f63dd21b03bb04c9e2e74d
Justin Wozniak <wozniak@mcs.anl.gov>
2014-08-14 17:16:45 +0000

    Demonstrates STC bug: #732
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@13707 dc4e9af1-7f46-4ead-bba6-71afc04862de

---
+++ tests/8561-file-misuse.swift
@@ -0,0 +1,13 @@
+
+// SKIP-THIS-TEST #732 
+// THIS-TEST-SHOULD-NOT-COMPILE
+
+(file o) f(file o) "turbine" "0.0"
+[ "exec cp <<o>> <<o>>" ];
+  
+main
+{
+  file i = input("i.txt");
+  file o<"o.txt">;
+  o = f(i);
+}
