a2d94b10995c8d0e4ad328ce84128ce5e9ce2c95
Justin Wozniak <wozniak@mcs.anl.gov>
2014-09-10 01:31:47 +0000

    Working MPI-IO copy operation
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@13848 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/tests/mpi-io-2.c
+++ code/tests/mpi-io-2.c
@@ -19,88 +19,98 @@
   Make local copies of file.
 */
 
+#include <assert.h>
+#include <mpi.h>
+#include <mpio.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-
 #include <sys/stat.h>
 #include <sys/types.h>
+//#include <unistd.h>
 
-#include "src/util/mpi-tools.h"
+#include <tools.h>
+#include "../src/util/mpi-tools.h"
+
+#define EXM_MPIIO_FILE_CHUNK_SIZE 40*1024*1024
+
+bool
+exm_mpiio_copy_to(MPI_Comm comm, const char* name_in,
+                  const char* name_out)
+{
+  int mpi_size;
+  MPI_Comm_size(comm, &mpi_size);
+  int mpi_rank;
+  MPI_Comm_rank(comm, &mpi_rank);
+
+  int rc;
+  MPI_Offset file_size;
+  if (mpi_rank == 0)
+  {
+    struct stat s;
+    rc = stat(name_in, &s);
+    file_size = s.st_size;
+  }
+
+  printf("mpi: %i/%i\n", mpi_rank, mpi_size);
+
+  MPI_Bcast(&file_size, sizeof(MPI_Offset), MPI_BYTE, 0, MPI_COMM_WORLD);
+  int size_int = (int) file_size;
+  printf("file size: %i\n", size_int);
+
+  int chunk_max = EXM_MPIIO_FILE_CHUNK_SIZE;
+
+  void* buffer = malloc(chunk_max);
+  memset(buffer, '\0', chunk_max);
+
+  MPI_File fd_in;
+  rc = MPI_File_open(MPI_COMM_WORLD, name_in,
+                     MPI_MODE_RDONLY,
+                     MPI_INFO_NULL, &fd_in);
+  MPI_ASSERT_MSG(rc, "could not open fd_in");
+
+  FILE* fd_out = fopen(name_out, "w");
+  if (fd_out == NULL)
+  {
+    printf("could not write to: %s", name_out);
+    return false;
+  }
+
+  MPI_Status status;
+  int64_t total = 0;
+  while (total < file_size)
+  {
+    int chunk = min_integer(chunk_max, file_size-total);
+    memset(buffer, '\0', chunk_max);
+    rc = MPI_File_read_all(fd_in, buffer, chunk, MPI_BYTE, &status);
+    MPI_ASSERT(rc);
+    int r;
+    MPI_Get_count(&status, MPI_BYTE, &r);
+    assert(r == chunk);
+    printf("r: %i\n", r);
+    printf("data: %s\n", (char*) buffer);
+
+    rc = fwrite(buffer, chunk, 1, fd_out);
+    assert(rc != 0);
+
+    total += r;
+  }
+
+  free(buffer);
+  MPI_File_close(&fd_in);
+  fclose(fd_out);
+  return true;
+}
+
 
 int
 main(int argc, char** argv)
 {
-  int rc;
-
   char* filename = "tests/mpi-io.data";
-  MPI_File file;
 
   MPI_Init(&argc, &argv);
-
-  int mpi_size;
-  MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
-  int mpi_rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
-
-  printf("mpi: %i/%i\n", mpi_rank, mpi_size);
-
-  MPI_Offset size = 100;
-
-  if (mpi_rank == 0)
-  {
-    struct stat s;
-    rc = stat(filename, &s);
-    size = s.st_size;
-  }
-  MPI_Bcast(&size, sizeof(MPI_Offset), MPI_BYTE, 0, MPI_COMM_WORLD);
-  int size_int = (int) size;
-  printf("file size: %i\n", size_int);
-
-  int chunk_size = 4;
-  MPI_Datatype chunk;
-  rc = MPI_Type_contiguous(chunk_size, MPI_BYTE, &chunk);
-  MPI_ASSERT(rc);
-  MPI_Type_commit(&chunk);
-
-  int chunks = (int)size/chunk_size + 1;
-  MPI_Datatype strides;
-  //  rc = MPI_Type_vector(chunks, chunk_size, mpi_size*chunk_size,
-  //		       MPI_BYTE, &strides);
-  rc = MPI_Type_vector(chunks, 1, 2, chunk, &strides);
-  MPI_ASSERT(rc);
-  MPI_Type_commit(&strides);
-
-  rc = MPI_File_open(MPI_COMM_WORLD, filename,
-		     MPI_MODE_RDONLY,
-		     MPI_INFO_NULL, &file);
-  MPI_ASSERT_MSG(rc, "could not open file");
-
-  int disp = mpi_rank*chunk_size;
-  rc = MPI_File_set_view(file, disp, MPI_BYTE, strides,
-			 "native", MPI_INFO_NULL);
-  MPI_ASSERT(rc);
-
-  char buffer[size];
-  memset(buffer, '\0', 10000);
-
-  printf("read\n");
-
-  MPI_Status status;
-
-  rc = MPI_File_read_all(file, buffer, size, MPI_BYTE, &status);
-  MPI_ASSERT(rc);
-  int r;
-  MPI_Get_count(&status, MPI_BYTE, &r);
-  assert(r == size);
-  printf("r: %i\n", r);
-
-
-  MPI_Type_free(&strides);
-  MPI_Type_free(&chunk);
-
+  exm_mpiio_copy_to(MPI_COMM_WORLD, filename, "./t.txt");
   MPI_Finalize();
 
   return 0;
