4e6beb446e3500174103c15c69e441c2f23f96e1
Tim Armstrong <tim.g.armstrong@gmail.com>
2014-09-17 20:42:55 +0000

    Switch stc coaster test to active coasters
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@13870 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- tests/695-app-coasters.cleanup.sh
+++ tests/695-app-coasters.cleanup.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-stop-coaster-service -conf "$(dirname $0)/695-app-coasters.coaster.conf"
+
+echo "Killing service ${COASTER_SVC_PID} and immediate children"
+for child_pid in $(ps -ef| awk '$3 == '${COASTER_SVC_PID}' { print $2 }')
+do
+  echo "Killing process $pid"
+  kill $child_pid
+done
+
+kill ${COASTER_SVC_PID}
+wait || true
