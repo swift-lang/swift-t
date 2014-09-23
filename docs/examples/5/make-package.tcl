1792378840817898e41191109839945853ca2a7b
Justin Wozniak <wozniak@mcs.anl.gov>
2014-09-23 18:38:24 +0000

    Reformulating based on standard Turbine genleaf
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@13897 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- docs/examples/5/make-package.tcl
+++ docs/examples/5/make-package.tcl
@@ -1,7 +1,21 @@
+
+# Define convenience macros
+
+
+
+# dnl Receive USER_LEAF from environment for m4 processing
+set USER_LEAF ex2_wrap 
+set STATIC    0  
+
 set name     leaf_main
 set version  0.0
-set leaf_so  userlib.so
-set leaf_tcl user-leaf.tcl
+set leaf_so  lib${USER_LEAF}.so
+set leaf_tcl ${USER_LEAF}.tcl
 
-puts [ ::pkg::create -name $name -version $version            -load $leaf_so -source $leaf_tcl ]
-
+if { ! $STATIC } { 
+    exec echo [ ::pkg::create -name $name -version $version \
+                    -load $leaf_so -source $leaf_tcl ] > pkgIndex.tcl 
+} else { 
+     exec echo [ ::pkg::create -name $name -version $version \
+                     -source $leaf_tcl ] > pkgIndex.tcl 
+}
---
+++ docs/examples/5/test-main.sh
@@ -0,0 +1,2 @@
+genleaf -v ex2.c ex2.h prog-swift.swift
+swift-t -r $PWD user-code.swift
