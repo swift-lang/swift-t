32fbb4b2b95ef38ada6b27a29491c34f039d8f9f
Justin Wozniak <wozniak@mcs.anl.gov>
2014-09-23 17:41:22 +0000

    New 3/run.sh
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@13895 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- docs/examples/3/swig-b.sh
+++ docs/examples/3/swig-b.sh
@@ -1,6 +1,6 @@
 rm *.o
-swig -module b b.h
+swig -module b b.i
 gcc -c -fPIC b.c
-gcc -c -fPIC -I /usr/include/tcl8.5 b_wrap.c
+gcc -c -fPIC -I $TCL_INCLUDE b_wrap.c
 gcc -shared -o libb.so b_wrap.o b.o
 tclsh make-package.tcl > pkgIndex.tcl
