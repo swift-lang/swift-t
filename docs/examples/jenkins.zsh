c3e998d6a0884785b3eda7439f2a6aec8378d885
Justin Wozniak <wozniak@mcs.anl.gov>
2014-09-23 20:54:00 +0000

    Build BLAS
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@13929 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- docs/examples/jenkins.zsh
+++ docs/examples/jenkins.zsh
@@ -8,5 +8,25 @@ STC=/tmp/exm-install/stc
 MPICH=/tmp/mpich-install
 path+=( $MPICH/bin $TURBINE/bin $STC/bin )
 
+# Build BLAS
+if [[ ! -f blas.tgz ]]
+then
+  mkdir -p /tmp/blas-build
+  pushd /tmp/blas-build
+  wget http://www.netlib.org/blas/blas.tgz
+  tar xfz blas.tgz
+  cd BLAS
+  for f in *.f
+  do
+    gfortran -fPIC -c ${f}
+  done
+  gfortran -o BLAS.a *.o
+  popd
+fi
+
+ls /tmp/blas-build
+
+export BLAS=/tmp/blas-build/BLAS/BLAS.a
+
 print HI
 ./run.sh
