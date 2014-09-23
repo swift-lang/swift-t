b2fb282ad60513547e5cf09cf8a963e20ebdbb9e
Justin Wozniak <wozniak@mcs.anl.gov>
2014-09-23 19:15:42 +0000

    Example 5 works
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@13906 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- docs/examples/5/run.sh
+++ docs/examples/5/run.sh
@@ -1,12 +1,9 @@
 #!/bin/sh -eu
 
-CFLAGS="-std=gnu99"
 echo "Stand-alone"
-gcc -c $CFLAGS test-main.c
-sed 's/main(/leaf_main(/' main.c > leaf_main.c
-gcc -c $CFLAGS leaf_main.c
-gcc -o test-main.x test-main.o leaf_main.o
-./test-main.x 10
+CFLAGS="-std=gnu99"
+gcc $CFLAGS -o main.x main.c
+./main.x 10 11 12
 
 echo "Swift/T"
 sh -eu ./test-main.sh
