05d93b8a2f743cbcd7d6c80822be6baa7b876eb4
Justin Wozniak <wozniak@mcs.anl.gov>
2014-09-23 16:32:47 +0000

    Tell user to set TCL_INCLUDE at top level
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@13893 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- docs/examples/2/test-swig-g.sh
+++ docs/examples/2/test-swig-g.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -e
 rm *.o
 gcc -c -fPIC  g.c
-TCL_INCLUDE=$HOME/sfw/tcl-8.6.0/include
 gcc -c -fPIC -I $TCL_INCLUDE g_wrap.c
 gcc -shared -o libg.so g_wrap.o g.o
 tclsh make-package.tcl > pkgIndex.tcl
