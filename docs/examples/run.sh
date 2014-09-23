9ab9959662c54d7d9df10f8771711ef096e795d0
Justin Wozniak <wozniak@mcs.anl.gov>
2014-09-23 19:53:30 +0000

    Use TCL_INCLUDE_SPEC from Turbine
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@13909 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- docs/examples/run.sh
+++ docs/examples/run.sh
@@ -1,6 +1,19 @@
 #!/bin/bash -eu
 
-export TCL_INCLUDE=${HOME}/sfw/tcl-8.6.0
+source ./setup.sh
+
+# Sanity check variables:
+TCL_INCLUDE=${TCL_INCLUDE_SPEC:2}
+if [[ ! -f ${TCL_INCLUDE}/tcl.h ]]
+then
+  echo "Variable TCL_INCLUDE_SPEC is wrong!"
+  exit 1
+fi
+if [[ ! -f ${BLAS} ]]
+then
+  echo "Variable BLAS is wrong!"
+  exit 1
+fi
 
 for T in {1..9}
 do
