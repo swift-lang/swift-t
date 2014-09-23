a74f74399f42f411c08c30cc6ad54df759ce40ad
Justin Wozniak <wozniak@mcs.anl.gov>
2014-09-23 19:52:49 +0000

    Use new container format
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@13908 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- docs/examples/6/func.tcl
+++ docs/examples/6/func.tcl
@@ -1,22 +1,26 @@
 
 proc func { A } {
 
-    set C [ adlb::enumerate $A members all 0 ]
+    show A
 
-    set N [ llength $C ]
+    set N [ dict size $A ]
 
-    set A [ new_string_array ]
-    string_array_string_array_create $A $N
-    for { set i 1 } { $i <= $N } { incr i } {
-        set td [ lindex $C [ expr $i - 1 ] ]
-        set s [ retrieve $td ]
-        string_array_string_array_set $A $i $s
+    show N
+
+    set S [ new_string_array ]
+    string_array_string_array_create $S $N
+    for { set i 0 } { $i < $N } { incr i } {
+        # Fortran index: i+1
+        set j [ expr $i + 1 ]
+        # Pull out string i as t
+        set t [ dict get $A $i ]
+        string_array_string_array_set $S $j $t
     }
 
     set p [ blobutils_malloc [blobutils_sizeof_float]]
     set d [ blobutils_cast_to_dbl_ptr $p ]
 
-    FortFuncs_func 3 $A $d
+    FortFuncs_func 3 $S $d
 
     set v [ blobutils_get_float $d 0 ]
     blobutils_free $p
