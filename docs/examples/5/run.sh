f0019283f8339b41bf07b389d8140417a7278521
Justin Wozniak <wozniak@mcs.anl.gov>
2014-09-23 19:03:27 +0000

    Working...
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@13903 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- docs/examples/5/run.sh
+++ docs/examples/5/run.sh
@@ -3,7 +3,7 @@
 CFLAGS="-std=gnu99"
 echo "Stand-alone"
 gcc -c $CFLAGS prog-c.c
-sed 's/main/swift_main/' ex2.c > swift_main.c
+sed 's/main(/leaf_main(/' main.c > swift_main.c
 gcc -c $CFLAGS swift_main.c
 gcc -o prog-c.x prog-c.o swift_main.o
 ./prog-c.x 10
