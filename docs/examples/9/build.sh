405fa04f07763289de3410aae9a8bd18dbbc704d
Justin Wozniak <wozniak@mcs.anl.gov>
2014-09-23 20:04:20 +0000

    Use TCL_INCLUDE_SPEC
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@13916 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- docs/examples/9/build.sh
+++ docs/examples/9/build.sh
@@ -11,11 +11,10 @@ TURBINE_LIB=${TURBINE_HOME}/lib
 # Obtain Turbine build configuration
 source ${TURBINE_HOME}/scripts/turbine-config.sh
 
-TCL_INCLUDE=${TCL}/include
 C_UTILS_INCLUDE=${C_UTILS}/include
 ADLB_INCLUDE=${ADLB}/include
 
-INCLUDES="-I . -I ${TURBINE_INCLUDE} -I ${TCL_INCLUDE} "
+INCLUDES="-I . -I ${TURBINE_INCLUDE} ${TCL_INCLUDE_SPEC} "
 INCLUDES+="-I ${C_UTILS_INCLUDE} -I ${ADLB_INCLUDE}"
 
 stc -r ${PWD} -r ${TURBINE_LIB} test-f.swift test-f.tcl
@@ -24,4 +23,3 @@ ${MPICC} -c ${INCLUDES} controller.c
 ${MPICC} -o controller.x controller.o \
   -L ${TURBINE_LIB} -l tclturbine \
   -Wl,-rpath -Wl,${TURBINE_LIB}
-
