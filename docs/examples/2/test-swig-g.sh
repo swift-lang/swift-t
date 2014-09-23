ebdb7cca73f15369e3316e61288982c4cab51911
Justin Wozniak <wozniak@mcs.anl.gov>
2014-09-23 19:53:30 +0000

    Use TCL_INCLUDE_SPEC from Turbine
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@13909 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- docs/examples/2/test-swig-g.sh
+++ docs/examples/2/test-swig-g.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 rm *.o
 gcc -c -fPIC  g.c
-gcc -c -fPIC -I $TCL_INCLUDE g_wrap.c
+gcc -c -fPIC $TCL_INCLUDE_SPEC g_wrap.c
 gcc -shared -o libg.so g_wrap.o g.o
 tclsh make-package.tcl > pkgIndex.tcl
---
+++ docs/examples/setup.sh
@@ -0,0 +1,11 @@
+
+# EXAMPLES SETUP.SH
+# Source this before running examples
+
+# Set these for your system:
+
+# Get Turbine's Tcl location
+TURBINE_HOME=$( cd $( dirname $( which turbine ) )/.. ; /bin/pwd )
+source ${TURBINE_HOME}/scripts/turbine-build-config.sh
+export TCL_INCLUDE_SPEC
+export BLAS=${HOME}/Downloads/BLAS/BLAS.a
