f7e3ea52b3f5b950370166dda8eae20fc7b5695c
Justin Wozniak <wozniak@mcs.anl.gov>
2014-09-24 14:50:02 +0000

    Prepend paths
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@13947 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- docs/examples/8/build.sh
+++ docs/examples/8/build.sh
@@ -3,11 +3,11 @@
 MPICC=$( which mpicc )
 MPI=$( dirname $( dirname $MPICC ) )
 
-swig -I$MPI/include f.i
+swig -I${MPI}/include f.i
 
-$MPICC -c -fPIC -I . f.c
-$MPICC -c -fPIC $TCL_INCLUDE_SPEC f_wrap.c
-$MPICC -shared -o libf.so f_wrap.o f.o
+mpicc -c -fPIC -I . f.c
+mpicc -c -fPIC $TCL_INCLUDE_SPEC f_wrap.c
+mpicc -shared -o libf.so f_wrap.o f.o
 tclsh make-package.tcl > pkgIndex.tcl
 
 stc -r $PWD test-f.swift
