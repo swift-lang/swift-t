c837d5b2e12994f09032f66991b16a5053250e9b
Justin Wozniak <wozniak@mcs.anl.gov>
2014-10-09 18:18:33 +0000

    New HDF test - works
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@14024 dc4e9af1-7f46-4ead-bba6-71afc04862de

---
+++ tests/760-hdf-1.swift
@@ -0,0 +1,12 @@
+
+// Cf. 594-blob-write
+
+import blob;
+import io;
+
+main
+{
+  float A[] = [ 0.2, 0.3, 0.4, 0.5 ];
+  blob b = blob_from_floats(A);
+  file f<"A.hdf"> = blob_hdf_write("entry", b);
+}
