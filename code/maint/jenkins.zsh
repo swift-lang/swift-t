8bd8d5a4bd1aad316ba27b7055c037d108ff435d
Justin Wozniak <wozniak@mcs.anl.gov>
2014-10-13 15:49:38 +0000

    Disable HDF in Jenkins
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@14031 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/maint/jenkins.zsh
+++ code/maint/jenkins.zsh
@@ -1,41 +1,22 @@
 #!/bin/zsh
 
-C_UTILS=/tmp/exm-install/c-utils
-TURBINE=/tmp/exm-install/turbine
-STC=/tmp/exm-install/stc
 MPICH=/tmp/mpich-install
+C_UTILS=/tmp/exm-install/c-utils
+ADLB=/tmp/exm-install/lb
+TURBINE=/tmp/exm-install/turbine
 path+=( $MPICH/bin $TURBINE/bin $STC/bin )
 
 set -eu
 # set -x
 
-# Print a message on stderr to avoid putting it in the Jenkins XML
-message()
-{
-  print -u 2 ${*}
-}
-
-check_error()
-{
-  CODE=$1
-  MSG=$2
-  if (( CODE != 0 ))
-  then
-    print "Operation failed: ${MSG}"
-    print "Exit code: ${CODE}"
-    exit 1
-  fi
-}
-
 ./setup.sh
-check_error ${?} "setup.sh"
 
-./configure --prefix=$TURBINE               \
-            --with-tcl=/usr                 \
-            --with-mpi=$MPICH               \
-            --with-c-utils=$C_UTILS         \
-            --with-adlb=/tmp/exm-install/lb \
-            --enable-shared
+./configure --prefix=$TURBINE        \
+            --with-tcl=/usr          \
+            --with-mpi=$MPICH        \
+            --with-c-utils=$C_UTILS  \
+            --with-adlb=$ADLB        \
+            --with-hdf5=no
 make clean
 
 make V=1
@@ -48,6 +29,12 @@ make test_results || true
 cd tests
 SUITE_RESULT="result_aggregate.xml"
 rm -fv $SUITE_RESULT
+
+# Print a message on stderr to avoid putting it in the Jenkins XML
+message()
+{
+  print -u 2 ${*}
+}
 
 inspect_results()
 {
