96c85f2614e37748379d5624e7ea71b35c560d92
Justin Wozniak <wozniak@mcs.anl.gov>
2014-10-13 15:55:18 +0000

    Rename
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@14032 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/maint/jenkins-tests.zsh
+++ code/maint/jenkins-tests.zsh
@@ -1,27 +1,11 @@
 #!/bin/zsh
 
-MPICH=/tmp/mpich-install
-C_UTILS=/tmp/exm-install/c-utils
-ADLB=/tmp/exm-install/lb
-TURBINE=/tmp/exm-install/turbine
-path+=( $MPICH/bin $TURBINE/bin $STC/bin )
+# Jenkins script - run Turbine test suite
+
+path+=( $MPICH/bin $TURBINE/bin )
 
 set -eu
 # set -x
-
-./setup.sh
-
-./configure --prefix=$TURBINE        \
-            --with-tcl=/usr          \
-            --with-mpi=$MPICH        \
-            --with-c-utils=$C_UTILS  \
-            --with-adlb=$ADLB        \
-            --with-hdf5=no
-make clean
-
-make V=1
-
-make V=1 install
 
 # This is allowed to fail- we scan for output below
 make test_results || true
