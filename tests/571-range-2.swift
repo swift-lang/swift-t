37e489c20b5c85924b1598875094e9d8a7afddb1
Tim Armstrong <tim.g.armstrong@gmail.com>
2014-12-15 20:16:40 +0000

    Add additional tests for floating point range loops, fix bug with iteration calc
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@14202 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- tests/571-range-2.swift
+++ tests/571-range-2.swift
@@ -10,9 +10,22 @@ main {
   assertEqual(x[1], 2.0, "x[1]");
   assertEqual(size(x), 2, "len(x)");
 
-  trace(repr(x));
+  trace("x", repr(x));
 
   foreach y in [1.0:10.0] {
     printf("y=%f", y);
+  }
+  
+  
+  foreach z in [0:1004.9:7.5] {
+    printf("z=%f", z);
+  }
+
+  foreach a in [0:0.0:0.001] {
+    printf("a=%f", a);
+  }
+  
+  foreach b in [0:-1.0] {
+    assert(false, "Range loop should not run");
   }
 }
