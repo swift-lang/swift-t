2116bed7ff9d1a9ff5a657e6de2b4a76f7717574
Justin Wozniak <wozniak@mcs.anl.gov>
2014-12-16 15:53:32 +0000

    Updates for jenkins
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/lb/trunk@14207 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/maint/jenkins.zsh
+++ code/maint/jenkins.zsh
@@ -18,15 +18,19 @@ rm -rf /tmp/exm-install/lb
 
 set -x
 PATH=/tmp/mpich-install/bin:$PATH
+
+echo MPICC:
 which mpicc
 mpicc -show
+echo
 
 ./bootstrap
 mkdir -p /tmp/exm-install
 ./configure CC=$(which mpicc) --prefix=/tmp/exm-install/lb --with-c-utils=/tmp/exm-install/c-utils
+echo
 make clean
 make V=1 install
-ldd lib/libadlb.so
+# ldd lib/libadlb.so
 # make V=1 apps/batcher.x
 # ldd apps/batcher.x
 exit 0
