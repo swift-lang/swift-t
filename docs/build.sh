18f98556d593b6c92d4c58287aebbb140e624872
Justin Wozniak <wozniak@mcs.anl.gov>
2014-12-16 16:38:32 +0000

    Fixes for turbine- prefix
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@14211 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- docs/build.sh
+++ docs/build.sh
@@ -8,3 +8,5 @@ fi
 
 asciidoc --attribute stylesheet=${PWD}/swift.css internals.txt
 asciidoc --attribute stylesheet=${PWD}/swift.css sites.txt
+cp internals.html turbine-internals.html
+cp sites.html     turbine-sites.html
