d33826e47f915496d941a7d4ab2b2d2519a7fd18
Justin Wozniak <wozniak@mcs.anl.gov>
2014-12-17 17:37:34 +0000

    Fix exists() with optz
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/trunk@14229 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/export/builtins.swift
+++ code/export/builtins.swift
@@ -92,7 +92,7 @@ type external void;
 <T> (int n) size(T A[]) "turbine" "0.0.5" "container_size";
 @pure @implements=contains
 <K, V> (boolean o) contains(V A[K], K key) "turbine" "0.0.5" "contains";
-@pure
+
 <K, V> (boolean o) exists(V A[K], K key) "turbine" "0.7.0" "exists";
 
 @pure @implements=size
