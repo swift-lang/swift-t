aa5d2bcc8f6cdcfbc52517944ccad1eb94171290
Tim Armstrong <tim.g.armstrong@gmail.com>
2014-12-19 04:59:15 +0000

    Extend test for format operator.  Add additional tests for unicode and hex literals
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@14247 dc4e9af1-7f46-4ead-bba6-71afc04862de

---
+++ tests/527-unicode.swift
@@ -0,0 +1,7 @@
+// Basic support for unicode characters in basic multilingual plane
+
+main {
+  // Katakana - Basic multilingual plane
+  // Insert some operations to check they behave ok
+  trace("%s" % "゠ ァ ア ィ イ ゥ ウ ェ エ ォ オ カ ガ キ ギ ク グ ケ ゲ コ ゴ サ ザ シ ジ ス ズ セ ゼ ソ ゾ タ ダ チ ヂ ッ ツ ヅ テ デ ト " + "ド ナ ニ ヌ ネ ノ ハ バ パ ヒ ビ ピ フ ブ プ ヘ ベ ペ ホ ボ ポ マ ミ ム メ モ ャ ヤ ュ ユ ョ ヨ ラ リ ル レ ロ ヮ ワ ヰ ヱ ヲ ン ヴ ヵ ヶ ヷ ヸ ヹ ヺ ・ ー ヽ ヾ ヿ");
+}
