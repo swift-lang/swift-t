936bb80ca7fa8e190f6cdf4e95973bfc7bc72bd3
Tim Armstrong <tim.g.armstrong@gmail.com>
2014-12-24 03:35:38 +0000

    Update script name
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@14269 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- tests/134-func-err-10.swift
+++ tests/134-func-err-10.swift
@@ -1,12 +1,17 @@
+<<<<<<< HEAD
 // SKIP-THIS-TEST #759
 // THIS-TEST-SHOULD-NOT-COMPILE
 
+=======
+>>>>>>> Fix test 134 for new type/.var scope rules
 type x {
   int a;
   int b;
 }
-
+import assert;
 main {
-  // Check can't define conflicting variable with type
+  // Check can define variable with same name as type
   int x = 3;
+
+  assertEqual(x, 3, "x");
 }
