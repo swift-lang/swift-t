290de96a80c26918ef6116047bf75ca0c0edea9e
Tim Armstrong <tim.g.armstrong@gmail.com>
2014-12-31 22:15:39 +0000

    Fix tests for new copy behaviour.
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@14281 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- tests/692-file-map.swift
+++ tests/692-file-map.swift
@@ -1,11 +1,16 @@
+import files;
 import string;
 
-// See issue 
-// THIS-TEST-SHOULD-NOT-COMPILE
-
 main () {
-    foreach i in [0:31]{
-        file f<sprintf("f-%i.txt", i)> = input_file("/dev/null");
+    create_input() =>
+    foreach i in [0:31] {
+        /* Check that the combination of input_file and a mapped file
+           induces a copy */
+        file f<sprintf("692-out-%i.txt", i)> = input_file("692-in.txt");
     }
 }
 
+(void o) create_input() {
+  file infile<"692-in.txt"> = write("692") =>
+    o = make_void();
+}
