9d833ee00bdc1a0dbb1a7b6ff05e93a9174298f1
Tim Armstrong <tim.g.armstrong@gmail.com>
2015-01-15 21:12:30 +0000

    Add support for referencing top-level variables from functions
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@14417 dc4e9af1-7f46-4ead-bba6-71afc04862de

---
+++ tests/018-toplevel.swift
@@ -0,0 +1,13 @@
+import io;
+// SKIP-THIS-TEST
+
+/* Check support for referencing top-level variable from other functions*/
+
+string name = "Juan";
+
+say_hello();
+
+
+say_hello() {
+  printf("HELLO %s", name);
+}
