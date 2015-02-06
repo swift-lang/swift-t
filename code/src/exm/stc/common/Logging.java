0d7e796b7b512c5cf0ea03a5155a100b7e5c5752
Tim Armstrong <tim.g.armstrong@gmail.com>
2015-02-06 23:24:51 +0000

    Refactor logging to allow easy initialisation from other entry points
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@14579 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/src/exm/stc/common/Logging.java
+++ code/src/exm/stc/common/Logging.java
@@ -15,12 +15,18 @@
  */
 package exm.stc.common;
 
+import java.io.IOException;
 import java.util.HashSet;
 
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.Layout;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
 
 import exm.stc.common.util.Pair;
+import exm.stc.ui.ExitCode;
 
 public class Logging {
   private static final String STC_LOGGER_NAME = "exm.stc";
@@ -29,12 +35,61 @@ public class Logging {
    * Messages already emitted.
    */
   private static final HashSet<Pair<org.apache.log4j.Level, String>> emitted =
-          new HashSet<Pair<org.apache.log4j.Level, String>>(); 
-  
+          new HashSet<Pair<org.apache.log4j.Level, String>>();
+
   public static Logger getSTCLogger() {
     return Logger.getLogger(STC_LOGGER_NAME);
   }
-  
+
+
+  public static Logger setupLogging(String logfile, boolean trace) {
+    Logger stcLogger = getSTCLogger();
+    if (logfile != null && logfile.length() > 0) {
+      setupLoggingToStderr(stcLogger);
+      setupLoggingToFile(stcLogger, logfile, trace);
+    } else {
+      setupLoggingToStderr(stcLogger);
+    }
+
+    // Even if logging is disabled, this must be valid:
+    return stcLogger;
+  }
+
+  private static void setupLoggingToFile(Logger stcLogger, String logfile,
+      boolean trace) {
+    Layout layout = new PatternLayout("%-5p %m%n");
+    boolean append = false;
+    try {
+      FileAppender appender = new FileAppender(layout, logfile, append);
+      Level threshold;
+      if (trace) {
+        threshold = Level.TRACE;
+      } else {
+        threshold = Level.DEBUG;
+      }
+      appender.setThreshold(threshold);
+      stcLogger.addAppender(appender);
+      stcLogger.setLevel(threshold);
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+      System.exit(ExitCode.ERROR_IO.code());
+    }
+  }
+
+  /**
+   * Configures Log4j to log warnings to stderr
+   *
+   * @param stcLogger
+   */
+  private static void setupLoggingToStderr(Logger stcLogger) {
+    Layout layout = new PatternLayout("%-5p %m%n");
+    ConsoleAppender appender = new ConsoleAppender(layout,
+        ConsoleAppender.SYSTEM_ERR);
+    appender.setThreshold(Level.WARN);
+    stcLogger.addAppender(appender);
+    stcLogger.setLevel(Level.WARN);
+  }
+
   /**
    * @param level
    * @param msg
@@ -46,7 +101,7 @@ public class Logging {
 
   public static void uniqueWarn(String msg) {
     if (Logging.addEmitted(Level.WARN, msg)) {
-      Logging.getSTCLogger().warn(msg); 
+      Logging.getSTCLogger().warn(msg);
     } else {
       Logging.getSTCLogger().debug("Duplicate Warning: " + msg);
     }
