4615ecf0788b9905523f17125e15d763a83789a1
Tim Armstrong <tim.g.armstrong@gmail.com>
2015-02-07 23:13:48 +0000

    Remove unused features in HierarchicalMap
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@14598 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/src/exm/stc/common/util/HierarchicalMap.java
+++ code/src/exm/stc/common/util/HierarchicalMap.java
@@ -23,42 +23,28 @@ import exm.stc.common.exceptions.STCRuntimeError;
 
 /**
  * An implementation of the Map interface that allows cheap creation of
- * nested maps.  If a lookup fails in the current map, then it sees if 
- * the key is in the parent map.  Attempts to remove items that appear in 
+ * nested maps.  If a lookup fails in the current map, then it sees if
+ * the key is in the parent map.  Attempts to remove items that appear in
  * the parent result in a copy of the parent map.
- * 
+ *
  */
 public class HierarchicalMap<K, V> implements Map<K, V> {
   private final HashMap<K, V> map;
   private HierarchicalMap<K, V> parent;
-  
-  /** True if a copy on write was performed on parent */
-  private boolean parentCopy;
-  
+
   public HierarchicalMap() {
     this(null);
   }
-  
-  private void copyOnWrite() {
-    if (parentCopy || parent == null) {
-      // Already have exclusively owned copy
-      return;
-    }
-    HierarchicalMap<K, V> newParent = new HierarchicalMap<K,V>();
-    newParent.putAll(parent);
-    parent = newParent;
-    parentCopy = true;
-  }
-  
+
   private HierarchicalMap(HierarchicalMap<K, V> parent) {
     this.map = new HashMap<K, V>();
     this.parent = parent;
-    this.parentCopy = false;
   }
 
   public HierarchicalMap<K, V> makeChildMap() {
     return new HierarchicalMap<K,V>(this);
   }
+  @Override
   public boolean containsKey(Object key) {
     HierarchicalMap<K, V> curr = this;
     do {
@@ -70,6 +56,7 @@ public class HierarchicalMap<K, V> implements Map<K, V> {
     return false;
   }
 
+  @Override
   public V get(Object key) {
     HierarchicalMap<K, V> curr = this;
     do {
@@ -98,28 +85,12 @@ public class HierarchicalMap<K, V> implements Map<K, V> {
     return -1;
   }
 
+  @Override
   public V put(K key, V value) {
     return map.put(key, value);
   }
 
-  /**
-   * Put at a higher level in map
-   * @param key
-   * @param value
-   * @param depth
-   */
-  public void put(K key, V value, int depth) throws IllegalArgumentException {
-    HierarchicalMap<K, V> curr = this;
-    for (int i = 0; i < depth; i++) {
-      curr = curr.parent;
-      if (curr == null) {
-        throw new IllegalArgumentException("Hierarchical map didn't have "
-                    + depth + " ancestors"); 
-      }
-    }
-    curr.put(key, value);
-  }
-
+  @Override
   public void putAll(Map<? extends K, ? extends V> m) {
     map.putAll(m);
   }
@@ -145,7 +116,7 @@ public class HierarchicalMap<K, V> implements Map<K, V> {
   }
 
   @Override
-  public boolean isEmpty() { 
+  public boolean isEmpty() {
     HierarchicalMap<K, V> curr = this;
     do {
       if (!curr.map.isEmpty()) {
@@ -164,20 +135,13 @@ public class HierarchicalMap<K, V> implements Map<K, V> {
       return new HierarchicalSet<K>(this.map.keySet());
     }
   }
-  
+
   @Override
   public V remove(Object key) {
-    return remove(key, true);
-  }
-  
-  public V remove(Object key, boolean copyOnWrite) {
     V removed = map.remove(key);
     if (parent != null) {
       if (parent.containsKey(key)) {
-        if (copyOnWrite) {
-          copyOnWrite();
-        }
-        V parentRemoved = parent.remove(key, copyOnWrite);
+        V parentRemoved = parent.remove(key);
         if (removed == null) {
           removed = parentRemoved;
         }
@@ -196,7 +160,8 @@ public class HierarchicalMap<K, V> implements Map<K, V> {
   public Collection<V> values() {
     throw new STCRuntimeError("not implemented");
   }
-  
+
+  @Override
   public String toString() {
     StringBuilder sb = new StringBuilder();
     sb.append("{");
@@ -209,7 +174,7 @@ public class HierarchicalMap<K, V> implements Map<K, V> {
     sb.append("}");
     return sb.toString();
   }
-  
+
   private int writeContents(StringBuilder sb, boolean first) {
     for (Entry<K, V> e: this.map.entrySet()) {
       if (first) {
@@ -223,5 +188,5 @@ public class HierarchicalMap<K, V> implements Map<K, V> {
     }
     return map.size();
   }
-  
+
 }
