03c73befa0020ff70cbdd61b9c22368ca136e79e
Tim Armstrong <tim.g.armstrong@gmail.com>
2015-02-16 23:09:37 +0000

    Factor wu_array out into abstract data type ptr_array
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/c-utils/trunk@14743 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- src/heap_iu32.h
+++ src/heap_iu32.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 University of Chicago and Argonne National Laboratory
+ * Copyright 2014 University of Chicago and Argonne National Laboratory
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,28 @@
  * See the License for the specific language governing permissions and
  * limitations under the License
  */
-#include <heap.h>
-#include <heap_ii.h>
-#include <heap_iu32.h>
-// Empty module to make sure that heap headers compile
+
+/*
+ *  Simple binary heap with integer keys and integer values.
+ *
+ *  Implements a min-heap
+ *
+ *  Tim Armstrong, 2012-2014
+ */
+
+#ifndef __HEAP_IU32_H
+#define __HEAP_IU32_H
+
+#include <stdint.h>
+
+#define HEAP_KEY_T int
+#define HEAP_VAL_T uint32_t
+#define HEAP_PFX heap_iu32_
+
+#include "heap-template.h"
+
+#undef HEAP_KEY_T
+#undef HEAP_VAL_T
+#undef HEAP_PFX
+
+#endif // __HEAP_IU32_H
