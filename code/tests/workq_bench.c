fdc876fd41d819b655d2c1d15cba379474660887
Tim Armstrong <tim.g.armstrong@gmail.com>
2015-03-18 03:11:05 +0000

    Beginning of test/benchmark of workqueue and requestqueue.
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/lb/trunk@15045 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/tests/workq_bench.c
+++ code/tests/workq_bench.c
@@ -1,11 +1,33 @@
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
 #include "checks.h"
 #include "common.h"
 #include "layout.h"
+#include "requestqueue.h"
 #include "workqueue.h"
 
 static void make_fake_hosts(const char **fake_hosts, int comm_size);
+static adlb_code run(void);
 
 int main(int argc, char **argv)
+{
+  // TODO: fill in other things in xlb_s
+
+  adlb_code ac = run();
+
+  if (ac != ADLB_SUCCESS) {
+    fprintf(stderr, "FAILED!: %i", ac);
+    return 1;
+  }
+
+  fprintf(stderr, "DONE!\n");
+  return 0;
+}
+
+
+static adlb_code run(void)
 {
   adlb_code ac;
 
@@ -17,7 +39,6 @@ int main(int argc, char **argv)
   int my_rank = comm_size - 1;
   int nservers = 1;
 
-
   const char *fake_hosts[comm_size];
   make_fake_hosts(fake_hosts, comm_size);
 
@@ -27,11 +48,51 @@ int main(int argc, char **argv)
 
   ac = xlb_layout_init(comm_size, my_rank, nservers,
                        &hostnames, &xlb_s.layout);
-  assert(ac == ADLB_SUCCESS);
+  ADLB_CHECK(ac);
 
   ac = xlb_workq_init(xlb_s.types_size, &xlb_s.layout);
-  assert(ac == ADLB_SUCCESS);
+  ADLB_CHECK(ac);
 
+  ac = xlb_requestqueue_init(xlb_s.types_size, &xlb_s.layout);
+  ADLB_CHECK(ac);
+
+  for (int rank = 0; rank < xlb_s.layout.workers; rank++)
+  {
+    CHECK_MSG(xlb_workq_get(rank, 0) == NULL,
+              "workq_get failed for rank %i", rank);
+  }
+
+  // TODO: test work queue
+
+  for (int rank = 0; rank < xlb_s.layout.workers; rank++)
+  {
+    int match;
+
+    match = xlb_requestqueue_matches_target(rank, 0,
+                                ADLB_TGT_ACCRY_RANK);
+    CHECK_MSG(match == ADLB_RANK_NULL, "Unexpected match");
+
+    match = xlb_requestqueue_matches_type(0);
+    CHECK_MSG(match == ADLB_RANK_NULL, "Unexpected match");
+
+    ac = xlb_requestqueue_add(rank, 0, 2, true);
+    ADLB_CHECK(ac);
+
+    CHECK_MSG(xlb_requestqueue_nblocked() == 1, "Check nblocked");
+
+    match = xlb_requestqueue_matches_target(rank, 0,
+                                ADLB_TGT_ACCRY_RANK);
+    CHECK_MSG(match == rank, "Expected match");
+
+    CHECK_MSG(xlb_requestqueue_nblocked() == 0, "Check nblocked");
+
+    match = xlb_requestqueue_matches_type(0);
+    CHECK_MSG(match == rank, "Expected match");
+
+    CHECK_MSG(xlb_requestqueue_nblocked() == 0, "Check nblocked");
+  }
+
+  return ADLB_SUCCESS;
 }
 
 static void make_fake_hosts(const char **fake_hosts, int comm_size)
