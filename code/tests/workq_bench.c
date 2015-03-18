f1192782dbca848068366d60a89cab760b50097c
Tim Armstrong <tim.g.armstrong@gmail.com>
2015-03-18 14:26:03 +0000

    Modularise test a little.  Add check for bad hostnames building
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/lb/trunk@15049 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/tests/workq_bench.c
+++ code/tests/workq_bench.c
@@ -2,14 +2,22 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "adlb.h"
 #include "checks.h"
 #include "common.h"
 #include "layout.h"
 #include "requestqueue.h"
 #include "workqueue.h"
 
-static void make_fake_hosts(const char **fake_hosts, int comm_size);
 static adlb_code run(void);
+static adlb_code init(void);
+static adlb_code warmup(void);
+
+static void make_fake_hosts(const char **fake_hosts, int comm_size);
+static adlb_code check_hostnames(struct xlb_hostnames *hostnames,
+                              const char **hosts, int comm_size);
+static adlb_code setup_hostmap(struct xlb_hostnames *hostnames,
+                              const char **hosts, int comm_size);
 
 int main(int argc, char **argv)
 {
@@ -31,6 +39,81 @@ static adlb_code run(void)
 {
   adlb_code ac;
 
+  ac = init();
+  ADLB_CHECK(ac);
+
+  ac = warmup();
+  ADLB_CHECK(ac);
+
+  return ADLB_SUCCESS;
+}
+
+/*
+  Do some warming up and testing.
+ */
+static adlb_code warmup(void) {
+  adlb_code ac;
+
+  int warmup_iters = 1000;
+
+  for (int i = 0; i < warmup_iters; i++)
+  {
+
+    for (int rank = 0; rank < xlb_s.layout.workers; rank++)
+    {
+      CHECK_MSG(xlb_workq_get(rank, 0) == NULL,
+                "workq_get failed for rank %i", rank);
+    }
+
+    for (int rank = 0; rank < xlb_s.layout.workers; rank++)
+    {
+      int match = xlb_requestqueue_matches_target(rank, 0,
+                                  ADLB_TGT_ACCRY_RANK);
+      CHECK_MSG(match == ADLB_RANK_NULL, "Unexpected match");
+
+      match = xlb_requestqueue_matches_target(rank, 0,
+                                  ADLB_TGT_ACCRY_NODE);
+      CHECK_MSG(match == ADLB_RANK_NULL, "Unexpected match");
+    }
+
+    // TODO: test work queue
+
+    for (int rank = 0; rank < xlb_s.layout.workers; rank++)
+    {
+      int match;
+
+      match = xlb_requestqueue_matches_target(rank, 0,
+                                  ADLB_TGT_ACCRY_RANK);
+      CHECK_MSG(match == ADLB_RANK_NULL, "Unexpected match");
+
+      match = xlb_requestqueue_matches_type(0);
+      CHECK_MSG(match == ADLB_RANK_NULL, "Unexpected match");
+
+      ac = xlb_requestqueue_add(rank, 0, 2, true);
+      ADLB_CHECK(ac);
+
+      CHECK_MSG(xlb_requestqueue_nblocked() == 1, "Check nblocked");
+
+      match = xlb_requestqueue_matches_target(rank, 0,
+                                  ADLB_TGT_ACCRY_RANK);
+      CHECK_MSG(match == rank, "Expected match");
+
+      CHECK_MSG(xlb_requestqueue_nblocked() == 0, "Check nblocked");
+
+      match = xlb_requestqueue_matches_type(0);
+      CHECK_MSG(match == rank, "Expected match");
+
+      CHECK_MSG(xlb_requestqueue_nblocked() == 0, "Check nblocked");
+    }
+  }
+
+  return ADLB_SUCCESS;
+}
+
+static adlb_code init(void)
+{
+  adlb_code ac;
+
   // Workaround: disable debug logging to avoid calls to MPI_WTime
   xlb_debug_enabled = false;
 
@@ -46,8 +129,14 @@ static adlb_code run(void)
   ac = xlb_hostnames_fill(&hostnames, fake_hosts, comm_size, my_rank);
   ADLB_CHECK(ac);
 
+  ac = check_hostnames(&hostnames, fake_hosts, comm_size);
+  ADLB_CHECK(ac);
+
   ac = xlb_layout_init(comm_size, my_rank, nservers,
                        &hostnames, &xlb_s.layout);
+  ADLB_CHECK(ac);
+
+  ac = setup_hostmap(&hostnames, fake_hosts, comm_size);
   ADLB_CHECK(ac);
 
   ac = xlb_workq_init(xlb_s.types_size, &xlb_s.layout);
@@ -56,41 +145,7 @@ static adlb_code run(void)
   ac = xlb_requestqueue_init(xlb_s.types_size, &xlb_s.layout);
   ADLB_CHECK(ac);
 
-  for (int rank = 0; rank < xlb_s.layout.workers; rank++)
-  {
-    CHECK_MSG(xlb_workq_get(rank, 0) == NULL,
-              "workq_get failed for rank %i", rank);
-  }
-
-  // TODO: test work queue
-
-  for (int rank = 0; rank < xlb_s.layout.workers; rank++)
-  {
-    int match;
-
-    match = xlb_requestqueue_matches_target(rank, 0,
-                                ADLB_TGT_ACCRY_RANK);
-    CHECK_MSG(match == ADLB_RANK_NULL, "Unexpected match");
-
-    match = xlb_requestqueue_matches_type(0);
-    CHECK_MSG(match == ADLB_RANK_NULL, "Unexpected match");
-
-    ac = xlb_requestqueue_add(rank, 0, 2, true);
-    ADLB_CHECK(ac);
-
-    CHECK_MSG(xlb_requestqueue_nblocked() == 1, "Check nblocked");
-
-    match = xlb_requestqueue_matches_target(rank, 0,
-                                ADLB_TGT_ACCRY_RANK);
-    CHECK_MSG(match == rank, "Expected match");
-
-    CHECK_MSG(xlb_requestqueue_nblocked() == 0, "Check nblocked");
-
-    match = xlb_requestqueue_matches_type(0);
-    CHECK_MSG(match == rank, "Expected match");
-
-    CHECK_MSG(xlb_requestqueue_nblocked() == 0, "Check nblocked");
-  }
+  xlb_hostnames_free(&hostnames);
 
   return ADLB_SUCCESS;
 }
@@ -126,4 +181,56 @@ static void make_fake_hosts(const char **fake_hosts, int comm_size)
         break;
     }
   }
+}
+
+/*
+  Verify that hostnames filled correctly
+ */
+static adlb_code check_hostnames(struct xlb_hostnames *hostnames,
+                              const char **hosts, int comm_size)
+{
+  for (int rank = 0; rank < comm_size; rank++)
+  {
+    const char *expect = hosts[rank];
+    const char *actual = xlb_hostnames_lookup(hostnames, rank);
+    CHECK_MSG(strcmp(expect, actual) == 0,
+          "Expected \"%s\" Actual \"%s\"", expect, actual);
+  }
+
+  return ADLB_SUCCESS;
+}
+
+/*
+  Test hostmap building.
+ */
+static adlb_code setup_hostmap(struct xlb_hostnames *hostnames,
+                              const char **hosts, int comm_size)
+{
+  adlb_code ac;
+
+  ac = xlb_hostmap_init(&xlb_s.layout, hostnames, &xlb_s.hostmap);
+  ADLB_CHECK(ac);
+  
+  for (int rank = 0; rank < comm_size; rank++)
+  {
+    const char *hostname = hosts[rank];
+
+    int nranks;
+    int ranks[xlb_s.layout.size];
+    ac = ADLB_Hostmap_lookup(hostname, xlb_s.layout.size, ranks, &nranks);
+    ADLB_CHECK(ac);
+    
+    int matches = 0;
+    for (int i = 0; i < nranks; i++)
+    {
+      if (ranks[i] == rank)
+      {
+        matches++;
+      }
+    }
+    CHECK_MSG(matches == 1, "expected %i in ranks for host %s",
+              rank, hostname);
+  }
+
+  return ADLB_SUCCESS;
 }
