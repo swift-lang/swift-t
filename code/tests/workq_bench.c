0abeceafdfe820076187ddda9f6c202ce73d260f
Tim Armstrong <tim.g.armstrong@gmail.com>
2015-03-18 03:10:56 +0000

    WIP on workqueue benchmark - not working yet
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/lb/trunk@15041 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/tests/workq_bench.c
+++ code/tests/workq_bench.c
@@ -1,13 +1,26 @@
-#include "workqueue.h"
 #include "checks.h"
+#include "common.h"
+#include "layout.h"
+#include "workqueue.h"
 
 int main(int argc, char **argv)
 {
   adlb_code ac;
-  
+
   // Workaround: disable debug logging to avoid calls to MPI_WTime
   xlb_debug_enabled = false;
 
-  ac = xlb_workq_init(1, 64, 8);
-  ADLB_CHECK(ac);
+  xlb_s.types_size = 1;
+  int comm_size = 64;
+
+  // TODO: need way to provide hostnames
+  const struct xlb_hostnames *hostnames = NULL;
+
+  ac = xlb_layout_init(comm_size, comm_size - 1, 1,
+                       hostnames, &xlb_s.layout);
+  assert(ac == ADLB_SUCCESS);
+
+  ac = xlb_workq_init(xlb_s.types_size, &xlb_s.layout);
+  assert(ac == ADLB_SUCCESS);
+
 }
