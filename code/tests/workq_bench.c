d1229c61a331405d3ddb39bab47225a4c6d876e6
Tim Armstrong <tim.g.armstrong@gmail.com>
2015-03-18 03:11:04 +0000

    Create fake hostnames in test
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/lb/trunk@15044 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/tests/workq_bench.c
+++ code/tests/workq_bench.c
@@ -3,6 +3,8 @@
 #include "layout.h"
 #include "workqueue.h"
 
+static void make_fake_hosts(const char **fake_hosts, int comm_size);
+
 int main(int argc, char **argv)
 {
   adlb_code ac;
@@ -12,15 +14,55 @@ int main(int argc, char **argv)
 
   xlb_s.types_size = 1;
   int comm_size = 64;
+  int my_rank = comm_size - 1;
+  int nservers = 1;
 
-  // TODO: need way to provide hostnames
-  const struct xlb_hostnames *hostnames = NULL;
 
-  ac = xlb_layout_init(comm_size, comm_size - 1, 1,
-                       hostnames, &xlb_s.layout);
+  const char *fake_hosts[comm_size];
+  make_fake_hosts(fake_hosts, comm_size);
+
+  struct xlb_hostnames hostnames;
+  ac = xlb_hostnames_fill(&hostnames, fake_hosts, comm_size, my_rank);
+  ADLB_CHECK(ac);
+
+  ac = xlb_layout_init(comm_size, my_rank, nservers,
+                       &hostnames, &xlb_s.layout);
   assert(ac == ADLB_SUCCESS);
 
   ac = xlb_workq_init(xlb_s.types_size, &xlb_s.layout);
   assert(ac == ADLB_SUCCESS);
 
+}
+
+static void make_fake_hosts(const char **fake_hosts, int comm_size)
+{
+  for (int i = 0; i < comm_size; i++) {
+    switch (i % 8)
+    {
+      case 0:
+        fake_hosts[i] = "ZERO";
+        break;
+      case 1:
+        fake_hosts[i] = "ONE";
+        break;
+      case 2:
+        fake_hosts[i] = "TWO";
+        break;
+      case 3:
+        fake_hosts[i] = "THREE";
+        break;
+      case 4:
+        fake_hosts[i] = "FOUR";
+        break;
+      case 5:
+        fake_hosts[i] = "FIVE";
+        break;
+      case 6:
+        fake_hosts[i] = "SIX";
+        break;
+      case 7:
+        fake_hosts[i] = "SEVEN";
+        break;
+    }
+  }
 }
