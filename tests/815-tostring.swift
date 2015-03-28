a28ecf7b1d95d6a4ce5c9c828787b400aef58b83
Tim Armstrong <tim.g.armstrong@gmail.com>
2015-03-28 03:06:13 +0000

    Add tostring for booleans, test tostring
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@15215 dc4e9af1-7f46-4ead-bba6-71afc04862de

---
+++ tests/815-tostring.swift
@@ -0,0 +1,8 @@
+import assert;
+
+assertEqual(toString(true), "true", "true");
+assertEqual(toString(false), "false", "false");
+
+assertEqual(toString(-12), "-12", "-12");
+
+assertEqual(toString(1.234), "1.234", "1.234");
