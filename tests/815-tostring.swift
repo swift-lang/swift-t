7a3dfd3d04999eb8995067cd4049986169f2c0f2
Tim Armstrong <tim.g.armstrong@gmail.com>
2015-03-28 03:06:15 +0000

    implement toUpper and toLower
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@15217 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- tests/815-tostring.swift
+++ tests/815-tostring.swift
@@ -6,3 +6,7 @@ assertEqual(toString(false), "false", "false");
 assertEqual(toString(-12), "-12", "-12");
 
 assertEqual(toString(1.234), "1.234", "1.234");
+
+import string;
+assertEqual(toUpper("aBcD"), "ABCD", "toUpper");
+assertEqual(toLower("aBcD"), "abcd", "toLower");
