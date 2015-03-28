fa0c5edf24a2f499af20fe490907470e904e6406
Tim Armstrong <tim.g.armstrong@gmail.com>
2015-03-28 04:49:16 +0000

    Implement replaceAll for stndard library
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@15218 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- tests/513-strings-14.swift
+++ tests/513-strings-14.swift
@@ -15,11 +15,13 @@ main {
     string s1 = replace("banana", "an", "ah", 0);
     assertEqual("bahana", s1, "s1");
 
+    string s1a = replaceAll("banana", "an", "..", 0, 4);
+    assertEqual(s1a, "b..ana", "s1a");
 
     string s2 = replace(y, "ana", "i", 0);
     assertEqual("bina nana\n", s2, "s2");
 
-    string s3 = replace_all("banana", "an", "ah", 0);
+    string s3 = replaceAll("banana", "an", "ah");
     assertEqual("bahaha", s3, "s3");
 
     string s4 = replace_all(y, "ana", "i", 0);
