aadca81916f794f488c5455a7c8c39133e3ab6f8
Tim Armstrong <tim.g.armstrong@gmail.com>
2015-04-11 02:40:32 +0000

    Add makefile rules to build text grammars
    Nicer formatting for text grammar
    
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/stc/trunk@15414 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- bench/suite/annealing/o-level-takeone-test.sh
+++ bench/suite/annealing/o-level-takeone-test.sh
@@ -8,10 +8,10 @@ export SWIFT_PATH=$SCS/lib
 export PATH=$PATH:~/ExM/inst/stc/bin:~/ExM/inst/turbine/bin
 
 ARGS="--graph_file=${SCS}/data/movie_graph.txt \
-            --annealingcycles=50 \
+            --annealingcycles=1 \
             --evoreruns=100 --reruns_per_task=1 \
             --minrange=58 --maxrange=108 --rangeinc=50 \
-            --n_epochs=30 --n_steps=50"
+            --n_epochs=30 --n_steps=1"
 
 export ARGS
 
