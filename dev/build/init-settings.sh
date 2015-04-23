207df00a31921fc607480cca6aafa0fe02154f97
Justin Wozniak <wozniak@mcs.anl.gov>
2015-04-23 14:06:10 -0500

    Possibly helpful output

--- dev/build/init-settings.sh
+++ dev/build/init-settings.sh
@@ -4,6 +4,12 @@ THISDIR=$( cd $(dirname $0) && pwd )
 
 if [ ! -f "${THISDIR}/exm-settings.sh" ]; then
   cp "${THISDIR}/exm-settings.sh.template" "${THISDIR}/exm-settings.sh"
+  echo "Created ${THISDIR}/exm-settings.sh"
+else
+  echo "Already exists: ${THISDIR}/exm-settings.sh"
 fi
 
 source "${THISDIR}/exm-settings.sh"
+echo "Sourced ${THISDIR}/exm-settings.sh"
+echo "Installation target is: EXM_PREFIX=${EXM_PREFIX}"
+echo "You may edit exm-settings.sh and source again before building."
