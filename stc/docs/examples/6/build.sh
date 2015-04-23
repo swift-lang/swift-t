94c5e0a10327502db5f6d7231fbc1a6d7ff97dee
Justin M Wozniak <wozniak@mcs.anl.gov>
2015-04-23 11:01:35 -0500

    Compiler settings for BG/Q

--- stc/docs/examples/6/build.sh
+++ stc/docs/examples/6/build.sh
@@ -2,9 +2,15 @@
 set -eu
 
 # Editable settings:
-TCL=${HOME}/sfw/tcl-8.6.0
+# TCL=${HOME}/sfw/tcl-8.6.0
+# Mira/Cetus:
+TCL=${HOME}/Public/sfw/ppc64/bgxlc/dynamic/tcl-8.5.12
 TCL_CONFIG=${TCL}/lib/tclConfig.sh
+# COMPILER=GNU
+# COMPILER=BGQ-GNU
+COMPILER=BGQ
 
+echo Using COMPILER=$COMPILER
 . $TCL_CONFIG
-export TCL_INCLUDE_SPEC
-make
+export COMPILER TCL_INCLUDE_SPEC 
+make ${*}
