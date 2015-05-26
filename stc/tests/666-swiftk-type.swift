7dba4a705188dcc324f002f49c2c3251e40117d7
Justin M Wozniak <wozniak@mcs.anl.gov>
2015-05-26 11:03:43 -0500

    Formatting

--- stc/tests/666-swiftk-type.swift
+++ stc/tests/666-swiftk-type.swift
@@ -1,14 +1,12 @@
 
-// THIS-TEST-SHOULD-CAUSE-WARNING;
+// THIS-TEST-SHOULD-CAUSE-WARNING
 type blah;
-
 
 app (blah out) make_blah () {
   "/bin/echo" 12345 @stdout=out
 }
 
 blah f<"666-out.tmp"> = make_blah();
-
 
 import assert;
 import files;
