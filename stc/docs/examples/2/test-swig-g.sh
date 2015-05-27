0ab5469df5bd9bbbb7d618bb38704f9edae807b6
Justin M Wozniak <wozniak@mcs.anl.gov>
2015-05-27 10:50:17 -0500

    Make include location based on setup.sh

--- stc/docs/examples/2/test-swig-g.sh
+++ stc/docs/examples/2/test-swig-g.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-rm -fv *.o
 set -ex
+
 gcc -c -fPIC -Wall g.c
-gcc -c -fPIC -I/home/wozniak/sfw/tcl-8.6.0/include g_wrap.c
+gcc -c -fPIC -I ${TCL_HOME}/include g_wrap.c
 gcc -shared -o libg.so g_wrap.o g.o
 tclsh make-package.tcl > pkgIndex.tcl
