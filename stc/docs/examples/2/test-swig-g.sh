5ba9905aea90c97e99ce6690d1696906888d20a2
Justin M Wozniak <wozniak@mcs.anl.gov>
2015-05-27 10:57:32 -0500

    Try TCL_INCLUDE_SPEC

--- stc/docs/examples/2/test-swig-g.sh
+++ stc/docs/examples/2/test-swig-g.sh
@@ -2,6 +2,6 @@
 set -ex
 
 gcc -c -fPIC -Wall g.c
-gcc -c -fPIC -I ${TCL_HOME}/include g_wrap.c
+gcc -c -fPIC $TCL_INCLUDE_SPEC g_wrap.c
 gcc -shared -o libg.so g_wrap.o g.o
 tclsh make-package.tcl > pkgIndex.tcl
