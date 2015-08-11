18a494e0dfeafddc4882059c2058db42cf79f81c
Justin M Wozniak <wozniak@mcs.anl.gov>
2015-08-11 13:23:48 -0500

    Disable HDF5 by default

--- dev/build/turbine-build.sh
+++ dev/build/turbine-build.sh
@@ -131,13 +131,19 @@ if (( ENABLE_MKSTATIC_CRC )); then
   EXTRA_ARGS+=" --enable-mkstatic-crc-check"
 fi
 
+if [ -z "$WITH_HDF5" ]; then
+  EXTRA_ARGS+=" --with-hdf5=no"
+else
+  EXTRA_ARGS+=" --with-hdf5=$WITH_HDF5"
+fi
+
 if (( CONFIGURE )); then
   ./configure --with-adlb=${LB_INSTALL} \
               ${CRAY_ARGS} \
               --with-c-utils=${C_UTILS_INSTALL} \
               --prefix=${TURBINE_INSTALL} \
-              ${EXTRA_ARGS}
-#             --disable-log
+              ${EXTRA_ARGS} \
+              --disable-log
 fi
 
 if (( MAKE_CLEAN )); then
