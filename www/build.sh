acfdb77d60175608b73d587a10658525db2bba52
jwozniak <wozniak@mcs.anl.gov>
2016-02-11 09:44:42 -0600

    Bring in ALCF WWW changes

--- www/build.sh
+++ www/build.sh
@@ -1,4 +1,10 @@
 #!/bin/sh -eu
 
+if [ ! -f swift.css ]
+then
+  echo "Make a soft link to swift.css!"
+  exit 1
+fi
+
 asciidoc --attribute stylesheet=$PWD/swift.css \
                     -a max-width=750px -a textwidth=80 downloads.txt
