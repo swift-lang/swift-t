d00147a36f7c8ff78846d847dc86fea0f1da0045
Justin M Wozniak <wozniak@mcs.anl.gov>
2016-06-24 12:42:36 -0500

    WIP fixing Jenkins 3

--- turbine/code/maint/jenkins-configure.sh
+++ turbine/code/maint/jenkins-configure.sh
@@ -6,17 +6,4 @@ MPICH=/tmp/mpich-install
 C_UTILS=/tmp/exm-install/c-utils
 ADLB=/tmp/exm-install/lb
 TURBINE=/tmp/exm-install/turbine
-PATH=${PATH}:$MPICH/bin
-
-rm -rf autom4te.cache
-./bootstrap
-
-./configure --prefix=$TURBINE        \
-            --with-tcl=/usr          \
-            --with-mpi=$MPICH        \
-            --with-c-utils=$C_UTILS  \
-            --with-adlb=$ADLB        \
-            --with-hdf5=no           \
-            --disable-static-pkg     \
-            --disable-static
-
+PATH=${PATH}:$MPICH/bin:$TURBINE/bin
--- turbine/code/maint/jenkins.sh
+++ turbine/code/maint/jenkins.sh
@@ -9,6 +9,18 @@ source maint/jenkins-configure.sh
 
 cat export/blob.swift
 
+rm -rf autom4te.cache
+./bootstrap
+
+./configure --prefix=$TURBINE        \
+            --with-tcl=/usr          \
+            --with-mpi=$MPICH        \
+            --with-c-utils=$C_UTILS  \
+            --with-adlb=$ADLB        \
+            --with-hdf5=no           \
+            --disable-static-pkg     \
+            --disable-static
+
 make clean
 
 make V=1
