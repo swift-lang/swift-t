56f99a69d106b761f9f78d15580cabe53f5467cd
Justin M Wozniak <wozniak@mcs.anl.gov>
2016-06-24 12:50:33 -0500

    WIP fixing Jenkins 5

--- turbine/code/maint/jenkins.sh
+++ turbine/code/maint/jenkins.sh
@@ -7,22 +7,6 @@ print JENKINS.ZSH
 
 source maint/jenkins-configure.sh
 
-cat export/blob.swift
-
-rm -rf autom4te.cache
-./bootstrap
-
-./configure --prefix=$TURBINE        \
-            --with-tcl=/usr          \
-            --with-mpi=$MPICH        \
-            --with-c-utils=$C_UTILS  \
-            --with-adlb=$ADLB        \
-            --with-hdf5=no           \
-            --disable-static-pkg     \
-            --disable-static
-
-make clean
-
 make V=1
 
 make V=1 install
