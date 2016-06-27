5a8c52c5d5a75ffc82c2ca8fc815c51a1f41c91c
Justin M Wozniak <wozniak@mcs.anl.gov>
2016-06-27 14:06:29 -0500

    New test (refs #98)

---
+++ stc/tests/941-loop.swift
@@ -0,0 +1,20 @@
+
+// Reported in issue #98
+
+int a[][];
+foreach i in [1:50]
+{
+  foreach j in [1:50]
+  {
+    a[i][j]=3;
+  }
+}
+
+int b[][];
+foreach x,i in a
+{
+  foreach y,j in x
+  {
+    b[j][i]=a[i][j];
+  }
+}
