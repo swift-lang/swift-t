14d1bbee23d07381b47df471806c9d6559d4ee11
Justin M Wozniak <wozniak@mcs.anl.gov>
2016-06-28 13:30:01 -0500

    Fix clean script

--- stc/docs/examples/5/clean.sh
+++ stc/docs/examples/5/clean.sh
@@ -1,6 +1,8 @@
-#!/bin/sh -e
+#!/bin/sh
+set -e
 
-rm -f main_leaf.* extension.*
+rm -f extension.*
+rm -f main_leaf.h main_leaf.tcl
 rm -f make-package.tcl pkgIndex.tcl
 rm -f *.tic *.o *.so *.x
 rm -f user-code.swift
