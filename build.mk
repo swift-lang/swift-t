d92380a8eed8a13bf2f40dcebebe17f26909f1ee
Justin M Wozniak <wozniak@mcs.anl.gov>
2016-06-29 11:34:30 -0500

    Accommodate longer wget line

--- build.mk
+++ build.mk
@@ -3,7 +3,7 @@
 # Use build.sh
 
 ASCIIDOC = asciidoc --attribute stylesheet=$(PWD)/swift.css \
-                    -a max-width=750px -a textwidth=80
+                    -a max-width=800px
 
 # Must compile leaf.txt with make-stc-docs.zsh (snippets, etc.)
 all: guide.html gallery.html dev.html sites.html
