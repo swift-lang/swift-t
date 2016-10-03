f6102f35b37466b69c396de7c9f74b33f8a1ec3a
Justin M Wozniak <wozniak@mcs.anl.gov>
2016-10-03 14:51:25 -0500

    Skip user startup files

--- c-utils/code/maint/file-list.zsh
+++ c-utils/code/maint/file-list.zsh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/zsh -f
 set -eu
 
 # FILE LIST
@@ -7,10 +7,9 @@ set -eu
 # either for the bin or dev package
 
 print c-utils-config.h.in configure configure.ac README.txt
-print bootstrap Makefile.in src/module.mk.in
+print bootstrap Makefile.in src/c-utils.h.in src/module.mk.in
 print maint/*sh maint/*.mk
 print src/*.[ch]
-print c-utils-config.h.in src/c-utils.h.in
 print tests/module.mk.in
 print tests/*.[ch]
 print version.txt NOTICE
