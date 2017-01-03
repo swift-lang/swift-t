69b155134a05fc5d2e24c61c459ca26fd59cb0ce
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-01-03 14:05:50 -0600

    Default to GNU

--- examples/6/build.sh
+++ examples/6/build.sh
@@ -6,11 +6,11 @@ set -eu
 # Mira/Cetus:
 TCL=${HOME}/Public/sfw/ppc64/bgxlc/dynamic/tcl-8.5.12
 TCL_CONFIG=${TCL}/lib/tclConfig.sh
-# COMPILER=GNU
+COMPILER=GNU
 # COMPILER=BGQ-GNU
-COMPILER=BGQ
+# COMPILER=BGQ
 
 echo Using COMPILER=$COMPILER
 . $TCL_CONFIG
-export COMPILER TCL_INCLUDE_SPEC 
+export COMPILER TCL_INCLUDE_SPEC
 make ${*}
