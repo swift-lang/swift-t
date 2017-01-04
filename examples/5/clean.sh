4452342b2409ce8c130b5b4c7f6be05ffc1f7359
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-01-04 14:03:57 -0600

    Fix cleanup and comments

--- examples/5/clean.sh
+++ examples/5/clean.sh
@@ -1,6 +1,9 @@
-#!/bin/sh -e
+#!/bin/bash
+set -e
 
-rm -f main_leaf.* extension.*
+# Do not delete main_leaf.c, it is pasted into the Leaf Guide.
+
+rm -f main_leaf.{h,o,tcl} extension.*
 rm -f make-package.tcl pkgIndex.tcl
 rm -f *.tic *.o *.so *.x
 rm -f user-code.swift
