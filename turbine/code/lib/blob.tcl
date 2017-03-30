f1717aa2ca89c68478820c7cd1b2ac08108320d5
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-03-30 14:38:29 -0500

    Blob doc improvements

--- turbine/code/lib/blob.tcl
+++ turbine/code/lib/blob.tcl
@@ -1,3 +1,4 @@
+
 # Copyright 2013 University of Chicago and Argonne National Laboratory
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,30 +14,40 @@
 # limitations under the License
 # Turbine builtin functions for blob manipulation
 
+# DOCSECTION(Tcl convenience functions)
+# DOCN(These functions operate on Tcl blobs.)
+
 namespace eval turbine {
 
     namespace export blob_fmt blob_debug blob_debug_ints
 
-  proc blob_size_async { out blob } {
-    rule "$blob" "blob_size_body $out $blob" \
-        name "blob_size-$out-$blob"
-  }
+    # DOCD(blob_size_async out blob,
+    #      Turbine rule to obtain the size of TD +blob+ in TD +out+)
+    proc blob_size_async { out blob } {
+        rule "$blob" "blob_size_body $out $blob" \
+            name "blob_size-$out-$blob"
+    }
+    proc blob_size_body { out blob } {
+        set blob_val [ retrieve_decr_blob $blob ]
+        set sz [ blob_size $blob_val ]
+        store_integer $out $sz
+        adlb::blob_free $blob
+    }
 
-  proc blob_size_body { out blob } {
-    set blob_val [ retrieve_decr_blob $blob ]
-    set sz [ blob_size $blob_val ]
-    store_integer $out $sz
-    adlb::blob_free $blob
-  }
-
+  # DOCD(blob_size blob,
+  #      Obtain the size of blob)
   proc blob_size { blob_val } {
     return [ lindex $blob_val 1 ]
   }
 
+  # DOCD(blob_null blob,
+  #      Store +NULL+ in the blob)
   proc blob_null { result input } {
       store_blob $result 0 0
   }
 
+  # DOCD(string2blob,
+  #      Turbine rule to translate TD blob +input+ into TD string +result+)
   proc string2blob { result input } {
     rule $input "string2blob_body $input $result" \
         name "string2blob-$input-$result"
@@ -46,6 +57,8 @@ namespace eval turbine {
     store_blob_string $result $t
   }
 
+  # DOCD(blob2string,
+  #      Turbine rule to translate TD string +input+ in TD blob +result+)
   proc blob2string { result input } {
     rule $input "blob2string_body $input $result" \
         name "sfb-$input-$result"
@@ -55,8 +68,11 @@ namespace eval turbine {
     store_string $result $s
   }
 
+  # DOCD(blob2floats,
+  #      `Obtain doubles from blob, return as dictionary
+  #       mapping integer index to double')
   proc blob2floats_impl { blob } {
-    set s       [ blobutils_sizeof_float ]
+    set s      [ blobutils_sizeof_float ]
     set p      [ blobutils_cast_long_to_dbl_ptr [ lindex $blob 0 ] ]
     set length [ lindex $blob 1 ]
 
@@ -68,10 +84,11 @@ namespace eval turbine {
     return $result
   }
 
+  # DOCD(matrix_from_blob_fortran_impl blob m n,
+  #      `Obtain doubles from blob, return as dictionary
+  #       mapping integer indices to double in Fortran order,
+  #       +m+ is the number of rows, +n+ is the number of columns')
   # This is just in Fortran order (column-major) for now
-  # b: the blob
-  # m: number of rows
-  # n: number of columns
   proc matrix_from_blob_fortran_impl { L m n } {
     set s       [ blobutils_sizeof_float ]
     set p       [ blobutils_cast_long_to_dbl_ptr [ lindex $L 0 ] ]
@@ -110,12 +127,12 @@ namespace eval turbine {
     return $A
   }
 
+  # DOCD(floats2blob out in, Store Tcl floats from dict into blob)
   proc floats2blob { out in } {
     set floats [ lindex $in 0 ]
     set blob [ lindex $out 0 ]
     rule $floats "floats2blob_body $blob $floats"
   }
-
   proc floats2blob_body { blob floats } {
     set floats_val [ adlb::retrieve $floats container ]
     set blob_val [ floats2blob_impl $floats_val ]
@@ -123,17 +140,16 @@ namespace eval turbine {
     store_blob $blob $blob_val
     adlb::local_blob_free $blob_val
   }
-
   proc floats2blob_impl { kv_dict } {
     return [ adlb::blob_from_float_list [ sorted_dict_values $kv_dict ] ]
   }
 
+  # DOCD(floats2blob out in, Store Tcl ints from dict into blob)
   proc ints2blob { out in } {
     set ints [ lindex $in 0 ]
     set blob [ lindex $out 0 ]
     rule $ints "ints2blob_body $blob $ints"
   }
-
   proc ints2blob_body { blob ints } {
     set ints_val [ adlb::retrieve $ints container ]
     set blob_val [ ints2blob_impl $ints_val ]
@@ -141,11 +157,11 @@ namespace eval turbine {
     store_blob $blob $blob_val
     adlb::local_blob_free $blob_val
   }
-
   proc ints2blob_impl { kv_dict } {
     return [ adlb::blob_from_int_list [ sorted_dict_values $kv_dict ] ]
   }
 
+  # DOCD(floats2blob n, Store +n+ zeroes into new blob)
   proc blob_zeroes_float { N } {
 
       log "blob_zeroes_float($N)"
@@ -158,6 +174,8 @@ namespace eval turbine {
       return [ list [ blobutils_cast_to_long $p ] $length ]
   }
 
+  # DOCD(turbine_run_output_blob dummy blob,
+  #      `Store blob as +turbine_run()+ result, +dummy+ is unused')
   proc turbine_run_output_blob { outputs b } {
 
       rule [ list $b ] "turbine_run_output_blob_body $b" \
@@ -185,7 +203,9 @@ namespace eval turbine {
       eval $action
   }
 
-  proc blob_debug_ints { ptr n } {
+  # DOCD(blob_debug_ints blob, Print blob of ints)
+  proc blob_debug_ints { b } {
+      lassign $b ptr n
       puts "blob_debug_ints: $ptr $n"
       for { set i 0 } { $i < $n } { incr i } {
           set v [ blobutils_get_int $ptr $i ]
@@ -193,17 +213,23 @@ namespace eval turbine {
       }
   }
 
+    # DOCD(blob_fmt blob,
+    #      Return printable blob metadata
+    #      (TD, pointer, length) as string)
     proc blob_fmt { b } {
         return [ format "( <%i> pointer=%X length=%i )" \
                      [ lindex $b 2 ] [ lindex $b 0 ] [ lindex $b 1 ] ]
     }
 
+    # DOCD(blob_fmt blob,
+    #      Print blob metadata (TD, pointer, length) via +blob_fmt+)
     proc blob_debug { b } {
         puts [ blob_fmt $b ]
     }
 
-    # Input:  L: a Tcl dict of integer->integer indexed from 0
-    # Output:    a SWIG pointer (double*) to a fresh double array
+    # DOCD(blob_dict_to_int_array,
+    #      `Input:  L: a Tcl dict of integer->integer indexed from 0. +
+    #       Output:    a SWIG pointer (+int*+) to a fresh array of +int+.')
     proc blob_dict_to_int_array { d } {
         set L [ dict size $d ]
         set bytes [ expr $L * [ blobutils_sizeof_int ] ]
@@ -216,13 +242,18 @@ namespace eval turbine {
         return $ptr
     }
 
+    # DOCD(blob_string_list_to_char_ptr_ptr,
+    #      `Convert Tcl list of strings to +char**+. +
+    #       Helpful for passing data into C-style argc/argv interfaces.')
     proc blob_string_list_to_char_ptr_ptr { L } {
         set D [ list2dict $L ]
         return [ blob_string_dict_to_char_ptr_ptr $D ]
     }
 
-    # Input:  L: a Tcl dict of integer->string indexed from 0
-    # Output:    a SWIG pointer (char**) to the C strings
+    # DOCD(blob_string_dict_to_char_ptr_ptr d,
+    #      `Input: A Tcl dict of integer->string indexed from 0. +
+    #       Output: A SWIG pointer (+char**+) to the C strings. +
+    #       Helpful for passing data into C-style argc/argv interfaces.')
     proc blob_string_dict_to_char_ptr_ptr { d } {
         set argc [ dict size $d ]
         # Allocate array of char*
@@ -244,13 +275,19 @@ namespace eval turbine {
         return $charss
     }
 
-    # Deprecated alias for blob_string_dict_to_char_ptr_ptr
+    # DOCD(`blob_strings_to_char_ptr_ptr d',
+    #      Deprecated alias for +blob_string_dict_to_char_ptr_ptr+.)
     proc blob_strings_to_char_ptr_ptr { d } {
         return [ blob_string_dict_to_char_ptr_ptr $d ]
     }
 
-    # Puts result (char**) in argv_name
-    # Returns argc
+    # DOCD(blob_string_list_to_argv program L argv_name,
+    #      `Convert Tcl list of strings +L+ to +char\*\*+. +
+    #       Helpful for passing data into C-style argc/argv interfaces. +
+    #       Puts result (+char**+) in +argv_name+. +
+    #       +program+: Program name as string (i.e., argv[0]). +
+    #       +L+: list of strings (i.e., argv[1]...argv[argc-1]). +
+    #       Returns +argc+.')
     proc blob_string_list_to_argv { program L argv_name } {
         upvar $argv_name argv
         set D [ list2dict [ list $program {*}$L ] ]
