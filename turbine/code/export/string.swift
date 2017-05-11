97b8696d7dc122149ea56a078656e528a1ade541
Justin Wozniak <wozniak@mcs.anl.gov>
2017-05-11 16:09:43 -0500

    Provide a default separator argument for join()

--- turbine/code/export/string.swift
+++ turbine/code/export/string.swift
@@ -101,7 +101,7 @@
   [ "set <<s>> [ turbine::string_join_impl <<A>> <<separator>> ]" ];
 
 @pure
-(string s) join(string A[], string separator)
+(string s) join(string A[], string separator=",")
   "turbine" "0.4.0" // "string_join"
   [ "set <<s>> [ turbine::string_join_impl <<A>> <<separator>> ]" ];
 
