feeea16403375b114aadbcbda59992f017bebee3
Justin M Wozniak <Justin Wozniak>
2017-07-26 13:28:33 -0500

    Solve #128

--- stc/tests/5611-file-analysis.swift
+++ stc/tests/5611-file-analysis.swift
@@ -1,4 +1,9 @@
 (file o) f()
 {
-  trace("file: " + filename(o));
+  // Remove next two lines to reproduce #128
+  file r = input("5611-file-analysis.txt");
+  o = r;
+  // Note: file r does not exist!  But f() is unused.
+
+  trace("file: " + filename(r));
 }
