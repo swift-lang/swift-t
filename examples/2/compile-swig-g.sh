b1bc7fe57e977e98ab343b3a100ddbff6806520b
Justin Wozniak <wozniak@mcs.anl.gov>
2018-02-27 11:12:17 -0600

    Minor improvements (for FPGA work)

--- examples/2/compile-swig-g.sh
+++ examples/2/compile-swig-g.sh
@@ -1,8 +1,12 @@
-#!/bin/sh
-set -ex
+#!/bin/bash
+set -e
 
 # Compile the SWIG-generated Tcl extension library
 
+# Load the Turbine build settings (TCL_INCLUDE_SPEC)
+source $( turbine -C )
+
+set -x
 gcc -c -fPIC -Wall g.c
 gcc -c -fPIC $TCL_INCLUDE_SPEC g_wrap.c
 gcc -shared -o libg.so g_wrap.o g.o
--- examples/2/run.sh
+++ examples/2/run.sh
@@ -1,17 +1,22 @@
-#!/bin/sh -eu
+#!/bin/sh
+set -eu
 
-echo "Stand-alone"
+echo "Stand-alone C program"
 ./test-g.sh
 
-echo "SWIG"
+echo
+echo "Run SWIG"
 swig g.i
 
-echo "Compile SWIG"
+echo
+echo "Compile SWIG-generated module"
 ./compile-swig-g.sh
 
+echo
 echo "TEST-G-TCL"
 export TCLLIBPATH=$PWD
 tclsh test-g.tcl
 
+echo
 echo "TEST-G-SWIFT"
 swift-t -r $PWD test-g-1.swift
