2e27ef75d7a3e4d8006f6bc5e1f2c7ec15a99f2f
Justin Wozniak <woz@anl.gov>
2020-03-09 18:24:18 -0500

    Try /bin/sh in examples/9

--- examples/9/run.sh
+++ examples/9/run.sh
@@ -1,12 +1,12 @@
-#!/bin/zsh
+#!/bin/sh
 set -eu
 
 echo run.sh $PATH
-print -l $path
 which turbine
 
 ./build.sh
 
 export TURBINE_LOG=1 TURBINE_DEBUG=0 ADLB_DEBUG=0
 set -x
-=mpiexec -n 4 ./controller.x
+which mpiexec
+mpiexec -n 4 ./controller.x
