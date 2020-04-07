d4eb1d3764255aab184ac88b5ca3b0b48ec207ff
Justin M Wozniak <woz@anl.gov>
2020-04-07 16:15:38 -0500

    WIP Spack test

--- dev/jenkins-spack.sh
+++ dev/jenkins-spack.sh
@@ -4,11 +4,38 @@ set -eu
 # JENKINS SPACK SH
 # Install Swift/T from Jenkins under various techniques
 
+setopt PUSHD_SILENT
+
+git-log()
+{
+  git log -n 1 --color=always --date="format:%Y-%m-%d %H:%M" --pretty=format:"%Cblue%h%Creset %ad %Cgreen%s%Creset%n"
+}
+
 set -x
+
+git-log
+
+ls -l
+
 mkdir -pv /tmp/ExM/jenkins-spack
-cd /tmp/ExM/jenkins-spack
+pushd /tmp/ExM/jenkins-spack
 
 if [[ ! -d spack ]]
 then
   git clone https://github.com/spack/spack.git
+  pushd spack
+  git checkout develop
+  popd
 fi
+
+pushd spack
+git-log
+git pull
+git-log
+popd
+
+PATH=/tmp/ExM/jenkins-spack/spack/bin:$PATH
+
+which spack
+
+spack install stc@master
