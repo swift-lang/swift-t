56fa9cfa94a94b7b7d3964758f766c849f19b6f4
Justin M Wozniak <woz@anl.gov>
2020-07-29 16:25:31 -0500

    Better swift-t -v output

--- stc/code/etc/stc-config.sh
+++ stc/code/etc/stc-config.sh
@@ -4,24 +4,28 @@
 # Bash and ZSH compatible script that loads STC configuration settings into
 # shell variables.
 # Can be sourced by other scripts.
-# This will abort the process if any errors are encountered.
 # This is filtered by build.xml target "install"
 
 # Exit codes: (cf. ExitCode.java)
 EXIT_ERROR_SCRIPT=6
 
-# Find Turbine (for include path).  Order of priority is:
+# These values are filtered in by build.xml
+TURBINE_DEFAULT_HOME=@TURBINE_HOME@
+STC_SRC=@STC_SRC@
+DEBIAN_BUILD=@DEBIAN_BUILD@
+USE_JAVA=@USE_JAVA@
+# End build.xml variables
+
+# Find Turbine (for include path).  The order of priority is:
 # 1. User-set TURBINE_HOME environment variable
 # 2. TURBINE_DEFAULT_HOME - the build-time setting
-TURBINE_DEFAULT_HOME=@TURBINE_HOME@ # Filled in by build.xml
 TURBINE_HOME=${TURBINE_HOME:-${TURBINE_DEFAULT_HOME}}
 
-DEBIAN_BUILD=@DEBIAN_BUILD@ # Filled in by build.xml
 if (( DEBIAN_BUILD ))
 then
-  TURBINE_TOP=@TURBINE_HOME@/lib/turbine
+  TURBINE_TOP=${TURBINE_DEFAULT_HOME}/lib/turbine
 else
-  TURBINE_TOP=@TURBINE_HOME@
+  TURBINE_TOP=${TURBINE_DEFAULT_HOME}
 fi
 
 if [[ ! -x "${TURBINE_HOME}/bin/turbine" ]]
@@ -31,7 +35,6 @@ then
   exit ${EXIT_ERROR_SCRIPT}
 fi
 
-USE_JAVA=@USE_JAVA@
 if (( ${#USE_JAVA} > 0 ))
 then
   JVM=${USE_JAVA}
