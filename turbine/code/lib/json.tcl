c5cc0051f2e167011290de31a0e2ca9bc3d70cb1
Justin M Wozniak <woz@anl.gov>
2020-08-03 11:09:15 -0500

    Reorder into logical sections

--- turbine/code/lib/json.tcl
+++ turbine/code/lib/json.tcl
@@ -3,13 +3,131 @@
 # Special features for handling JSON
 
 # Encode paths:
-#  Simple encodes: use json_encode_infer to translate
-#                  from Turbine types
+#  Simple encodes: use json_encode_infer to infer
+#                  from Turbine types (via adlb::typeof)
 #  retype/format encode: use json_encode_translate to translate
 #                        from user type strings
 
+# Major sections are prefixed with ##
+
 namespace eval turbine {
 
+  ## ARRAY
+  proc json_encode_array { result inputs } {
+    rule $inputs "json_encode_array_body $result $inputs" \
+        name "json_encode_array-$result"
+  }
+  proc json_encode_array_body { result args } {
+    set s [ json_encode_array_contents_action $result {*}$args ]
+    store_string $result "\[$s\]"
+  }
+  proc json_encode_array_contents { result inputs } {
+    rule $inputs "json_encode_array_contents_body $result $inputs" \
+        name "json_encode_array-$result"
+  }
+  proc json_encode_array_contents_body { result args } {
+    set s [ json_encode_array_contents_action $result {*}$args ]
+    store_string $result $s
+  }
+  proc json_encode_array_contents_action { result args } {
+    set L [ list ]
+    set i 0
+    foreach a $args {
+      set name  ""
+      set value [ retrieve_decr $a ]
+      set type  [ adlb::typeof  $a ]
+      lappend L [ list $name $value $type ]
+      incr i
+    }
+    set s [ json_encode_infer {*}$L ]
+    return $s
+  }
+
+  ## ARRAY RETYPE
+  proc json_encode_array_retype { result inputs } {
+    set types [ list_pop_first inputs ]
+    rule $inputs "json_encode_array_retype_body $result $types $inputs" \
+        name "json_encode_array_retype-$result"
+  }
+  proc json_encode_array_retype_body { result types args } {
+    set s [ json_encode_array_contents_retype_action $types {*}$args ]
+    store_string $result "\[$s\]"
+  }
+  proc json_encode_array_contents_retype { result inputs } {
+    set types [ list_pop_first inputs ]
+    rule $inputs "json_encode_array_contents_retype_body $result $types $inputs" \
+        name "json_encode_array_retype-$result"
+  }
+  proc json_encode_array_contents_retype_body { result types args } {
+    set s [ json_encode_array_contents_retype_action $types {*}$args ]
+    store_string $result $s
+  }
+  proc json_encode_array_contents_retype_action { types args } {
+    # A dict (index->"name"):
+    set D_types [ adlb::enumerate $types dict all 0 1 ]
+
+    json_encode_check_count [ dict size $D_types ] $args \
+        "json_encode_array_retype" "types"
+
+    set L [ list ]
+    set i 0
+    foreach a $args {
+      set name  ""
+      set type  [ dict get $D_types $i ]
+      set value [ retrieve_decr $a ]
+      lappend L [ list $name $type $value ]
+      incr i
+    }
+    set s [ json_encode_translate {*}$L ]
+    return $s
+  }
+
+  ## ARRAY FORMAT
+  proc json_encode_array_format { result inputs } {
+    set fmt [ list_pop_first inputs ]
+    rule $inputs "json_encode_array_format_body $result $fmt $inputs" \
+        name "json_encode_array_format-$result"
+  }
+  proc json_encode_array_format_body { result fmt args } {
+    set s [ json_encode_array_contents_format_action $fmt {*}$args ]
+    store_string $result "\[$s\]"
+  }
+  proc json_encode_array_contents_format { result inputs } {
+    set fmt [ list_pop_first inputs ]
+    rule $inputs "json_encode_array_contents_format_body $result $fmt $inputs" \
+        name "json_encode_array_format-$result"
+  }
+  proc json_encode_array_contents_format_body { result fmt args } {
+    set s [ json_encode_array_contents_format_action $fmt {*}$args ]
+    store_string $result $s
+  }
+  proc json_encode_array_contents_format_action { fmt args } {
+
+    set fmt_value [ retrieve $fmt ]
+    set fmts [ ::split $fmt_value ]
+    # show fmts
+
+    json_encode_check_count [ llength $fmts ] $args \
+        "json_encode_array_format" "types"
+
+    set L [ list ]
+    set i 0
+    foreach a $args {
+      set name  ""
+      set fmti  [ lindex $fmts $i ]
+      if { [ string first "%" $fmti ] != 0 } {
+        turbine_error "json_encode_array_format(): " \
+            "format token without %: '$fmti'"
+      }
+      set value [ retrieve_decr $a ]
+      lappend L [ list $name $fmti $value ]
+      incr i
+    }
+    set s [ json_encode_translate {*}$L ]
+    return $s
+  }
+
+  ## OBJECT
   proc json_encode_object { result inputs } {
     set names [ list_pop_first inputs ]
     rule $inputs "json_encode_object_body $result $names $inputs" \
@@ -48,39 +166,7 @@ namespace eval turbine {
     return $s
   }
 
-  # Encode into comma separated list, possibly with "name": prefixes
-  # Uses Turbine type inferencing to retain Turbine types
-  proc json_encode_infer { args } {
-    # List of key-values
-    set kvs [ list ]
-    # nvt: name+value+type (strings), name may be empty for JSON array
-    foreach nvt $args {
-      lassign $nvt name value type
-      set token [ json_prefix $name $value ]
-      if { $type eq "float" || $type eq "integer" } {
-        append token $value
-      } elseif { $type eq "string" } {
-        append token "\"$value\""
-      }
-      lappend kvs $token
-    }
-    set result [ join $kvs ", " ]
-    return $result
-  }
-
-  proc json_prefix { name value } {
-    # Sets up the prefix token for an array or object.
-    # The value will be appended later.
-    if { $name eq "" } {
-      # JSON array - no prefix
-      set token ""
-    } else {
-      # JSON object - prefix is "name":
-      set token "\"$name\":"
-    }
-    return $token
-  }
-
+  ## OBJECT RETYPE
   proc json_encode_object_retype { result inputs } {
     set names [ list_pop_first inputs ]
     set types [ list_pop_first inputs ]
@@ -126,119 +212,7 @@ namespace eval turbine {
     return $s
   }
 
-  proc json_encode_array { result inputs } {
-    rule $inputs "json_encode_array_body $result $inputs" \
-        name "json_encode_array-$result"
-  }
-  proc json_encode_array_body { result args } {
-    set s [ json_encode_array_contents_action $result {*}$args ]
-    store_string $result "\[$s\]"
-  }
-  proc json_encode_array_contents { result inputs } {
-    rule $inputs "json_encode_array_contents_body $result $inputs" \
-        name "json_encode_array-$result"
-  }
-  proc json_encode_array_contents_body { result args } {
-    set s [ json_encode_array_contents_action $result {*}$args ]
-    store_string $result $s
-  }
-  proc json_encode_array_contents_action { result args } {
-    set L [ list ]
-    set i 0
-    foreach a $args {
-      set name  ""
-      set value [ retrieve_decr $a ]
-      set type  [ adlb::typeof  $a ]
-      lappend L [ list $name $value $type ]
-      incr i
-    }
-    set s [ json_encode_infer {*}$L ]
-    return $s
-  }
-
-  proc json_encode_array_retype { result inputs } {
-    set types [ list_pop_first inputs ]
-    rule $inputs "json_encode_array_retype_body $result $types $inputs" \
-        name "json_encode_array_retype-$result"
-  }
-  proc json_encode_array_retype_body { result types args } {
-    set s [ json_encode_array_contents_retype_action $types {*}$args ]
-    store_string $result "\[$s\]"
-  }
-  proc json_encode_array_contents_retype { result inputs } {
-    set types [ list_pop_first inputs ]
-    rule $inputs "json_encode_array_contents_retype_body $result $types $inputs" \
-        name "json_encode_array_retype-$result"
-  }
-  proc json_encode_array_contents_retype_body { result types args } {
-    set L [ list ]
-    set s [ json_encode_array_contents_retype_action $types {*}$args ]
-    store_string $result $s
-  }
-  proc json_encode_array_contents_retype_action { types args } {
-    # A dict (index->"name"):
-    set D_types [ adlb::enumerate $types dict all 0 1 ]
-
-    json_encode_check_count [ dict size $D_types ] $args \
-        "json_encode_array_retype" "types"
-
-    set L [ list ]
-    set i 0
-    foreach a $args {
-      set name  ""
-      set type  [ dict get $D_types $i ]
-      set value [ retrieve_decr $a ]
-      lappend L [ list $name $type $value ]
-      incr i
-    }
-    set s [ json_encode_translate {*}$L ]
-    return $s
-  }
-
-  proc json_encode_array_format { result inputs } {
-    set fmt [ list_pop_first inputs ]
-    rule $inputs "json_encode_array_format_body $result $fmt $inputs" \
-        name "json_encode_array_format-$result"
-  }
-  proc json_encode_array_format_body { result fmt args } {
-    set s [ json_encode_array_contents_format_action $fmt {*}$args ]
-    store_string $result "\[$s\]"
-  }
-  proc json_encode_array_contents_format { result inputs } {
-    set fmt [ list_pop_first inputs ]
-    rule $inputs "json_encode_array_contents_format_body $result $fmt $inputs" \
-        name "json_encode_array_format-$result"
-  }
-  proc json_encode_array_contents_format_body { result fmt args } {
-    set s [ json_encode_array_contents_format_action $fmt {*}$args ]
-    store_string $result $s
-  }
-  proc json_encode_array_contents_format_action { fmt args } {
-
-    set fmt_value [ retrieve $fmt ]
-    set fmts [ ::split $fmt_value ]
-    # show fmts
-
-    json_encode_check_count [ llength $fmts ] $args \
-        "json_encode_array_format" "types"
-
-    set L [ list ]
-    set i 0
-    foreach a $args {
-      set name  ""
-      set fmti  [ lindex $fmts $i ]
-      if { [ string first "%" $fmti ] != 0 } {
-        turbine_error "json_encode_array_format(): " \
-            "format token without %: '$fmti'"
-      }
-      set value [ retrieve_decr $a ]
-      lappend L [ list $name $fmti $value ]
-      incr i
-    }
-    set s [ json_encode_translate {*}$L ]
-    return $s
-  }
-
+  ## OBJECT FORMAT
   proc json_encode_object_format { result inputs } {
     set names [ list_pop_first inputs ]
     set fmt   [ list_pop_first inputs ]
@@ -289,7 +263,47 @@ namespace eval turbine {
     return $s
   }
 
+  ## UTILITIES
+
+  proc json_encode_infer { args } {
+    # Encode and return all args in comma separated list,
+    #        possibly with "name": prefixes
+    # Uses Turbine type inferencing to retain Turbine types
+
+    # List of key-values
+    set kvs [ list ]
+    # nvt: name+value+type (strings), name may be empty for JSON array
+    foreach nvt $args {
+      lassign $nvt name value type
+      set token [ json_prefix $name $value ]
+      if { $type eq "float" || $type eq "integer" } {
+        append token $value
+      } elseif { $type eq "string" } {
+        append token "\"$value\""
+      }
+      lappend kvs $token
+    }
+    set result [ join $kvs ", " ]
+    return $result
+  }
+
+  proc json_prefix { name value } {
+    # Sets up the prefix token for an array or object.
+    # The value will be appended later.
+    if { $name eq "" } {
+      # JSON array - no prefix
+      set token ""
+    } else {
+      # JSON object - prefix is "name":
+      set token "\"$name\":"
+    }
+    return $token
+  }
+
   proc json_encode_translate { args } {
+    # Encode and return all args as requested type
+    #        in comma separated list,
+    #        possibly with "name": prefixes
     # args: list of ntv: name,type,value (strings)
     # List of results
     set L [ list ]
