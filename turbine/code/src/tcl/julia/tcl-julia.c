41ff6aeaf69ba6423cebbbd38f78c7cc1cbbd454
Justin Wozniak <woz@anl.gov>
2021-01-05 11:15:21 -0600

    Bring in Julia updates (ref #190)

--- turbine/code/src/tcl/julia/tcl-julia.c
+++ turbine/code/src/tcl/julia/tcl-julia.c
@@ -8,6 +8,7 @@
 
 #include "config.h"
 
+#include <dlfcn.h>
 #include <stdio.h>
 
 #include <tcl.h>
@@ -29,25 +30,34 @@
 
 static bool initialized = false;
 
-static void inline
-julia_initialize(void)
+static inline int
+julia_initialize(Tcl_Interp* interp, Tcl_Obj* const objv[])
 {
-  jl_init(NULL);
-  JL_SET_STACK_BASE;
+  if (initialized) return TCL_OK;
+  DEBUG_TCL_TURBINE("julia initialize...");
+  void* rc = dlopen("libjulia.so", RTLD_NOW | RTLD_GLOBAL);
+  DEBUG_TCL_TURBINE("dlopen(libjulia.so): OK");
+  TCL_CONDITION(rc != NULL, "Turbine-Julia: "
+                "could not dlopen(libjulia.so)!");
+  jl_init();
   initialized = true;
+  DEBUG_TCL_TURBINE("julia initialized.");
+  return TCL_OK;
 }
 
 static int
-julia_eval(const char* code, Tcl_Obj** result)
+julia_eval(Tcl_Interp* interp, Tcl_Obj* const objv[],
+           const char* code, Tcl_Obj** result)
 {
-  if (!initialized) julia_initialize();
+  int rc = julia_initialize(interp, objv);
+  TCL_CONDITION(rc == TCL_OK, "Turbine-Julia: "
+                "could not initialize!");
   int length = strlen(code);
   char assignment[length + 32];
   DEBUG_TCL_TURBINE("julia evaluation:\n%s", code);
   sprintf(assignment, "_t = %s", code);
   // sprintf(assignment, "_t = sqrt(2.0)");
   jl_eval_string(assignment);
-
   jl_value_t* value = jl_eval_string("\"$_t\n\"");
   char* s = jl_string_data(value);
   chomp(s);
@@ -63,7 +73,7 @@ Julia_Eval_Cmd(ClientData cdata, Tcl_Interp* interp,
   TCL_ARGS(2);
   char* code = Tcl_GetString(objv[1]);
   Tcl_Obj* result = NULL;
-  int rc = julia_eval(code, &result);
+  int rc = julia_eval(interp, objv, code, &result);
   TCL_CHECK(rc);
   Tcl_SetObjResult(interp, result);
   return TCL_OK;
