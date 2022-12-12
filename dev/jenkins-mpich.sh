977f5955e8e0d08f6d7318c9a66746128f121252
Justin Wozniak <woz@anl.gov>
2022-12-12 15:11:03 -0600

    Initial MPICH builder for Jenkins

---
+++ dev/jenkins-mpich.sh
@@ -0,0 +1,24 @@
+
+# JENKINS MPICH SH
+# Build MPICH for GCE Jenkins
+
+set -eu
+
+renice --priority 19 --pid $$
+
+JENKINS=/nfs/gce/projects/Swift-T/jenkins
+mkdir -pv $JENKINS/downloads
+cd $JENKINS/downloads
+rm -fv mpich-4.0.3.tar.gz
+wget --no-verbose https://www.mpich.org/static/downloads/4.0.3/mpich-4.0.3.tar.gz
+tar xfz mpich-4.0.3.tar.gz
+cd mpich-4.0.3
+CFG=( --prefix=$WORKSPACE/sfw/mpich-4.0.3
+      --with-pm=hydra
+      --enable-shared
+      --with-enable-g=dbg,meminit )
+
+set -x
+./configure ${CFG[@]}
+make
+make install
