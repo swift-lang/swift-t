3f86030f7b4f934f1c2ba418d052942af220d655
Justin Wozniak <woz@anl.gov>
2023-01-24 11:47:19 -0600

    Install pip package python-csv

--- dev/jenkins-anaconda.sh
+++ dev/jenkins-anaconda.sh
@@ -7,9 +7,11 @@ set -eux
 renice --priority 19 --pid $$
 
 rm -fv Anaconda3-2022.10-Linux-x86_64.sh
+rm -fv $WORKSPACE/sfw/Anaconda3
 wget --no-verbose https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh
-nice -n 19 bash Anaconda3-2022.10-Linux-x86_64.sh -b -f -p $WORKSPACE/sfw/Anaconda3
+bash Anaconda3-2022.10-Linux-x86_64.sh -b -p $WORKSPACE/sfw/Anaconda3
 
 PATH=$WORKSPACE/sfw/Anaconda3/bin:$PATH
 which pip
 pip install deap
+pip install python-csv
