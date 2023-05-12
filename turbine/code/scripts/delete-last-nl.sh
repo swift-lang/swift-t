16321dc0c2afabe5cac8b342d92742fd0a97b841
Justin Wozniak <woz@anl.gov>
2023-05-12 16:08:10 -0500

    Arithmetic fix

--- turbine/code/scripts/delete-last-nl.sh
+++ turbine/code/scripts/delete-last-nl.sh
@@ -16,6 +16,6 @@ fi
 # set -x
 FILE=$1
 SIZE=$( stat --format "%s" $FILE )
-let SIZE=SIZE-1 || true # No error on SIZE=0
+SIZE=$(( SIZE-1 ))
 mv --backup=numbered $FILE $FILE.bak
 head --bytes=$SIZE $FILE.bak > $FILE
