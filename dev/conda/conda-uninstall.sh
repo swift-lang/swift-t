81ef85e20e062acd477111a8728c48314ae2a426
Justin Wozniak <woz@anl.gov>
2024-01-24 11:38:29 -0600

    Fix file format

--- dev/conda/conda-uninstall.sh
+++ dev/conda/conda-uninstall.sh
@@ -1,1 +1,63 @@
-#!/bin/zshset -eu# CONDA UNINSTALL# Uninstall Swift/T and any cached packages from the Anaconda in PATH# Useful before reinstalling in a test installation# Report information about active Python/Conda:if ! which conda >& /dev/nullthen  print "No conda!"  return 1fiprintprint "using python:" $( which python )print "using conda: " $( which conda )printCONDA_PREFIX=${CONDA_EXE:h:h}conda uninstall swift-tSWIFTS=( $CONDA_PREFIX/pkgs/swift-t-*(/) )print "Found ${#SWIFTS} Swift/T directories"for S in $SWIFTSdo  rm -rf $SdoneSWIFTS=( $CONDA_PREFIX/pkgs/swift-t-* )for S in $SWIFTSdo  rm -v $Sdone# Local Variables:# buffer-file-coding-system: utf-8-unix# End:
+#!/bin/zsh
+set -eu
+
+# CONDA UNINSTALL
+# Uninstall Swift/T and any cached packages from the Anaconda in PATH
+# Useful before reinstalling in a test installation
+
+# Report information about active Python/Conda:
+if ! which conda >& /dev/null
+then
+  print "No conda!"
+  return 1
+fi
+
+# Get this directory
+THIS=${0:A:h}
+source $THIS/../../turbine/code/scripts/helpers.zsh
+LOG_LABEL="conda-uninstall.sh:"
+
+print
+PY=$( which python )
+PY=${PY:h:h}
+log "using python in:"
+log $PY
+C=$( which conda )
+C=${C:h:h}
+log "using conda in:"
+log $C
+print
+
+printf "CONFIRM? "
+read -t 10 _ || true
+print
+
+CONDA_PREFIX=${CONDA_EXE:h:h}
+
+if conda uninstall --yes swift-t
+then
+  : OK
+else
+  log "proceeding..."
+fi
+
+
+SWIFTS=( $CONDA_PREFIX/pkgs/swift-t-*(/) )
+log "found ${#SWIFTS} Swift/T directories"
+
+for S in $SWIFTS
+do
+  rm -rf $S
+done
+
+SWIFTS=( $CONDA_PREFIX/pkgs/swift-t-* )
+for S in $SWIFTS
+do
+  rm -v $S
+done
+
+log "OK"
+
+# Local Variables:
+# buffer-file-coding-system:utf-8-unix
+# End:
