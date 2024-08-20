1560df20ea2004ba4a5a6be500931f2c48e7a8f1
Justin Wozniak <woz@anl.gov>
2024-08-20 14:04:52 -0500

    Improve path handling

--- dev/get-versions.sh
+++ dev/get-versions.sh
@@ -5,8 +5,11 @@
 # This has to be manually edited:
 SWIFT_T_VERSION=1.6.5
 
-# These are automatically extracted:
-CUTILS_VERSION=$(  cat c-utils/code/version.txt )
-ADLBX_VERSION=$(   cat lb/code/version.txt      )
-TURBINE_VERSION=$( cat turbine/code/version.txt )
-STC_VERSION=$(     cat stc/code/etc/version.txt )
+THIS=$( cd $( dirname $0 ) ; /bin/pwd )
+SWIFT_TOP=$( cd $THIS/.. ; /bin/pwd )
+
+# These  are automatically extracted:
+CUTILS_VERSION=$(  cat $SWIFT_TOP/c-utils/code/version.txt )
+ADLBX_VERSION=$(   cat $SWIFT_TOP/lb/code/version.txt      )
+TURBINE_VERSION=$( cat $SWIFT_TOP/turbine/code/version.txt )
+STC_VERSION=$(     cat $SWIFT_TOP/stc/code/etc/version.txt )
