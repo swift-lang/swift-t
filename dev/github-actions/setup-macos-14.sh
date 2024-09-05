3d69895db6e3826c1f51a28c197da9d7a6177502
Justin Wozniak <woz@anl.gov>
2024-09-05 14:43:11 -0500

    Run OS-specific installer

---
+++ dev/github-actions/setup-macos-14.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+
+# SETUP for matrix.os == macos-14
+
+START=$SECONDS
+
+log()
+{
+  printf "setup-macos-14.sh: %s\n" "$*"
+}
+
+log "Installing Homebrew packages..."
+
+if (
+  set -ex
+  brew update
+  brew install autoconf automake java make mpich swig tcl-tk
+) > brew.log
+then
+  log "Installed Homebrew packages in %i seconds."
+else
+  log "FAILED to install Homebrew packages!"
+  return 1
+fi
