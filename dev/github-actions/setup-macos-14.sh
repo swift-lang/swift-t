f8da19be3a1c2164c3d9c0a5dcc733610bdf69aa
Justin Wozniak <woz@anl.gov>
2024-09-05 14:44:51 -0500

    Capture error stream from Homebrew

--- dev/github-actions/setup-macos-14.sh
+++ dev/github-actions/setup-macos-14.sh
@@ -15,10 +15,12 @@ if (
   set -ex
   brew update
   brew install autoconf automake java make mpich swig tcl-tk
-) > brew.log
+) >& brew.log
 then
   log "Installed Homebrew packages in %i seconds."
 else
   log "FAILED to install Homebrew packages!"
+  log "brew.log:"
+  cat brew.log
   return 1
 fi
