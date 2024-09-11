44e200e003e6ad0ba70af67ec8d275031168e4cb
Justin Wozniak <woz@anl.gov>
2024-09-10 20:00:00 -0500

    Clean up; better comment

--- dev/conda/helpers.zsh
+++ dev/conda/helpers.zsh
@@ -4,15 +4,11 @@
 
 checksum()
 {
-  # Use redirection to suppress filename in md5 output
+  # Use redirection to suppress filename in program output
   local PKG=$1
   if [[ $PLATFORM =~ osx-* ]] {
     md5 -r < $PKG
   } else {
     md5sum < $PKG
   }
-}
-
-print_environment()
-{
 }
