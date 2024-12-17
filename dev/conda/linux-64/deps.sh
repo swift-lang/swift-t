4d96e2e5a6f4a6f384a4b763f0bfbece040d546b
Justin Wozniak <woz@anl.gov>
2024-12-17 11:50:31 -0600

    Turn on USE_ZLIB for all linux-64

--- dev/conda/linux-64/deps.sh
+++ dev/conda/linux-64/deps.sh
@@ -3,4 +3,6 @@
 
 # For some reason zlib is not found on linux-64, Python 3.8
 #     as of 2024-12-11:
-if [[ $PYTHON_VERSION == 3.8* ]] USE_ZLIB=1
+# Needed for all linux-64 as of 2024-12-17
+# if [[ $PYTHON_VERSION == 3.8* ]]
+export USE_ZLIB=1
