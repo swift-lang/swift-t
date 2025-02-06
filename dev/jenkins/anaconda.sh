e5e2116ad7c8aa2b68d77958a7d04c40812132b8
Justin Wozniak <woz@anl.gov>
2025-02-06 14:02:11 -0600

    Update comment

--- dev/jenkins/anaconda.sh
+++ dev/jenkins/anaconda.sh
@@ -57,7 +57,7 @@ zparseopts h=HELP
 if (( ${#HELP} )) help
 
 # Main argument processing
-R=""  # May become "-r"
+R=""  # May become ( -r R_VERSION )
 zparseopts -D -E -F c:=CL p:=PV r:=R u=UNINSTALL
 if (( ${#PV} )) PYTHON_VERSION=${PV[2]}
 if (( ${#CT} )) CONDA_TIMESTAMP=${CT[2]}
