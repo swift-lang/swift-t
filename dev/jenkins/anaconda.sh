fb7874730b7fc60a6f8b481adefcb9811006c083
Justin Wozniak <woz@anl.gov>
2025-04-24 11:31:53 -0500

    Try to fix LLP

--- dev/jenkins/anaconda.sh
+++ dev/jenkins/anaconda.sh
@@ -261,6 +261,10 @@ print
 
 task $SWIFT_T/dev/conda/conda-install.sh $PKG
 
+echo LLP1: $LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:-}:$CONDA_PREFIX/lib
+echo LLP2: $LD_LIBRARY_PATH
+
 log "TRY SWIFT/T..."
 (
   set -x
