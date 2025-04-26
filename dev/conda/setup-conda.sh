99208ecdc3cedd454409eb7220a491a1b979e30c
Justin Wozniak <woz@anl.gov>
2025-04-26 13:54:11 -0500

    Turn off env list

--- dev/conda/setup-conda.sh
+++ dev/conda/setup-conda.sh
@@ -8,7 +8,7 @@ echo "setting up Anaconda build tools in:"
 which conda
 echo
 
-conda env list
+# conda env list
 
 # Suppress this behavior from Conda on "conda build" errors:
 # Would you like conda to send this report to the core maintainers? [y/N]:
