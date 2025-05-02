140055340bca9f5a10687d2d828b371ea631dc84
Justin Wozniak <woz@anl.gov>
2025-05-02 16:17:46 -0500

    WIP R support

--- dev/github-actions/edit-settings.sh
+++ dev/github-actions/edit-settings.sh
@@ -1,12 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 set -eu
 
 # SETTINGS SH
 # Edit swift-t-settings.sh via settings.sed
 # Assumes PWD is the top of the Swift/T clone
 # We immediately cd into /dev/
+# Provide -r to turn on R support
 
 cd dev
 SETTINGS_SH=build/swift-t-settings.sh
+SETTINGS_ORIG=build/swift-t-settings.orig
+
+cp -v $SETTINGS_SH $SETTINGS_ORIG
+
 echo "Editing $SETTINGS_SH ..."
-sed -i -f github-actions/settings.sed build/swift-t-settings.sh
+sed -i -f github-actions/settings.sed $SETTINGS_SH
+
+if [[ "$1" == "-r" ]]
+then
+  RHOME=$( R RHOME )
+  echo "RHOME=$RHOME"
+  sed -i 's/ENABLE_R=0/ENABLE_R=1/'       $SETTINGS_SH
+  sed -i "s@# R_INSTALL=.*@R_INSTALL=$RHOME@" $SETTINGS_SH
+fi
+
+echo "Settings changed:"
+diff $SETTINGS_ORIG $SETTINGS_SH
---
+++ dev/github-actions/install-R.sh
@@ -0,0 +1,34 @@
+#!/bin/zsh
+set -eu
+
+# INSTALL R SH
+# and RInside
+
+MATRIX_OS=$1
+
+START=$SECONDS
+
+log()
+{
+  printf "install-R.sh: %s\n" "$*"
+}
+
+case $MATRIX_OS {
+  "ubuntu-latest") PKG="r"       ;;
+  "macos-14")      PKG="emews-r" ;;
+  *) log "unknown OS=$MATRIX_OS"
+     exit 1 ;;
+}
+
+# For 'set -x' , including newline
+PS4="
++ "
+
+() {
+  set -x
+  conda install --yes --quiet -c conda-forge -c swift-t $PKG
+  which Rscript
+  Rscript dev/conda/install-RInside.R
+}
+
+log OK
