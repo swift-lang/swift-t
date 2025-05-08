492755c640acd41f6b4e0a60b737afc93c7e35bb
Justin Wozniak <woz@anl.gov>
2025-05-08 13:49:37 -0500

    Activate Anaconda; use a log()

--- dev/github-actions/edit-settings.sh
+++ dev/github-actions/edit-settings.sh
@@ -7,22 +7,31 @@ set -eu
 # We immediately cd into /dev/
 # Provide -r to turn on R support
 
+log()
+{
+  echo "edit-settings.sh:" ${*}
+}
+
 cd dev
 SETTINGS_SH=build/swift-t-settings.sh
 SETTINGS_ORIG=build/swift-t-settings.orig
 
 cp -v $SETTINGS_SH $SETTINGS_ORIG
 
-echo "Editing $SETTINGS_SH ..."
+ACTIVATE=( source $CONDA/bin/activate base )
+log ${ACTIVATE[@]}
+$ACTIVATE
+
+log $SETTINGS_SH ...
 sed -i -f github-actions/settings.sed $SETTINGS_SH
 
 if [[ "$1" == "-r" ]]
 then
   RHOME=$( R RHOME )
-  echo "RHOME=$RHOME"
+  log "RHOME=$RHOME"
   sed -i 's/ENABLE_R=0/ENABLE_R=1/'       $SETTINGS_SH
   sed -i "s@# R_INSTALL=.*@R_INSTALL=$RHOME@" $SETTINGS_SH
 fi
 
-echo "Settings changed:"
+log "settings changed:"
 diff $SETTINGS_ORIG $SETTINGS_SH
