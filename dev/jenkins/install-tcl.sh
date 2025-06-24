f7410c94abbbd1cd9b17ba1066f7db15fe41ff34
Justin Wozniak <woz@anl.gov>
2025-06-24 16:59:32 -0500

    Do make clean

--- dev/jenkins/install-tcl.sh
+++ dev/jenkins/install-tcl.sh
@@ -8,11 +8,21 @@ renice --priority 19 --pid $$ >& /dev/null
 pwd
 ls -l
 
-rm -rfv downloads
-rm -fv tcl8.6.12-src.tar.gz
+# Clean up old stuff
+(
+  set -x
+  rm -fv tcl8.6.12/README.md
+  rm -fr tcl8.6.12
+  rm -fv tcl8.6.12-src.tar.gz
+)
+
+# Download and extract
 wget --no-verbose https://prdownloads.sourceforge.net/tcl/tcl8.6.12-src.tar.gz
 tar xfz tcl8.6.12-src.tar.gz
+
+# Build it!
 cd tcl8.6.12/unix
 ./configure --prefix=$WORKSPACE/sfw/tcl-8.6.12
+make clean
 make binaries libraries
 make install-binaries install-libraries install-headers
