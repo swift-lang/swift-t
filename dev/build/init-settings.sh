19b287762589c745972ee87ae4c64005f5690589
Justin Wozniak <woz@anl.gov>
2025-10-27 14:08:48 -0500

    Suppress output under conda build

--- dev/build/init-settings.sh
+++ dev/build/init-settings.sh
@@ -13,5 +13,10 @@ then
 fi
 
 cp ${THIS}/swift-t-settings.sh.template ${THIS}/swift-t-settings.sh
-echo "Created ${THIS}/swift-t-settings.sh"
-echo "You may edit swift-t-settings.sh before building."
+
+if (( ${CONDA_BUILD:-0} == 0 ))
+then
+  # This output is suppressed during a conda build
+  echo "Created ${THIS}/swift-t-settings.sh"
+  echo "You may edit swift-t-settings.sh before building."
+fi
