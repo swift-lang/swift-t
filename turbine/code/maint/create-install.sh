313d65bf3affe12fbe5b25d35519fe3bd4a444ca
Justin Wozniak <woz@anl.gov>
2025-10-28 13:38:56 -0500

    Improve metadata

--- turbine/code/maint/create-install.sh
+++ turbine/code/maint/create-install.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-# set -eu
+#!/bin/zsh
+set -eu
 
 # CREATE INSTALL SH
 # Make install.txt, a user-readable provenance file
@@ -7,6 +7,19 @@
 OUTPUT=$1
 INSTALL_PREFIX=$2
 ENABLE_DEB=$3
+
+THIS=${0:h:A}
+
+report-timestamp()
+{
+  TIMESTAMP=$THIS/../../../dev/build/timestamp.txt
+  if [[ -f $TIMESTAMP ]]
+  then
+    cat $TIMESTAMP
+  else
+    echo "TIMESTAMP MISSING"
+  fi
+}
 
 {
   echo  "PREFIX:      $INSTALL_PREFIX"
@@ -16,5 +29,15 @@ ENABLE_DEB=$3
 
   echo -n "REPO: "
   # Use true to ignore errors (e.g., if this is not an git clone)
-  git log -n 1 --pretty=format:"COMMIT: %h %aD %s%n" 2>&1 || true
+  if git log -n 1 > /dev/null 2>&1
+  then
+    git log -n 1 --pretty=format:"COMMIT: %h %aD %s%n" 2>&1
+  elif (( ${CONDA_BUILD:-0} ))
+  then
+    echo "CONDA:"
+    report-timestamp
+  else
+    echo "RELEASE:"
+    report-timestamp
+  fi
 } > $OUTPUT
