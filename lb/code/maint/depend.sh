a2152ac72313065db8507b2ccaa7ca629a746a5d
Justin Wozniak <woz@anl.gov>
2026-07-03 11:12:43 -0500

    Improve depend.sh

--- lb/code/maint/depend.sh
+++ lb/code/maint/depend.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+set -eu
+set -o pipefail
 
 # Autogenerates Makefile dependencies
 # See the GCC documentation for -M, -MG
-# This does not work with XLC - set DEPCC to gcc
+# This does not work with XLC - set DEPCC=gcc
 
 DIR="$1"
 shift
-if [ -n "$DIR" ] ; then
-    DIR="$DIR"
-fi
 
+# This sed command replaces the directory component of the
+#      target file with a standardized directory in DIR,
+#      and add the .d file to the dependency list
 ${DEPCC} -M -MG "$@" | sed -e "s@^\(.*\)\.o:@$DIR\1.d $DIR\1.o:@"
