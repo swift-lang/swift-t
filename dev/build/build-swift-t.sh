b338bfbfa8fdc21d24d5ea9cc13e33c374d8de79
Justin Wozniak <woz@anl.gov>
2026-08-27 11:09:44 -0500

    Use source for speed and global variables

--- dev/build/build-swift-t.sh
+++ dev/build/build-swift-t.sh
@@ -16,7 +16,7 @@ $THIS/check-settings.sh
 source $THIS/functions.sh
 source $THIS/options.sh
 source $THIS/swift-t-settings.sh
-$THIS/check-tools.sh
+source $THIS/check-tools.sh
 source $THIS/setup.sh
 
 LOG $LOG_WARN "Installing Swift/T into: $SWIFT_T_PREFIX"
